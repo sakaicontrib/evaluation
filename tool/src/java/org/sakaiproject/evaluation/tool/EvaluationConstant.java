84864d4ccb4854e2664000f2cc8cf961c0da2bb3
kahuja@vt.edu <kahuja@vt.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-01-18 21:10:24 +0000

    Main administrative page: Finish work in producer and html file with correct RSF mappings.
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@3065 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/EvaluationConstant.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/EvaluationConstant.java
@@ -109,4 +109,15 @@ public class EvaluationConstant {
 	 * The default number of rows to use when displaying a textarea type input box
 	 */
 	public static final Integer DEFAULT_ROWS = new Integer(2);
+	
+	// For main administrative page
+	public static final String[] NUM_QUESTIONS_INST_ADMINS = new String[] {
+		"0", "1", "2", "3", "4", "5", "8", "10", "15", "20"};	
+
+	public static final String[] NUM_RESPONSES_BEFORE_VIEW_RESULTS = new String[] {
+		"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "15", "20"};	
+
+	public static final String[] MAX_QUESTIONS_IN_BLOCK = new String[] {
+		"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "12", "15", "18", "20"};	
 }
+
--- tool/src/java/org/sakaiproject/evaluation/tool/producers/AdministrateProducer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/producers/AdministrateProducer.java
@@ -18,11 +18,19 @@ import java.util.List;
 
 import org.sakaiproject.evaluation.logic.EvalExternalLogic;
 import org.sakaiproject.evaluation.logic.EvalSettings;
+import org.sakaiproject.evaluation.tool.EvaluationConstant;
 
 import uk.org.ponder.messageutil.MessageLocator;
+import uk.org.ponder.rsf.components.ELReference;
+import uk.org.ponder.rsf.components.UIBoundBoolean;
+import uk.org.ponder.rsf.components.UIBoundList;
+import uk.org.ponder.rsf.components.UICommand;
 import uk.org.ponder.rsf.components.UIContainer;
+import uk.org.ponder.rsf.components.UIForm;
+import uk.org.ponder.rsf.components.UIInput;
 import uk.org.ponder.rsf.components.UIInternalLink;
 import uk.org.ponder.rsf.components.UIOutput;
+import uk.org.ponder.rsf.components.UISelect;
 import uk.org.ponder.rsf.flow.jsfnav.NavigationCaseReporter;
 import uk.org.ponder.rsf.view.ComponentChecker;
 import uk.org.ponder.rsf.view.ViewComponentProducer;
@@ -73,45 +81,118 @@ public class AdministrateProducer implements ViewComponentProducer, NavigationCa
 			UIInternalLink.make(tofill, "control-panel-toplink", messageLocator.getMessage("controlpanel.page.title"), //$NON-NLS-1$ //$NON-NLS-2$
 					new SimpleViewParameters(ControlPanelProducer.VIEW_ID));
 		}
-
 		UIInternalLink.make(tofill, "summary-toplink", messageLocator.getMessage("summary.page.title"), new SimpleViewParameters(SummaryProducer.VIEW_ID)); //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "system-settings-instructions", messageLocator.getMessage("administrate.system.settings.instructions")); //$NON-NLS-1$ //$NON-NLS-2$
 
+		//System Settings
+		UIForm form = UIForm.make(tofill, "basic-form"); //$NON-NLS-1$		
+		UIOutput.make(form, "system-settings-header", messageLocator.getMessage("administrate.system.settings.header")); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "system-settings-instructions", messageLocator.getMessage("administrate.system.settings.instructions")); //$NON-NLS-1$ //$NON-NLS-2$
 
-		UIOutput.make(tofill, "instructors-eval-create-note", messageLocator.getMessage("administrate.instructors.eval.create.note")); //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "instructors-view-results-note", messageLocator.getMessage("administrate.instructors.view.results.note")); //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "instructors-email-students-note", messageLocator.getMessage("administrate.instructors.email.students.note")); //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "instructors-hierarchy-email-note", messageLocator.getMessage("administrate.instructors.hierarchy.email.note")); //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "instructors-num-questions-note", messageLocator.getMessage("administrate.instructors.num.questions.note")); //$NON-NLS-1$ //$NON-NLS-2$
+		//Instructor Settings
+		UIOutput.make(form, "instructor-settings-header", messageLocator.getMessage("administrate.instructor.settings.header")); //$NON-NLS-1$ //$NON-NLS-2$
+		UIBoundBoolean.make(form, "instructors-eval-create", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "instructors-eval-create-note", messageLocator.getMessage("administrate.instructors.eval.create.note")); //$NON-NLS-1$ //$NON-NLS-2$
+		UIBoundBoolean.make(form, "instructors-view-results", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "instructors-view-results-note", messageLocator.getMessage("administrate.instructors.view.results.note")); //$NON-NLS-1$ //$NON-NLS-2$
+		UIBoundBoolean.make(form, "instructors-email-students", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "instructors-email-students-note", messageLocator.getMessage("administrate.instructors.email.students.note")); //$NON-NLS-1$ //$NON-NLS-2$
+		UIBoundBoolean.make(form, "instructors-hierarchy-email", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "instructors-hierarchy-email-note", messageLocator.getMessage("administrate.instructors.hierarchy.email.note")); //$NON-NLS-1$ //$NON-NLS-2$
 		
-		UIOutput.make(tofill, "student-settings-header", messageLocator.getMessage("administrate.student.settings.header")); //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "students-unanswered-note", messageLocator.getMessage("administrate.students.unanswered.note")); //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "students-modify-responses-note", messageLocator.getMessage("administrate.students.modify.responses.note")); //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "students-view-results-note", messageLocator.getMessage("administrate.students.view.results.note")); //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "administrator-settings-header", messageLocator.getMessage("administrate.admin.settings.header"));		 //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "admin-hierarchy-num-questions-note", messageLocator.getMessage("administrate.admin.hierarchy.num.questions.note")); //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "admin-view-instructor-questions-note", messageLocator.getMessage("administrate.admin.view.instructor.questions.note"));		 //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "admin-super-modify-question-note", messageLocator.getMessage("administrate.admin.super.modify.question.note")); //$NON-NLS-1$ //$NON-NLS-2$
+		//Select for number of questions intructors can add
+		UISelect numQuestionsInst = UISelect.make(form, "instructors-num-questions"); //$NON-NLS-1$
+		numQuestionsInst.selection = new UIInput();
+		numQuestionsInst.selection.valuebinding = new ELReference("#{????.????}"); //$NON-NLS-1$
+		UIBoundList numQuestionsInstValues = new UIBoundList();
+		numQuestionsInstValues.setValue(EvaluationConstant.NUM_QUESTIONS_INST_ADMINS);
+		numQuestionsInst.optionlist = numQuestionsInstValues;
+		numQuestionsInst.optionnames = numQuestionsInstValues;	
+		UIOutput.make(form, "instructors-num-questions-note", messageLocator.getMessage("administrate.instructors.num.questions.note")); //$NON-NLS-1$ //$NON-NLS-2$
 		
-		UIOutput.make(tofill, "general-settings-header", messageLocator.getMessage("administrate.general.settings.header"));		 //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "general-helpdesk-email-note", messageLocator.getMessage("administrate.general.helpdesk.email.note")); //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "general-responses-before-view-note", messageLocator.getMessage("administrate.general.responses.before.view.note"));		 //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "general-na-allowed-note", messageLocator.getMessage("administrate.general.na.allowed.note"));	 //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "general-max-questions-block-note", messageLocator.getMessage("administrate.general.max.questions.block.note"));		 //$NON-NLS-1$ //$NON-NLS-2$
+		// Student Settings
+		UIOutput.make(form, "student-settings-header", messageLocator.getMessage("administrate.student.settings.header")); //$NON-NLS-1$ //$NON-NLS-2$
+		UIBoundBoolean.make(form, "students-unanswered", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "students-unanswered-note", messageLocator.getMessage("administrate.students.unanswered.note")); //$NON-NLS-1$ //$NON-NLS-2$
+		UIBoundBoolean.make(form, "students-modify-responses", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "students-modify-responses-note", messageLocator.getMessage("administrate.students.modify.responses.note")); //$NON-NLS-1$ //$NON-NLS-2$
+		UIBoundBoolean.make(form, "students-view-results", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "students-view-results-note", messageLocator.getMessage("administrate.students.view.results.note")); //$NON-NLS-1$ //$NON-NLS-2$
 		
-		// TODO - permission-settings select, functionality and i18n
-		
-		UIOutput.make(tofill, "general-template-sharing-note", messageLocator.getMessage("administrate.general.template.sharing.note"));		 //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "general-default-question-category", messageLocator.getMessage("administrate.general.default.question.category.note"));	 //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "general-use-stop-date-note", messageLocator.getMessage("administrate.general.use.stop.date.note"));		 //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "general-expert-templates-note", messageLocator.getMessage("administrate.general.expert.templates.note"));	 //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "general-expert-questions-note", messageLocator.getMessage("administrate.general.expert.questions.note"));		 //$NON-NLS-1$ //$NON-NLS-2$
-		UIOutput.make(tofill, "general-same-view-date-note", messageLocator.getMessage("administrate.general.same.view.date.note"));	 //$NON-NLS-1$ //$NON-NLS-2$
+		// Administrator Settings
+		UIOutput.make(form, "administrator-settings-header", messageLocator.getMessage("administrate.admin.settings.header"));		 //$NON-NLS-1$ //$NON-NLS-2$
 
-		// TODO - submit button i18n
+		//Select for number of questions admins can add
+		UISelect numQuestionsAdmin = UISelect.make(form, "admin-hierarchy-num-questions"); //$NON-NLS-1$
+		numQuestionsAdmin.selection = new UIInput();
+		numQuestionsAdmin.selection.valuebinding = new ELReference("#{????.????}"); //$NON-NLS-1$
+		UIBoundList numQuestionsAdminValues = new UIBoundList();
+		numQuestionsAdminValues.setValue(EvaluationConstant.NUM_QUESTIONS_INST_ADMINS);
+		numQuestionsAdmin.optionlist = numQuestionsAdminValues;
+		numQuestionsAdmin.optionnames = numQuestionsAdminValues;	
+		UIOutput.make(form, "admin-hierarchy-num-questions-note", messageLocator.getMessage("administrate.admin.hierarchy.num.questions.note")); //$NON-NLS-1$ //$NON-NLS-2$
+
+		UIBoundBoolean.make(form, "admin-view-instructor-questions", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "admin-view-instructor-questions-note", messageLocator.getMessage("administrate.admin.view.instructor.questions.note"));		 //$NON-NLS-1$ //$NON-NLS-2$
+		UIBoundBoolean.make(form, "admin-super-modify-question", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "admin-super-modify-question-note", messageLocator.getMessage("administrate.admin.super.modify.question.note")); //$NON-NLS-1$ //$NON-NLS-2$
 		
+		UIOutput.make(form, "general-settings-header", messageLocator.getMessage("administrate.general.settings.header"));		 //$NON-NLS-1$ //$NON-NLS-2$
+		UIInput.make(form, "general-helpdesk-email", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "general-helpdesk-email-note", messageLocator.getMessage("administrate.general.helpdesk.email.note")); //$NON-NLS-1$ //$NON-NLS-2$
 		
-		// TODO - add functionality
+		// Select for number of responses before results could be viewed
+		UISelect numResponsesBeforeView= UISelect.make(form, "general-responses-before-view"); //$NON-NLS-1$
+		numResponsesBeforeView.selection = new UIInput();
+		numResponsesBeforeView.selection.valuebinding = new ELReference("#{????.????}"); //$NON-NLS-1$
+		UIBoundList numResponsesBeforeViewValues = new UIBoundList();
+		numResponsesBeforeViewValues.setValue(EvaluationConstant.NUM_RESPONSES_BEFORE_VIEW_RESULTS);
+		numResponsesBeforeView.optionlist = numResponsesBeforeViewValues;
+		numResponsesBeforeView.optionnames = numResponsesBeforeViewValues;	
+		UIOutput.make(form, "general-responses-before-view-note", messageLocator.getMessage("administrate.general.responses.before.view.note"));		 //$NON-NLS-1$ //$NON-NLS-2$
+		
+		UIBoundBoolean.make(form, "general-na-allowed", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "general-na-allowed-note", messageLocator.getMessage("administrate.general.na.allowed.note"));	 //$NON-NLS-1$ //$NON-NLS-2$
+		
+		// Select for maximum number of questions in a block
+		UISelect maxQuestionsInBlock = UISelect.make(form, "general-max-questions-block"); //$NON-NLS-1$
+		maxQuestionsInBlock.selection = new UIInput();
+		maxQuestionsInBlock.selection.valuebinding = new ELReference("#{????.????}"); //$NON-NLS-1$
+		UIBoundList maxQuestionsInBlockValues = new UIBoundList();
+		maxQuestionsInBlockValues.setValue(EvaluationConstant.MAX_QUESTIONS_IN_BLOCK);
+		maxQuestionsInBlock.optionlist = maxQuestionsInBlockValues;
+		maxQuestionsInBlock.optionnames = maxQuestionsInBlockValues;	
+		UIOutput.make(form, "general-max-questions-block-note", messageLocator.getMessage("administrate.general.max.questions.block.note"));		 //$NON-NLS-1$ //$NON-NLS-2$
+		
+		// Select for template sharing and visibility settings
+		UISelect templateSharingVisibility = UISelect.make(form, "general-template-sharing"); //$NON-NLS-1$
+		templateSharingVisibility.selection = new UIInput();
+		templateSharingVisibility.selection.valuebinding = new ELReference("#{????.????}"); //$NON-NLS-1$
+		UIBoundList templateSharingVisibilityValues = new UIBoundList();
+		String[] sharingList = 
+		{
+			messageLocator.getMessage("modifytemplatetitledesc.sharing.private"),
+			messageLocator.getMessage("modifytemplatetitledesc.sharing.visible"),
+			messageLocator.getMessage("modifytemplatetitledesc.sharing.shared"),
+			messageLocator.getMessage("modifytemplatetitledesc.sharing.public")
+		};		
+		templateSharingVisibilityValues.setValue(sharingList);
+		templateSharingVisibility.optionlist = templateSharingVisibilityValues;
+		templateSharingVisibility.optionnames = templateSharingVisibilityValues;	
+		UIOutput.make(form, "general-template-sharing-note", messageLocator.getMessage("administrate.general.template.sharing.note"));		 //$NON-NLS-1$ //$NON-NLS-2$
+		
+		UIBoundBoolean.make(form, "general-default-question", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "general-default-question-category", messageLocator.getMessage("administrate.general.default.question.category.note"));	 //$NON-NLS-1$ //$NON-NLS-2$
+		UIBoundBoolean.make(form, "general-use-stop-date", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "general-use-stop-date-note", messageLocator.getMessage("administrate.general.use.stop.date.note"));		 //$NON-NLS-1$ //$NON-NLS-2$
+		UIBoundBoolean.make(form, "general-expert-templates", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "general-expert-templates-note", messageLocator.getMessage("administrate.general.expert.templates.note"));	 //$NON-NLS-1$ //$NON-NLS-2$
+		UIBoundBoolean.make(form, "general-expert-questions", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "general-expert-questions-note", messageLocator.getMessage("administrate.general.expert.questions.note"));		 //$NON-NLS-1$ //$NON-NLS-2$
+		UIBoundBoolean.make(form, "general-same-view-date", "#{????.????}", null); //$NON-NLS-1$ //$NON-NLS-2$
+		UIOutput.make(form, "general-same-view-date-note", messageLocator.getMessage("administrate.general.same.view.date.note"));	 //$NON-NLS-1$ //$NON-NLS-2$
+
+		// Save settings button
+		UICommand.make(form, "saveSettings", messageLocator.getMessage("administrate.save.settings.button"), "#{templateBean.createTemplateAction}"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$		
 	}
 	
 	public List reportNavigationCases() {
