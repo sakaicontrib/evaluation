87b1f0bd6f02b9f3b5bf2246f99c06f18c779db4
Aaron Zeckoski <azeckoski@vt.edu>
2007-01-19 20:59:21 +0000

    COMPILING - all code compiles again A good bit of functionality is not working right now but we will get to that Completed removal of the old logic API by removing the entries from the spring files Code starts up and tool can be accessed in Sakai again with no runtime failures
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@3089 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- impl/logic/src/java/org/sakaiproject/evaluation/logic/impl/EvalTemplatesLogicImpl.java
+++ impl/logic/src/java/org/sakaiproject/evaluation/logic/impl/EvalTemplatesLogicImpl.java
@@ -15,9 +15,7 @@
 package org.sakaiproject.evaluation.logic.impl;
 
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
