915170474c589e2828addabccaf28bd1c929ddad
Aaron Zeckoski <azeckoski@vt.edu>
2007-01-22 15:28:26 +0000

    Updated interceptor comments to make them more clear
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@3095 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- impl/logic/src/java/org/sakaiproject/evaluation/logic/impl/interceptors/EvaluationInterceptor.java
+++ impl/logic/src/java/org/sakaiproject/evaluation/logic/impl/interceptors/EvaluationInterceptor.java
@@ -1,6 +1,17 @@
-/*
- * Created on 03-Jan-2007
- */
+/******************************************************************************
+ * EvalAssignContextInterceptor.java - created on Jan 03, 2006
+ * 
+ * Copyright (c) 2007 Virginia Polytechnic Institute and State University
+ * Licensed under the Educational Community License version 1.0
+ * 
+ * A copy of the Educational Community License has been included in this 
+ * distribution and is available at: http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Contributors:
+ * Antranig Basman
+ * 
+ *****************************************************************************/
+
 package org.sakaiproject.evaluation.logic.impl.interceptors;
 
 import org.aopalliance.intercept.MethodInterceptor;
@@ -9,34 +20,39 @@ import org.sakaiproject.evaluation.logic.impl.EvalEvaluationsLogicImpl;
 import org.sakaiproject.evaluation.model.EvalEvaluation;
 
 
+/**
+ * This interceptor uses methods in the other logic APIs to enforce "modify"
+ * rules for persistent objects by doing checks whenever any method on the 
+ * intercepted object is accessed (modifyEvaluation filters to only look at
+ * set methods)
+ * 
+ * @author Antranig Basman
+ */
 public class EvaluationInterceptor implements MethodInterceptor {
-  private EvalEvaluationsLogicImpl evalEvaluationsLogicImpl;
-  
-  public EvaluationInterceptor(EvalEvaluationsLogicImpl evalEvaluationsLogicImpl) {
-    this.evalEvaluationsLogicImpl = evalEvaluationsLogicImpl;
-  }
-  
-  public Object invoke(MethodInvocation arg0) throws Throwable {
-    String method = arg0.getMethod().getName();
-    EvalEvaluation eval = (EvalEvaluation) arg0.getThis();
-    if (method.equals("equals")) {
-      return Boolean.valueOf(testEquals(eval, (EvalEvaluation) arg0.getArguments()[0]));
-    }
-    if (eval.getId() != null) {
-      evalEvaluationsLogicImpl.modifyEvaluation(eval, arg0.getMethod().getName());
-    }
-    return arg0.proceed();
-  }
+	private EvalEvaluationsLogicImpl evalEvaluationsLogicImpl;
 
-  private boolean testEquals(EvalEvaluation eval, EvalEvaluation eval2) {
-    if (eval.getClass() == EvalEvaluation.class ^ eval2.getClass() == EvalEvaluation.class) {
-      throw new IllegalArgumentException("Illegal comparison of EvalEvaluation - " +
-            " can only compare persistent entities with other persistent entities");
-    }
-    return eval.getId().equals(eval2.getId());
-  }
+	public EvaluationInterceptor(EvalEvaluationsLogicImpl evalEvaluationsLogicImpl) {
+		this.evalEvaluationsLogicImpl = evalEvaluationsLogicImpl;
+	}
 
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		String method = invocation.getMethod().getName();
+		EvalEvaluation eval = (EvalEvaluation) invocation.getThis();
+		if (method.equals("equals")) {
+			return Boolean.valueOf(testEquals(eval, (EvalEvaluation) invocation.getArguments()[0]));
+		}
+		if (eval.getId() != null) {
+			evalEvaluationsLogicImpl.modifyEvaluation(eval, invocation.getMethod().getName());
+		}
+		return invocation.proceed();
+	}
 
-
+	private boolean testEquals(EvalEvaluation eval, EvalEvaluation eval2) {
+		if (eval.getClass() == EvalEvaluation.class ^ eval2.getClass() == EvalEvaluation.class) {
+			throw new IllegalArgumentException("Illegal comparison of EvalEvaluation - " +
+			" can only compare persistent entities with other persistent entities");
+		}
+		return eval.getId().equals(eval2.getId());
+	}
 
 }
