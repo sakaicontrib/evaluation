7f6d0637a4d1d235efda3f15b6e782792cd7e15a
Kapil Ahuja <kahuja@vt.edu>
2007-01-25 02:05:29 +0000

    Administrative homepage completely done.
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@3240 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/SettingsWBL.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/SettingsWBL.java
@@ -1,6 +1,16 @@
-/*
- * Created on 23 Jan 2007
- */
+/******************************************************************************
+ * SettingsWBL.java - created by antranig@caret.cam.ac.uk
+ * 
+ * Copyright (c) 2007 Virginia Polytechnic Institute and State University
+ * Licensed under the Educational Community License version 1.0
+ * 
+ * A copy of the Educational Community License has been included in this 
+ * distribution and is available at: http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Contributors:
+ * Antranig Basman (antranig@caret.cam.ac.uk)
+ * Kapil Ahuja (kahuja@vt.edu)
+ *****************************************************************************/
 package org.sakaiproject.evaluation.tool;
 
 import org.sakaiproject.evaluation.logic.EvalSettings;
@@ -8,11 +18,18 @@ import org.sakaiproject.evaluation.logic.EvaluationSettingsParse;
 
 import uk.org.ponder.beanutil.WriteableBeanLocator;
 import uk.org.ponder.conversion.StaticLeafParser;
+import uk.org.ponder.messageutil.MessageLocator;
 import uk.org.ponder.util.UniversalRuntimeException;
 
+/**
+ * Obviates the need for a backing bean for administrative functionality.
+ * 
+ * @author Antranig Basman (antranig@caret.cam.ac.uk)
+ */
 public class SettingsWBL implements WriteableBeanLocator {
   private StaticLeafParser leafParser;
   private EvalSettings evalSettings;
+  private MessageLocator messageLocator;
   
   public void setEvalSettings(EvalSettings evalSettings) {
     this.evalSettings = evalSettings;
@@ -22,6 +39,10 @@ public class SettingsWBL implements WriteableBeanLocator {
     this.leafParser = leafParser;
   }
 
+  public void setMessageLocator(MessageLocator messageLocator) {
+	this.messageLocator = messageLocator;
+  }
+  
   public boolean remove(String beanname) {
     throw new UnsupportedOperationException("Removal not supported from SettingsWBL");
   }
@@ -36,17 +57,81 @@ public class SettingsWBL implements WriteableBeanLocator {
     }
   }
 
+  /*
+   * Sets the data from producer to EvalSettings. 
+   */
   public void set(String beanname, Object toset) {
-    // The UI has already converted Booleans - this is primarily to catch Integers
+	  
+    /*
+     *  The UI has already converted Booleans.
+     *  this is primarily to catch Integers and strings. 
+     */
     if (toset instanceof String) {
-      Class proptype = getPropertyType(beanname);
-      toset = leafParser.parse(proptype, (String) toset);
+    	Class proptype = getPropertyType(beanname);
+
+	  /*
+	   * Fields inside the if block are not directly mapped to those in database.
+	   * Thus parsing them.  
+	   */
+      if (  beanname.equals(EvalSettings.INSTRUCTOR_ALLOWED_VIEW_RESULTS) ||
+			beanname.equals(EvalSettings.INSTRUCTOR_MUST_USE_EVALS_FROM_ABOVE) ||
+			beanname.equals(EvalSettings.STUDENT_ALLOWED_LEAVE_UNANSWERED) ||
+			beanname.equals(EvalSettings.STUDENT_MODIFY_RESPONSES) ||
+			beanname.equals(EvalSettings.STUDENT_VIEW_RESULTS) ||
+			beanname.equals(EvalSettings.ITEM_USE_COURSE_CATEGORY_ONLY) ) {
+    	  
+        if ( ((String)toset).equals(messageLocator.getMessage("administrate.configurable.label")) ) 
+          toset = null;
+        else if ( ((String)toset).equals(messageLocator.getMessage("administrate.true.label")) ) 
+          toset = "true";
+        else if ( ((String)toset).equals(messageLocator.getMessage("administrate.false.label")) ) 
+          toset = "false";
+        else {
+          //do nothing
+        }
+	  }
+
+      /*
+       * If the value of field is null it means that this particular value should be
+       * deleted from the table. This parsing using leaf parser is not needed. 
+       */ 
+	  if (toset != null)
+        toset = leafParser.parse(proptype, (String) toset);
+	  
     }
+    
     evalSettings.set(beanname, toset);
   }
 
+  /*
+   * Gets the data from EvalSettings and returns to producer. 
+   */
   public Object locateBean(String path) {
-    return evalSettings.get(path);
-  }
+	  
+    Object toget = evalSettings.get(path);
 
+    /*
+     * Fields inside the if block are not directly mapped to those in database.
+     * Thus parsing them.  
+     */
+    if ( path.equals(EvalSettings.INSTRUCTOR_ALLOWED_VIEW_RESULTS) ||
+    	 path.equals(EvalSettings.INSTRUCTOR_MUST_USE_EVALS_FROM_ABOVE) ||
+    	 path.equals(EvalSettings.STUDENT_ALLOWED_LEAVE_UNANSWERED) ||
+    	 path.equals(EvalSettings.STUDENT_MODIFY_RESPONSES) ||
+    	 path.equals(EvalSettings.STUDENT_VIEW_RESULTS) ||
+    	 path.equals(EvalSettings.ITEM_USE_COURSE_CATEGORY_ONLY)) {
+
+      if (toget == null)
+        toget = messageLocator.getMessage("administrate.configurable.label");
+      else if ( (toget instanceof Boolean) && ((Boolean)toget).booleanValue() )
+        toget = messageLocator.getMessage("administrate.true.label");
+      else if ( (toget instanceof Boolean) && !(((Boolean)toget).booleanValue()) )
+        toget = messageLocator.getMessage("administrate.false.label");
+      else {
+      	//do nothing
+      }
+    }
+    return toget;
+  }
+  
 }
