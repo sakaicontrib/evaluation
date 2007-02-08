5b154e190c4868c2dd67951a0afa75ad5a6858a5
fengr@vt.edu <fengr@vt.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-02-08 16:57:43 +0000

    Enforce at least 2 check box slection for Block creation Enforce Same scale of slected item for Block creation, Error page
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@3391 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/producers/ModifyBlockProducer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/producers/ModifyBlockProducer.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.sakaiproject.evaluation.logic.EvalItemsLogic;
+import org.sakaiproject.evaluation.model.EvalScale;
 import org.sakaiproject.evaluation.model.EvalTemplate;
 import org.sakaiproject.evaluation.model.EvalTemplateItem;
 import org.sakaiproject.evaluation.model.constant.EvalConstants;
@@ -80,115 +81,137 @@ public class ModifyBlockProducer implements ViewComponentProducer,ViewParamsRepo
 		
 		if(evParameters != null && evParameters.templateItemIds != null){
 			templateId =  evParameters.templateId;
-				    
+			boolean modify = false;//this variable indicate if it is for modify existing Block
+			
 			System.out.println("templateId="+evParameters.templateId);
 			System.out.println("block item ids="+evParameters.templateItemIds);
 			
 			//analyze the string of templateItemIds
 			String[] strIds = evParameters.templateItemIds.split(",");
-				EvalTemplateItem templateItems[] = new EvalTemplateItem[strIds.length];
+			EvalTemplateItem templateItems[] = new EvalTemplateItem[strIds.length];
 			for(int i=0; i< strIds.length;i++){
 				System.out.println("checked id["+i+"]="+ strIds[i]);
 				templateItems[i]= itemsLogic.getTemplateItemById(Long.valueOf(strIds[i]));
-				//TODO:check if each templateItem has the same scale, otherwise throw error 
-				System.out.println("TemplateItems["+i+"].itemText="+templateItems[i].getItem().getItemText() );		
 			}
 			
-			//TODO:JAVACRIPT need to enforce at leat 2 items are checked for Block creation on TemplateModify page
-			//if strIds length is 1, then this is from an existing Block type--from Modify Link on TemplateModify page
+			if(strIds.length ==1 && templateItems[0]!= null)
+				modify=true;
 			
+			//check if each templateItem has the same scale, otherwise show warning text
+			boolean validScaleIds = true;
+			if(templateItems.length >1){
+				Long scaleId = templateItems[0].getItem().getScale().getId();
+				System.out.println("scale id["+0 +"]="+ scaleId.intValue());
+				for(int i=1; i<templateItems.length;i++){
+					Long myScaleId = templateItems[i].getItem().getScale().getId();
+					System.out.println("scale id["+i +"]="+ myScaleId.intValue());
+					if( ! myScaleId.equals(scaleId) ){
+						validScaleIds =false;
+						System.out.println("scale is not same");
+						break;
+					}
+				}			
+			}
 			
-			//render page
 			UIOutput.make(tofill, "modify-block-title", messageLocator.getMessage("modifyblock.page.title")); //$NON-NLS-1$ //$NON-NLS-2$
 			UIOutput.make(tofill, "create-eval-title", messageLocator.getMessage("createeval.page.title")); //$NON-NLS-1$ //$NON-NLS-2$
 			
 			UIInternalLink.make(tofill, "summary-toplink", messageLocator.getMessage("summary.page.title"),  //$NON-NLS-1$ //$NON-NLS-2$
-								new SimpleViewParameters(SummaryProducer.VIEW_ID));			
+								new SimpleViewParameters(SummaryProducer.VIEW_ID));		
 			
-			UIForm form = UIForm.make(tofill, "blockForm"); //$NON-NLS-1$
-			
-			UIOutput.make(form, "item-header", messageLocator.getMessage("modifyitem.item.header")); //$NON-NLS-1$ //$NON-NLS-2$
-			UIOutput.make(form,"itemNo","1."); //TODO:
-			UIOutput.make(form,"itemClassification",messageLocator.getMessage("modifytemplate.itemtype.block"));
-			UIOutput.make(form, "added-by", messageLocator.getMessage("modifyitem.added.by"));
-			UIOutput.make(form, "userInfo",templateItems[0].getOwner());
-			//TODO: remove link
-			
-			UIOutput.make(form, "item-header-text-header", messageLocator.getMessage("modifyblock.item.header.text.header"));
-			//UIInput.make(form, "item_text", "templateItemBeanLocator.new1."+"item.itemText", null);
-			
-		    UIOutput.make(form, "scale-type-header", messageLocator.getMessage("modifyblock.scale.type.header")); 
-			UIOutput.make(form, "scaleLabel",templateItems[0].getItem().getScale().getTitle());
-			
-			UIOutput.make(form,"add-na-header",messageLocator.getMessage("modifyitem.add.na.header")); //$NON-NLS-1$ //$NON-NLS-2$
-			//UIBoundBoolean.make(form, "item_NA",  "templateItemBeanLocator.new1."+ "usesNA", null); //$NON-NLS-1$ //$NON-NLS-2$
-			
-			UIOutput.make(form,"ideal-coloring-header",messageLocator.getMessage("modifyblock.ideal.coloring.header")); //$NON-NLS-1$ //$NON-NLS-2$
-			//UIBoundBoolean.make(form, "idealColor", "#{templateBBean.idealColor}",null); //$NON-NLS-1$ //$NON-NLS-2$
-			
-			UIOutput.make(form,"item-category-header",messageLocator.getMessage("modifyitem.item.category.header")); //$NON-NLS-1$ //$NON-NLS-2$
-			UIOutput.make(form,"course-category-header",messageLocator.getMessage("modifyitem.course.category.header")); //$NON-NLS-1$ //$NON-NLS-2$
-			UIOutput.make(form,"instructor-category-header",messageLocator.getMessage("modifyitem.instructor.category.header")); //$NON-NLS-1$ //$NON-NLS-2$
-			//Radio Buttons for "Item Category"
-			String[] courseCategoryList = 
-			{
-				messageLocator.getMessage("modifyitem.course.category.header"),
-				messageLocator.getMessage("modifyitem.instructor.category.header"),
-			};
-			
-			UISelect radios = null;			
-			if(templateItems.length ==1){
-				UIInput.make(form, "item_text", "templateItemBeanLocator."+templateItems[0].getId()+".item.itemText", null);
-					UIBoundBoolean.make(form, "item_NA",  "templateItemBeanLocator."+ templateItems[0].getId()+".usesNA", null); 
-				if(templateItems[0].getScaleDisplaySetting()!= null && 
-						templateItems[0].getScaleDisplaySetting().equals(EvalConstants.ITEM_SCALE_DISPLAY_STEPPED_COLORED))
-					UIBoundBoolean.make(form, "idealColor", "#{templateBBean.idealColor}",Boolean.TRUE);
-				else UIBoundBoolean.make(form, "idealColor", "#{templateBBean.idealColor}",null);
-				radios = UISelect.make(form, "item_category",
-			            EvaluationConstant.ITEM_CATEGORY_VALUES, courseCategoryList,
-			            "templateItemBeanLocator."+templateItems[0].getId()+".itemCategory", null);
-			}else{
-				UIInput.make(form, "item_text", "templateItemBeanLocator.new1."+"item.itemText", null);
-				UIBoundBoolean.make(form, "item_NA",  "templateItemBeanLocator.new1."+ "usesNA", null); 
-				UIBoundBoolean.make(form, "idealColor", "#{templateBBean.idealColor}",null); 
-				radios = UISelect.make(form, "item_category",
-			            EvaluationConstant.ITEM_CATEGORY_VALUES, courseCategoryList,
-			            "templateItemBeanLocator.new1."+"itemCategory", null);
-			}
-		
-
-			String selectID = radios.getFullID();
-			UISelectChoice.make(form, "item_category_C", selectID, 0); //$NON-NLS-1$
-			UISelectChoice.make(form, "item_category_I", selectID, 1);
-			
-			
-			if(templateItems.length ==1){//for modify existing block item
-				//get Block child item
-				EvalTemplate template = templateItems[0].getTemplate();
-				List l = itemsLogic.getTemplateItemsForTemplate(template.getId(),null);
-				List childList = ItemBlockUtils.getChildItmes(l,new Integer(templateItems[0].getId().intValue()));				
-				for(int i= 0; i< childList.size();i++){
-					EvalTemplateItem child = (EvalTemplateItem)childList.get(i);
-					UIBranchContainer radiobranch = UIBranchContainer.make(form,"queRow:",Integer.toString(i)); //$NON-NLS-1$
-					UIOutput.make(radiobranch, "childOrder",Integer.toString(i+1));
-					UIInput.make(radiobranch,"queText",null,child.getItem().getItemText() );	
-				} 				
-			}else{
-				//render child textfield form passed Ids for new Block Creation
-				for(int i= 0;i< templateItems.length; i++){
-					UIBranchContainer radiobranch = UIBranchContainer.make(form,"queRow:",Integer.toString(i)); //$NON-NLS-1$
-					UIOutput.make(radiobranch, "childOrder",Integer.toString(i+1));
-					UIInput.make(radiobranch,"queText",null,templateItems[i].getItem().getItemText() );		
+			if(! validScaleIds){
+				//show error page with back button
+				UIBranchContainer showError = UIBranchContainer.make(tofill, "errorPage:");
+				UIOutput.make(showError, "errorMsg",messageLocator.getMessage("modifyblock.error.message"));
+				UIOutput.make(showError, "back-button", messageLocator.getMessage("modifyblock.back.button"));
+				
+			}else{//render block page
+				
+				UIBranchContainer showBlock = UIBranchContainer.make(tofill, "blockPage:");
+				UIForm form = UIForm.make(showBlock, "blockForm"); //$NON-NLS-1$
+				
+				UIOutput.make(form, "item-header", messageLocator.getMessage("modifyitem.item.header")); //$NON-NLS-1$ //$NON-NLS-2$
+				UIOutput.make(form,"itemNo","1."); //TODO:
+				UIOutput.make(form,"itemClassification",messageLocator.getMessage("modifytemplate.itemtype.block"));
+				UIOutput.make(form, "added-by", messageLocator.getMessage("modifyitem.added.by"));
+				UIOutput.make(form, "userInfo",templateItems[0].getOwner());
+				//TODO: remove link
+				
+				UIOutput.make(form, "item-header-text-header", messageLocator.getMessage("modifyblock.item.header.text.header"));
+				//UIInput.make(form, "item_text", "templateItemBeanLocator.new1."+"item.itemText", null);
+				
+			    UIOutput.make(form, "scale-type-header", messageLocator.getMessage("modifyblock.scale.type.header")); 
+				UIOutput.make(form, "scaleLabel",templateItems[0].getItem().getScale().getTitle());
+				
+				UIOutput.make(form,"add-na-header",messageLocator.getMessage("modifyitem.add.na.header")); //$NON-NLS-1$ //$NON-NLS-2$
+				//UIBoundBoolean.make(form, "item_NA",  "templateItemBeanLocator.new1."+ "usesNA", null); //$NON-NLS-1$ //$NON-NLS-2$
+				
+				UIOutput.make(form,"ideal-coloring-header",messageLocator.getMessage("modifyblock.ideal.coloring.header")); //$NON-NLS-1$ //$NON-NLS-2$
+				//UIBoundBoolean.make(form, "idealColor", "#{templateBBean.idealColor}",null); //$NON-NLS-1$ //$NON-NLS-2$
+				
+				UIOutput.make(form,"item-category-header",messageLocator.getMessage("modifyitem.item.category.header")); //$NON-NLS-1$ //$NON-NLS-2$
+				UIOutput.make(form,"course-category-header",messageLocator.getMessage("modifyitem.course.category.header")); //$NON-NLS-1$ //$NON-NLS-2$
+				UIOutput.make(form,"instructor-category-header",messageLocator.getMessage("modifyitem.instructor.category.header")); //$NON-NLS-1$ //$NON-NLS-2$
+				//Radio Buttons for "Item Category"
+				String[] courseCategoryList = 
+				{
+					messageLocator.getMessage("modifyitem.course.category.header"),
+					messageLocator.getMessage("modifyitem.instructor.category.header"),
+				};
+				
+				UISelect radios = null;			
+				if(modify){//modify existing block
+					UIInput.make(form, "item_text", "templateItemBeanLocator."+templateItems[0].getId()+".item.itemText", null);
+						UIBoundBoolean.make(form, "item_NA",  "templateItemBeanLocator."+ templateItems[0].getId()+".usesNA", null); 
+					if(templateItems[0].getScaleDisplaySetting()!= null && 
+							templateItems[0].getScaleDisplaySetting().equals(EvalConstants.ITEM_SCALE_DISPLAY_STEPPED_COLORED))
+						UIBoundBoolean.make(form, "idealColor", "#{templateBBean.idealColor}",Boolean.TRUE);
+					else UIBoundBoolean.make(form, "idealColor", "#{templateBBean.idealColor}",null);
+					radios = UISelect.make(form, "item_category",
+				            EvaluationConstant.ITEM_CATEGORY_VALUES, courseCategoryList,
+				            "templateItemBeanLocator."+templateItems[0].getId()+".itemCategory", null);
+				}else{//create new block
+					UIInput.make(form, "item_text", "templateItemBeanLocator.new1."+"item.itemText", null);
+					UIBoundBoolean.make(form, "item_NA",  "templateItemBeanLocator.new1."+ "usesNA", null); 
+					UIBoundBoolean.make(form, "idealColor", "#{templateBBean.idealColor}",null); 
+					radios = UISelect.make(form, "item_category",
+				            EvaluationConstant.ITEM_CATEGORY_VALUES, courseCategoryList,
+				            "templateItemBeanLocator.new1."+"itemCategory", null);
 				}
-			}
 			
+
+				String selectID = radios.getFullID();
+				UISelectChoice.make(form, "item_category_C", selectID, 0); //$NON-NLS-1$
+				UISelectChoice.make(form, "item_category_I", selectID, 1);				
+				
+				if(modify){//for modify existing block item
+					//get Block child item
+					EvalTemplate template = templateItems[0].getTemplate();
+					List l = itemsLogic.getTemplateItemsForTemplate(template.getId(),null);
+					List childList = ItemBlockUtils.getChildItmes(l,new Integer(templateItems[0].getId().intValue()));				
+					for(int i= 0; i< childList.size();i++){
+						EvalTemplateItem child = (EvalTemplateItem)childList.get(i);
+						UIBranchContainer radiobranch = UIBranchContainer.make(form,"queRow:",Integer.toString(i)); //$NON-NLS-1$
+						UIOutput.make(radiobranch, "childOrder",Integer.toString(i+1));
+						UIInput.make(radiobranch,"queText",null,child.getItem().getItemText() );	
+					} 				
+				}else{
+					//render child textfield form passed Ids for new Block Creation
+					for(int i= 0;i< templateItems.length; i++){
+						UIBranchContainer radiobranch = UIBranchContainer.make(form,"queRow:",Integer.toString(i)); //$NON-NLS-1$
+						UIOutput.make(radiobranch, "childOrder",Integer.toString(i+1));
+						UIInput.make(radiobranch,"queText",null,templateItems[i].getItem().getItemText() );		
+					}
+				}
+				
+				UIOutput.make(form, "cancel-button", messageLocator.getMessage("general.cancel.button"));		
+				UICommand  saveCmd = UICommand.make(form, "saveBlockAction", messageLocator.getMessage("modifyitem.save.button"), 
+						"#{templateBBean.saveBlockItemAction}");
+				saveCmd.parameters.add(new UIELBinding("#{templateBBean.childTemplateItemIds}",
+						evParameters.templateItemIds));
+			} 
 			
-			UIOutput.make(form, "cancel-button", messageLocator.getMessage("general.cancel.button"));
-	
-			UICommand  saveCmd = UICommand.make(form, "saveBlockAction", messageLocator.getMessage("modifyitem.save.button"), 
-					"#{templateBBean.saveBlockItemAction}");
-			saveCmd.parameters.add(new UIELBinding("#{templateBBean.childTemplateItemIds}",
-					evParameters.templateItemIds));
+		
 			
 		}
 	}
--- tool/src/webapp/content/js/blockUtils.js
+++ tool/src/webapp/content/js/blockUtils.js
@@ -1,23 +1,29 @@
-//TODO:javacsript need to enforce at least 2 check box are checked
+//TODO:Javacsript can check if checked Ids hasv the same scale ID
 function extractSelectedItems(submitButton) {
 	sourceForm = document.getElementById("modifyFormRows");
 	destinationForm = submitButton.form;
-
+	var count = 0; //enforce at least 2 check box are checked
+	
 	if (sourceForm && destinationForm) {
 		var idList = "";
-
+		
 		// look for checkboxes which have IDs of the form "block-12-11" and keep track of the ones that are checked.
 		for (var a=0; a < sourceForm.elements.length; a++) {
 			if (sourceForm.elements[a].type=="checkbox" &&
 sourceForm.elements[a].checked) {
 				if (idList.length > 0) { idList += ","; }
 				idList += sourceForm.elements[a].id.substr(sourceForm.elements[a].id.lastIndexOf('-')+1);
+				count = count + 1;
 			}
 		}
 		destinationForm.templateItemIds.value=idList;
 	
 		alert("ids="+destinationForm.templateItemIds.value);
-		return true;
+		if(count < 2){
+		 alert("you must select at least 2 items to create a block");
+		 return false; 
+		}
+		else return true;
 	}
 
 	return false;
