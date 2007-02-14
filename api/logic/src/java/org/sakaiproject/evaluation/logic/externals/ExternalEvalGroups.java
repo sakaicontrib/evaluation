b0b46f3e194b28b34cb52d22e30c060efbd85112
Aaron Zeckoski <azeckoski@vt.edu>
2007-02-14 16:49:51 +0000

    EVALSYS-58 Adjustments to the interface based on rellis suggestions
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@3475 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- api/logic/src/java/org/sakaiproject/evaluation/logic/externals/ExternalEvalGroups.java
+++ api/logic/src/java/org/sakaiproject/evaluation/logic/externals/ExternalEvalGroups.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * ExternalCourses.java - created by aaronz@vt.edu
+ * ExternalEvalGroups.java - created by aaronz@vt.edu
  * 
  * Copyright (c) 2007 Virginia Polytechnic Institute and State University
  * Licensed under the Educational Community License version 1.0
@@ -21,71 +21,71 @@ import org.sakaiproject.evaluation.logic.model.Context;
 import org.sakaiproject.evaluation.model.constant.EvalConstants;
 
 /**
- * This interface provides methods to get external courses and enrollments into the
+ * This interface provides methods to get external eval groups and enrollments into the
  * evaluation system<br/>,
  * This interface can and should be implemented and then spring loaded to allow
- * an institution to bring in external courses without needing to load them all
+ * an institution to bring in external eval groups without needing to load them all
  * into Sakai or a similar system<br/>
- * <b>Note</b>: Specifically this allows us to reference courses and enrollments which
+ * <b>Note</b>: Specifically this allows us to reference eval groups and enrollments which
  * are not stored in something like Sakai
  * 
  * @author Aaron Zeckoski (aaronz@vt.edu)
  */
-public interface ExternalCourses {
+public interface ExternalEvalGroups {
 
 	/**
 	 * Get a list of all user ids that can take an evaluation or be evaluated in
 	 * course(s) identified by a unique identifier (context)
 	 * 
-	 * @param contexts courses identified by unique strings
+	 * @param contexts eval groups identified by unique strings
 	 * @param permission a permission string PERM constant from {@link EvalConstants},
 	 * <b>Note</b>: only take evaluation and be evaluated are supported
 	 * @return a Set of Strings which represent the user Ids of all users in the course with that permission
 	 */
-	public Set getUserIdsForCourses(String[] contexts, String permission);
+	public Set getUserIdsForEvalGroups(String[] contexts, String permission);
 
 	/**
 	 * Get a list of all user ids that can take an evaluation or be evaluated in
 	 * course(s) identified by a unique identifier (context),
 	 * <b>Note</b>: This is simply here for the sake of efficiency
 	 * 
-	 * @param contexts courses identified by unique strings
+	 * @param contexts eval groups identified by unique strings
 	 * @param permission a permission string PERM constant from {@link EvalConstants},
 	 * <b>Note</b>: only take evaluation and be evaluated are supported
 	 * @return the count of the users
 	 */
-	public int countUserIdsForCourses(String[] contexts, String permission);
+	public int countUserIdsForEvalGroups(String[] contexts, String permission);
 
 	/**
-	 * Get a list of all courses that a user can take an evaluation or be evaluated in,
-	 * courses are identified by a unique identifier (context)
+	 * Get a list of all eval groups that a user can take an evaluation or be evaluated in,
+	 * eval groups are identified by a unique identifier (context)
 	 * 
 	 * @param userId the internal user id (not username)
 	 * @param permission a permission string PERM constant from {@link EvalConstants},
 	 * <b>Note</b>: only take evaluation and be evaluated are supported
 	 * @return a List of {@link Context} objects
 	 */
-	public List getCoursesForUser(String userId, String permission);
+	public List getEvalGroupsForUser(String userId, String permission);
 
 	/**
-	 * Get a count of all courses that a user can take an evaluation or be evaluated in,
-	 * courses are identified by a unique identifier (context),
+	 * Get a count of all eval groups that a user can take an evaluation or be evaluated in,
+	 * eval groups are identified by a unique identifier (context),
 	 * <b>Note</b>: This is simply here for the sake of efficiency
 	 * 
 	 * @param userId the internal user id (not username)
 	 * @param permission a permission string PERM constant from {@link EvalConstants},
 	 * <b>Note</b>: only take evaluation and be evaluated are supported
-	 * @return the count of the courses
+	 * @return the count of the eval groups
 	 */
-	public int countCoursesForUser(String userId, String permission);
+	public int countEvalGroupsForUser(String userId, String permission);
 
 	/**
-	 * Return a Context object which represents a collection of users (a course 
-	 * in this case) based on the unique id (context string)
+	 * Return a Context object which represents a collection of users (a course or 
+	 * group or collection in this case) based on the unique id (context string)
 	 * 
 	 * @param context the unique id of an external course
 	 * @return a Context object or null if not found
 	 */
-	public Context getContextByCourseId(String context);
+	public Context getContextByGroupId(String context);
 
 }
