6dfa9f1887a5cae5d6c6532f29dc6cec7703f3fb
Aaron Zeckoski <azeckoski@vt.edu>
2007-02-17 13:21:03 +0000

    Moved provider API to a new package for clarity
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@3513 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- api/logic/src/java/org/sakaiproject/evaluation/logic/providers/ExternalEvalGroups.java
+++ api/logic/src/java/org/sakaiproject/evaluation/logic/providers/ExternalEvalGroups.java
@@ -12,7 +12,7 @@
  * 
  *****************************************************************************/
 
-package org.sakaiproject.evaluation.logic.externals;
+package org.sakaiproject.evaluation.logic.providers;
 
 import java.util.List;
 import java.util.Set;
