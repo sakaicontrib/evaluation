bc01a4416ec1def8905e2d076bebdb6bffa07eeb
Aaron Zeckoski <azeckoski@vt.edu>
2007-03-01 22:07:28 +0000

    EVALSYS-82 updated the provider so that there are no dependencies on anything outside it
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@3739 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- api/logic/src/java/org/sakaiproject/evaluation/logic/providers/EvalGroupsProvider.java
+++ api/logic/src/java/org/sakaiproject/evaluation/logic/providers/EvalGroupsProvider.java
@@ -18,7 +18,6 @@ import java.util.List;
 import java.util.Set;
 
 import org.sakaiproject.evaluation.logic.model.EvalGroup;
-import org.sakaiproject.evaluation.model.constant.EvalConstants;
 
 /**
  * This interface provides methods to get external eval groups and enrollments into the
@@ -35,8 +34,8 @@ import org.sakaiproject.evaluation.model.constant.EvalConstants;
  * </bean>
  * </xmp>
  * <br/>
- * The 2 permissions this provider has to deal with are {@link EvalConstants#PERM_BE_EVALUATED}
- * (roughly equivalent to instructor role) and {@link EvalConstants#PERM_TAKE_EVALUATION}
+ * The 2 permissions this provider has to deal with are {@link #PERM_BE_EVALUATED}
+ * (roughly equivalent to instructor role) and {@link #PERM_TAKE_EVALUATION}
  * (roughly equivalent to student role), compare the incoming permission to the constant
  * and only handle the cases indicated (do not try to handle all possible permissions)<br/>
  * <b>Note</b>: Specifically this allows us to reference eval groups and enrollments which
@@ -47,11 +46,20 @@ import org.sakaiproject.evaluation.model.constant.EvalConstants;
 public interface EvalGroupsProvider {
 
 	/**
+	 * Permission: User can be evaluated for any eval group they have this permission in
+	 */
+	public final static String PERM_BE_EVALUATED = "provider.by.evaluated";
+	/**
+	 * Permission: User can take an evaluation for any eval group they have this permission in
+	 */
+	public final static String PERM_TAKE_EVALUATION = "provider.take.evaluation";
+
+	/**
 	 * Get a list of all user ids that can take an evaluation or be evaluated in
 	 * eval group(s) or course(s) identified by a unique identifier (groupId)
 	 * 
 	 * @param groupIds eval groups identified by unique strings
-	 * @param permission a permission string PERM constant from {@link EvalConstants},
+	 * @param permission a permission string PERM constant (from this API),
 	 * <b>Note</b>: only take evaluation and be evaluated are supported
 	 * @return a Set of Strings which represent the user Ids of all users in the course with that permission
 	 */
@@ -63,7 +71,7 @@ public interface EvalGroupsProvider {
 	 * <b>Note</b>: This is simply here for the sake of efficiency
 	 * 
 	 * @param groupIds eval groups identified by unique strings
-	 * @param permission a permission string PERM constant from {@link EvalConstants},
+	 * @param permission a permission string PERM constant (from this API),
 	 * <b>Note</b>: only take evaluation and be evaluated are supported
 	 * @return the count of the users
 	 */
@@ -74,7 +82,7 @@ public interface EvalGroupsProvider {
 	 * eval groups are identified by a unique identifier (groupId)
 	 * 
 	 * @param userId the internal user id (not username)
-	 * @param permission a permission string PERM constant from {@link EvalConstants},
+	 * @param permission a permission string PERM constant (from this API),
 	 * <b>Note</b>: only take evaluation and be evaluated are supported
 	 * @return a List of {@link EvalGroup} objects representing eval groups
 	 */
@@ -86,7 +94,7 @@ public interface EvalGroupsProvider {
 	 * <b>Note</b>: This is simply here for the sake of efficiency
 	 * 
 	 * @param userId the internal user id (not username)
-	 * @param permission a permission string PERM constant from {@link EvalConstants},
+	 * @param permission a permission string PERM constant (from this API),
 	 * <b>Note</b>: only take evaluation and be evaluated are supported
 	 * @return the count of the eval groups
 	 */
@@ -105,7 +113,7 @@ public interface EvalGroupsProvider {
 	 * Check if a user has a specified permission/role within an eval group
 	 * 
 	 * @param userId the internal user id (not username)
-	 * @param permission a permission string PERM constant from {@link EvalConstants},
+	 * @param permission a permission string PERM constant (from this API),
 	 * <b>Note</b>: only take evaluation and be evaluated are supported
 	 * @param groupId the unique id of an external eval group
 	 * @return true if allowed, false otherwise
