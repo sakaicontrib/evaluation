ad64de8b5f5e3e291788506e463b4ba01a5fbc1b
Kapil Ahuja <kahuja@vt.edu>
2007-03-05 19:19:45 +0000

    EVALSYS 16 (Modify Scale): OTP stuff in place. Only need to check how to handle scale options.
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@3792 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ tool/src/java/org/sakaiproject/evaluation/tool/ScaleBean.java
@@ -0,0 +1,42 @@
+/******************************************************************************
+ * ScaleBean.java - created by kahuja@vt.edu on Mar 04, 2007
+ * 
+ * Copyright (c) 2007 Virginia Polytechnic Institute and State University
+ * Licensed under the Educational Community License version 1.0
+ * 
+ * A copy of the Educational Community License has been included in this 
+ * distribution and is available at: http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Contributors:
+ * Kapil Ahuja (kahuja@vt.edu)
+ *****************************************************************************/
+
+package org.sakaiproject.evaluation.tool;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.evaluation.model.EvalEvaluation;
+
+/**
+ * This request-scope bean handles scale creation and modification.
+ * 
+ * @author Kapil Ahuja (kahuja@vt.edu)
+ */
+public class ScaleBean {
+
+	private static Log log = LogFactory.getLog(ScaleBean.class);
+
+	public EvalEvaluation eval;
+	public String context;
+	
+	private ScaleBeanLocator scaleBeanLocator;
+	public void setScaleBeanLocator(ScaleBeanLocator scaleBeanLocator) {
+		this.scaleBeanLocator = scaleBeanLocator;
+	}
+
+	public String saveScale() {
+		log.debug("create scale");
+		scaleBeanLocator.saveAll();
+		return "success";
+	}
+}
---
+++ tool/src/java/org/sakaiproject/evaluation/tool/ScaleBeanLocator.java
@@ -0,0 +1,64 @@
+/******************************************************************************
+ * ScaleBeanLocator.java - created by kahuja@vt.edu on Mar 04, 2007
+ * 
+ * Copyright (c) 2007 Virginia Polytechnic Institute and State University
+ * Licensed under the Educational Community License version 1.0
+ * 
+ * A copy of the Educational Community License has been included in this 
+ * distribution and is available at: http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Contributors:
+ * Kapil Ahuja (kahuja@vt.edu)
+ *****************************************************************************/
+package org.sakaiproject.evaluation.tool;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.sakaiproject.evaluation.model.EvalScale;
+
+import uk.org.ponder.beanutil.BeanLocator;
+
+/**
+ * This is the OTP bean used to locate scales.
+ * 
+ * @author Kapil Ahuja (kahuja@vt.edu)
+ */
+
+public class ScaleBeanLocator implements BeanLocator {
+	public static final String NEW_PREFIX = "new";
+    public static String NEW_1 = NEW_PREFIX +"1";
+    
+    private LocalScaleLogic localScaleLogic;
+    public void setLocalScaleLogic(LocalScaleLogic localScaleLogic) {
+      this.localScaleLogic = localScaleLogic;
+    }
+	
+	private Map delivered = new HashMap();
+	
+	public Object locateBean(String path) {
+		Object togo=delivered.get(path);
+		if (togo == null){
+			if(path.startsWith(NEW_PREFIX)){
+				togo = localScaleLogic.newScale();
+			}
+            else { 
+				togo = localScaleLogic.fetchScale(new Long(Long.parseLong(path.trim())));
+			}
+			delivered.put(path, togo);
+		}
+		return togo;
+	}
+
+	public void saveAll() {
+      for (Iterator it = delivered.keySet().iterator(); it.hasNext();) {
+        String key = (String) it.next();
+        EvalScale scale = (EvalScale) delivered.get(key);
+        if (key.startsWith(NEW_PREFIX)) {
+        	// could do stuff here
+        }
+        localScaleLogic.saveScale(scale);
+      }
+    }
+}
