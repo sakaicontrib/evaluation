6c90d1825354f85c8ed077596dbdde49e27acb89
fengr@vt.edu <fengr@vt.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-03-05 21:23:08 +0000

    ModifyEssay page with proper Item Number
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@3798 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/producers/ModifyEssayProducer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/producers/ModifyEssayProducer.java
@@ -16,13 +16,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.sakaiproject.evaluation.logic.EvalExternalLogic;
+import org.sakaiproject.evaluation.logic.EvalItemsLogic;
 import org.sakaiproject.evaluation.logic.EvalSettings;
 import org.sakaiproject.evaluation.logic.EvalTemplatesLogic;
 import org.sakaiproject.evaluation.model.EvalTemplate;
+import org.sakaiproject.evaluation.model.EvalTemplateItem;
 import org.sakaiproject.evaluation.model.constant.EvalConstants;
 import org.sakaiproject.evaluation.tool.EvaluationConstant;
 import org.sakaiproject.evaluation.tool.params.EvalViewParameters;
 import org.sakaiproject.evaluation.tool.params.TemplateItemViewParameters;
+import org.sakaiproject.evaluation.tool.utils.TemplateItemUtils;
 
 import uk.org.ponder.rsf.components.ELReference;
 import uk.org.ponder.rsf.components.UIBoundBoolean;
@@ -65,7 +68,12 @@ public class ModifyEssayProducer implements ViewComponentProducer,
 	public void setTemplatesLogic(EvalTemplatesLogic templatesLogic) {
 		this.templatesLogic = templatesLogic;
 	}
-
+	
+	private EvalItemsLogic itemsLogic;
+	public void setItemsLogic(EvalItemsLogic itemsLogic) {
+		this.itemsLogic = itemsLogic;
+	}
+	
 	private EvalExternalLogic external;
 	public void setExternal(EvalExternalLogic external) {
 		this.external = external;
@@ -108,9 +116,19 @@ public class ModifyEssayProducer implements ViewComponentProducer,
 		UIForm form = UIForm.make(tofill, "essayForm"); //$NON-NLS-1$
 
 		UIMessage.make(form, "item-header", "modifyitem.item.header");
-
-		UIOutput.make(form, "itemNo", null, "1.");
-
+	
+		if(templateItemId != null){			
+			
+			EvalTemplateItem ti = itemsLogic.getTemplateItemById(templateItemId);
+			UIOutput.make(form, "itemNo",ti.getDisplayOrder().toString()); //$NON-NLS-1$ //$NON-NLS-2$
+			
+		}else{
+			List l = itemsLogic.getTemplateItemsForTemplate(templateId, external.getCurrentUserId());
+			List templateItemsList = TemplateItemUtils.getNonChildItems(l);			
+			Integer no = new Integer(templateItemsList.size()+1);
+			UIOutput.make(form, "itemNo",no.toString());
+		}
+		
 		UIMessage.make(form,"added-by", "modifyitem.added.by"); //$NON-NLS-1$ //$NON-NLS-2$
 		UIOutput.make(form, "itemClassification", EvalConstants.ITEM_TYPE_TEXT); //$NON-NLS-1$ //$NON-NLS-2$
 
--- tool/src/java/org/sakaiproject/evaluation/tool/producers/ModifyScaledProducer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/producers/ModifyScaledProducer.java
@@ -28,6 +28,7 @@ import org.sakaiproject.evaluation.tool.EvaluationConstant;
 import org.sakaiproject.evaluation.tool.ItemsBean;
 import org.sakaiproject.evaluation.tool.params.EvalViewParameters;
 import org.sakaiproject.evaluation.tool.params.TemplateItemViewParameters;
+import org.sakaiproject.evaluation.tool.utils.TemplateItemUtils;
 
 import uk.org.ponder.rsf.components.ELReference;
 import uk.org.ponder.rsf.components.UIBoundBoolean;
@@ -137,8 +138,19 @@ public class ModifyScaledProducer implements ViewComponentProducer,
 		UIForm form = UIForm.make(tofill, "itemForm"); //$NON-NLS-1$
 
 		UIMessage.make(form, "item-header", "modifyitem.item.header");
-		UIOutput.make(form, "itemNo", null, "1."); //$NON-NLS-1$ //$NON-NLS-2$
 
+		if(templateItemId != null){			
+			
+			EvalTemplateItem ti = itemsLogic.getTemplateItemById(templateItemId);
+			UIOutput.make(form, "itemNo",ti.getDisplayOrder().toString()); //$NON-NLS-1$ //$NON-NLS-2$
+			
+		}else{
+			List l = itemsLogic.getTemplateItemsForTemplate(templateId, external.getCurrentUserId());
+			List templateItemsList = TemplateItemUtils.getNonChildItems(l);			
+			Integer no = new Integer(templateItemsList.size()+1);
+			UIOutput.make(form, "itemNo",no.toString());
+		}
+		
 		UIOutput.make(form, "itemClassification", EvalConstants.ITEM_TYPE_SCALED);
 		UIMessage.make(form,
 				"added-by", "modifyitem.added.by"); //$NON-NLS-1$ //$NON-NLS-2$
