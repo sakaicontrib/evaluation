775f9309bb2d974ed4b23582bcab4075ead2042e
Aaron Zeckoski <azeckoski@vt.edu>
2007-03-09 19:53:57 +0000

    Expert items working, update to M6
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@4194 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/producers/ExpertObjectiveProducer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/producers/ExpertObjectiveProducer.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * ExpertCategoryProducer.java - created by aaronz on 8 Mar 2007
+ * ExpertObjectiveProducer.java - created by aaronz on 9 Mar 2007
  * 
  * Copyright (c) 2007 Centre for Academic Research in Educational Technologies
  * Licensed under the Educational Community License version 1.0
@@ -34,16 +34,16 @@ import uk.org.ponder.rsf.viewstate.ViewParameters;
 import uk.org.ponder.rsf.viewstate.ViewParamsReporter;
 
 /**
- * Handles the expert category view
+ * Handles the expert objectives view
  * 
  * @author Aaron Zeckoski (aaronz@vt.edu)
  */
-public class ExpertCategoryProducer implements ViewComponentProducer, ViewParamsReporter {
+public class ExpertObjectiveProducer implements ViewComponentProducer, ViewParamsReporter {
 
 	/**
 	 * Used for navigation within the system, this must match with the template name
 	 */
-	public static final String VIEW_ID = "choose_expert_category"; //$NON-NLS-1$
+	public static final String VIEW_ID = "choose_expert_objective"; //$NON-NLS-1$
 	public String getViewID() {
 		return VIEW_ID;
 	}
@@ -68,8 +68,9 @@ public class ExpertCategoryProducer implements ViewComponentProducer, ViewParams
 
 		ExpertItemViewParameters expertItemViewParameters = (ExpertItemViewParameters) viewparams;
 		Long templateId = expertItemViewParameters.templateId;
+		Long categoryId = expertItemViewParameters.categoryId;
 
-		UIMessage.make(tofill, "page-title", "expert.category.page.title"); //$NON-NLS-1$ //$NON-NLS-2$
+		UIMessage.make(tofill, "page-title", "expert.objective.page.title"); //$NON-NLS-1$ //$NON-NLS-2$
 
 		UIInternalLink.make(tofill, "summary-toplink", UIMessage.make("summary.page.title"), //$NON-NLS-1$ //$NON-NLS-2$
 				new SimpleViewParameters(SummaryProducer.VIEW_ID));
@@ -78,33 +79,38 @@ public class ExpertCategoryProducer implements ViewComponentProducer, ViewParams
 
 		// create a copy of the VP and then set it to the right view (to avoid corrupting the original)
 		ExpertItemViewParameters eivp = (ExpertItemViewParameters) expertItemViewParameters.copyBase();
+		eivp.viewID = ExpertCategoryProducer.VIEW_ID;
 
 		UIMessage.make(tofill, "expert-items", "expert.expert.items");
-		UIMessage.make(tofill, "expert-items-category", "expert.category");
+		UIInternalLink.make(tofill, "expert-items-category-link", UIMessage.make("expert.category"), eivp ); //$NON-NLS-1$ //$NON-NLS-2$
+		UIMessage.make(tofill, "expert-items-objective", "expert.objective");
 
-		UIMessage.make(tofill, "choose-category-1", "expert.choose.category");
+		UIInternalLink.make(tofill, "choose-category-1-link", UIMessage.make("expert.choose.category"), eivp ); //$NON-NLS-1$ //$NON-NLS-2$
 		UIMessage.make(tofill, "choose-objective-2", "expert.choose.objective");
 		UIMessage.make(tofill, "choose-items-3", "expert.choose.items");
 
 		UIMessage.make(tofill, "category", "expert.category");
-		UIMessage.make(tofill, "category-instructions", "expert.category.instructions");
+		EvalItemGroup category = expertItemsLogic.getItemGroupById(categoryId);
+		UIOutput.make(tofill, "category-current", category.getTitle() );
+		UIMessage.make(tofill, "objective", "expert.objective");
+		UIMessage.make(tofill, "objective-instructions", "expert.objective.instructions");
 
 		UIMessage.make(tofill, "description", "expert.description");
 
-		// set the VP to the correct target for objectives
-		eivp.viewID = ExpertObjectiveProducer.VIEW_ID;
+		// set the VP to the correct target for items
+		eivp.viewID = ExpertItemsProducer.VIEW_ID;
 
-		// loop through all non-empty expert categories
-		List expertCategories = expertItemsLogic.getItemGroups(null, currentUserId, false, true);
-		for (int i = 0; i < expertCategories.size(); i++) {
-			EvalItemGroup category = (EvalItemGroup) expertCategories.get(i);
-			UIBranchContainer categories = UIBranchContainer.make(tofill, "expert-category-list:", category.getId().toString());
-			eivp.categoryId = category.getId();
-			UIInternalLink.make(categories, "category-title-link", category.getTitle(), eivp); //$NON-NLS-1$
-			if (category.getDescription() != null && category.getDescription().length() > 0) {
-				UIOutput.make(categories, "category-description", category.getDescription()); //$NON-NLS-1$
+		// loop through all non-empty expert objectives
+		List expertObjectives = expertItemsLogic.getItemGroups(categoryId, currentUserId, false, true);
+		for (int i = 0; i < expertObjectives.size(); i++) {
+			EvalItemGroup objective = (EvalItemGroup) expertObjectives.get(i);
+			UIBranchContainer objectives = UIBranchContainer.make(tofill, "expert-objective-list:", objective.getId().toString());
+			eivp.objectiveId = objective.getId();
+			UIInternalLink.make(objectives, "objective-title-link", objective.getTitle(), eivp); //$NON-NLS-1$
+			if (objective.getDescription() != null && objective.getDescription().length() > 0) {
+				UIOutput.make(objectives, "objective-description", objective.getDescription()); //$NON-NLS-1$
 			} else {
-				UIMessage.make(categories, "category-no-description", "expert.no.description"); //$NON-NLS-1$
+				UIMessage.make(objectives, "objective-no-description", "expert.no.description"); //$NON-NLS-1$
 			}
 		}
 
