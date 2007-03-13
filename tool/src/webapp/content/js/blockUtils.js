dd345bc62b5ab1f4cc48cf405f68417a6ecba0bd
Wu Feng <fengr@vt.edu>
2007-03-13 16:58:10 +0000

    Intermediate resolve JIRA EVALSYS-87:The javascript should do the following: 1) Only enable the create block button once at least 2 boxes are checked
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@4233 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/blockUtils.js
+++ tool/src/webapp/content/js/blockUtils.js
@@ -1,4 +1,35 @@
 //TODO:Javacsript can check if checked Ids have the same scale ID
+
+
+function countCheckBox(){
+	//if total cout of checked checkbox > 2, enable "Create Block" button
+
+	sourceForm = document.modify_form_rows;
+	destinationForm = document.create_block_form;
+	submitButton = document.getElementById("createBlockBtn");
+	var count = 0; //enforce at least 2 check box are checked	
+	
+	if (sourceForm && destinationForm) {
+
+		// look for checkboxes which have IDs of the form "block-12-11" and keep track of the ones that are checked.
+		for (var a=0; a < sourceForm.elements.length; a++) {
+			if (sourceForm.elements[a].type=="checkbox" &&
+					sourceForm.elements[a].checked) {
+				count = count + 1;
+			}
+		}
+	}
+	
+	if(count < 2){ 
+		//alert("select less than 2 items,disable submit button");
+		submitButton.disabled = true;
+	}else{ 
+		//alert("select 2 or more items,enable submit button");
+		submitButton.disabled = false;
+	}
+	
+}//end of function
+
 function extractSelectedItems(submitButton) {
 	//sourceForm = document.getElementById("modifyFormRows");
 	sourceForm = document.modify_form_rows;
@@ -22,10 +53,11 @@ sourceForm.elements[a].checked) {
 		//alert("ids="+destinationForm.templateItemIds.value);
 		
 		if(count < 2){
-		 alert("you must select at least 2 items to create a block");
-		 return false; 
+		 	alert("you must select at least 2 items to create a block");
+		 	return false; 
+		}else {
+			return true;
 		}
-		else return true;
 	}
 
 	return false;
