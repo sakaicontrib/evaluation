a0a36ee0f8c56a54efafd53d5c4d4e39944fdfac
Aaron Zeckoski <azeckoski@vt.edu>
2007-06-07 12:16:15 +0000

    Updated new entity terminology
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@9907 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/inferrers/EvalCategoryVPInferrer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/inferrers/EvalCategoryVPInferrer.java
@@ -14,7 +14,7 @@
 
 package org.sakaiproject.evaluation.tool.inferrers;
 
-import org.sakaiproject.entitybroker.EntityID;
+import org.sakaiproject.entitybroker.EntityReference;
 import org.sakaiproject.evaluation.logic.entity.EvalCategoryEntityProvider;
 import org.sakaiproject.evaluation.tool.producers.ShowEvalCategoryProducer;
 import org.sakaiproject.evaluation.tool.viewparams.EvalCategoryViewParameters;
@@ -40,7 +40,7 @@ public class EvalCategoryVPInferrer implements EntityViewParamsInferrer {
 	 * @see uk.ac.cam.caret.sakai.rsf.entitybroker.EntityViewParamsInferrer#inferDefaultViewParameters(java.lang.String)
 	 */
 	public ViewParameters inferDefaultViewParameters(String reference) {
-		EntityID ep = new EntityID(reference);
+		EntityReference ep = new EntityReference(reference);
 		String category = ep.id;
 		// TODO - should this possibly authenticate first?
 		return new EvalCategoryViewParameters(ShowEvalCategoryProducer.VIEW_ID, category);
