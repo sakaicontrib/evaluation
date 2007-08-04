5c0e435fc12be16f8b0450d0d66e1120b73fe47d
Aaron Zeckoski <azeckoski@vt.edu>
2007-08-04 17:43:18 +0000

    EVALSYS-197 fixed email template and preview email template screens
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@10986 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/viewparams/EmailViewParameters.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/viewparams/EmailViewParameters.java
@@ -3,17 +3,19 @@
  */
 package org.sakaiproject.evaluation.tool.viewparams;
 
-
 public class EmailViewParameters extends TemplateViewParameters {
-  /** This takes on one of the two values 
-   * EvalConstants.EMAIL_TEMPLATE_AVAILABLE or 
-   * EvalConstants.EMAIL_TEMPLATE_REMINDER*/
-  public String emailType;
-  public EmailViewParameters() {}
-  public EmailViewParameters(String viewID, Long templateId,
-      String emailType) {
-    this.viewID = viewID;
-    this.templateId = templateId;
-    this.emailType = emailType;
-  }
+   /**
+    * This takes on one of the two values EvalConstants.EMAIL_TEMPLATE_AVAILABLE
+    * or EvalConstants.EMAIL_TEMPLATE_REMINDER
+    */
+   public String emailType;
+
+   public EmailViewParameters() {
+   }
+
+   public EmailViewParameters(String viewID, Long templateId, String emailType) {
+      this.viewID = viewID;
+      this.templateId = templateId;
+      this.emailType = emailType;
+   }
 }
