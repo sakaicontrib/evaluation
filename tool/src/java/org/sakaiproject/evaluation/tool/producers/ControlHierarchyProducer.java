06b8fc9975a968596470c62ebd34d75ce70554e3
Steven Githens <swgithen@mtu.edu>
2007-09-06 09:55:33 +0000

    EVALSYS-246
    -Added link for ControlHierarchy to Admin Breadcrumb
    -Stubbed out producers for ControlHierarchy
    -Dummy implementation of EvalHierarchyProvider
    
    
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@11754 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/producers/ControlHierarchyProducer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/producers/ControlHierarchyProducer.java
@@ -1,14 +1,25 @@
 package org.sakaiproject.evaluation.tool.producers;
 
-import org.sakaiproject.evaluation.logic.EvalExternalLogic;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
+import org.sakaiproject.evaluation.logic.EvalExternalLogic;
+import org.sakaiproject.evaluation.logic.providers.EvalHierarchyProvider;
+
+import uk.org.ponder.rsf.components.UIBranchContainer;
+import uk.org.ponder.rsf.components.UICommand;
 import uk.org.ponder.rsf.components.UIContainer;
+import uk.org.ponder.rsf.components.UIForm;
 import uk.org.ponder.rsf.components.UIInternalLink;
 import uk.org.ponder.rsf.components.UIMessage;
+import uk.org.ponder.rsf.components.UIOutput;
+import uk.org.ponder.rsf.components.decorators.UIFreeAttributeDecorator;
 import uk.org.ponder.rsf.view.ComponentChecker;
 import uk.org.ponder.rsf.view.ViewComponentProducer;
 import uk.org.ponder.rsf.viewstate.SimpleViewParameters;
 import uk.org.ponder.rsf.viewstate.ViewParameters;
+import org.sakaiproject.evaluation.logic.model.HierarchyNode;
 
 public class ControlHierarchyProducer implements ViewComponentProducer {
     public static final String VIEW_ID = "control_hierarchy";
@@ -20,6 +31,11 @@ public class ControlHierarchyProducer implements ViewComponentProducer {
     private EvalExternalLogic external;
     public void setExternal(EvalExternalLogic external) {
         this.external = external;
+    }
+    
+    private EvalHierarchyProvider hierarchyProvider;
+    public void setEvalHierarchyProvider(EvalHierarchyProvider provider) {
+        this.hierarchyProvider = provider;
     }
 
     public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker checker) {
@@ -38,6 +54,8 @@ public class ControlHierarchyProducer implements ViewComponentProducer {
         UIInternalLink.make(tofill, "administrate-toplink", UIMessage.make("administrate.page.title"), new SimpleViewParameters(AdministrateProducer.VIEW_ID));
         UIMessage.make(tofill, "page-title", "controlhierarchy.breadcrumb.title");
 
+        HierarchyNode root = hierarchyProvider.getRootLevelNode();
+        renderHierarchyNode(tofill, root);
         /*
          * Done Link at bottom of page.
          */
@@ -45,4 +63,29 @@ public class ControlHierarchyProducer implements ViewComponentProducer {
         
     }
 
+    public void renderHierarchyNode(UIContainer tofill, HierarchyNode node) {
+        System.out.println("Node: " + node.level + ", " + node.id + ", " + node.name);
+        
+        UIBranchContainer tableRow = UIBranchContainer.make(tofill, "hierarchy-level-row:");
+        UIOutput name = UIOutput.make(tableRow, "node-name", node.name);
+        Map attr = new HashMap();
+        attr.put("style", "text-indent:"+node.level+"em");
+        name.decorate(new UIFreeAttributeDecorator(attr));
+        UIInternalLink.make(tableRow, "add-child-link", new SimpleViewParameters(VIEW_ID));
+        UIInternalLink.make(tableRow, "modify-node-link", new SimpleViewParameters(VIEW_ID));
+        
+        Set<HierarchyNode> children = hierarchyProvider.getChildNodes(node.id);
+        
+        if (children.size() > 0) {
+            UIOutput.make(tableRow, "number-children", children.size()+"");
+        }
+        else {
+            UIForm removeForm = UIForm.make(tableRow, "remove-node-form");
+            UICommand.make(removeForm, "remove-node-button", "");
+        }
+        
+        for (HierarchyNode child: children) {
+            renderHierarchyNode(tofill, child);
+        }
+    }
 }
