c47a06f3731c453590e920aabc34cb8d23ffc309
Aaron Zeckoski <azeckoski@vt.edu>
2007-09-06 14:08:49 +0000

    Updated so that code will build again
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@11776 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/producers/ControlHierarchyProducer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/producers/ControlHierarchyProducer.java
@@ -1,3 +1,4 @@
+
 package org.sakaiproject.evaluation.tool.producers;
 
 import java.util.HashMap;
@@ -5,6 +6,8 @@ import java.util.Map;
 import java.util.Set;
 
 import org.sakaiproject.evaluation.logic.EvalExternalLogic;
+import org.sakaiproject.evaluation.logic.externals.ExternalHierarchyLogic;
+import org.sakaiproject.evaluation.logic.model.EvalHierarchyNode;
 import org.sakaiproject.evaluation.logic.providers.EvalHierarchyProvider;
 
 import uk.org.ponder.rsf.components.UIBranchContainer;
@@ -19,73 +22,75 @@ import uk.org.ponder.rsf.view.ComponentChecker;
 import uk.org.ponder.rsf.view.ViewComponentProducer;
 import uk.org.ponder.rsf.viewstate.SimpleViewParameters;
 import uk.org.ponder.rsf.viewstate.ViewParameters;
-import org.sakaiproject.evaluation.logic.model.HierarchyNode;
 
 public class ControlHierarchyProducer implements ViewComponentProducer {
-    public static final String VIEW_ID = "control_hierarchy";
 
-    public String getViewID() {
-        return VIEW_ID;
-    }
+   public static final String VIEW_ID = "control_hierarchy";
+   public String getViewID() {
+      return VIEW_ID;
+   }
 
-    private EvalExternalLogic external;
-    public void setExternal(EvalExternalLogic external) {
-        this.external = external;
-    }
-    
-    private EvalHierarchyProvider hierarchyProvider;
-    public void setEvalHierarchyProvider(EvalHierarchyProvider provider) {
-        this.hierarchyProvider = provider;
-    }
+   private EvalExternalLogic external;
+   public void setExternal(EvalExternalLogic external) {
+      this.external = external;
+   }
 
-    public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker checker) {
-        String currentUserId = external.getCurrentUserId();
-        boolean userAdmin = external.isUserAdmin(currentUserId);
+   private ExternalHierarchyLogic hierarchyLogic;
+   public void setHierarchyLogic(ExternalHierarchyLogic hierarchyLogic) {
+      this.hierarchyLogic = hierarchyLogic;
+   }
 
-        if (!userAdmin) {
-            // Security check and denial
-            throw new SecurityException("Non-admin users may not access this page");
-        }
-        
-        /*
-         * top menu links and bread crumbs here
-         */
-        UIInternalLink.make(tofill, "summary-toplink", UIMessage.make("summary.page.title"), new SimpleViewParameters(SummaryProducer.VIEW_ID));
-        UIInternalLink.make(tofill, "administrate-toplink", UIMessage.make("administrate.page.title"), new SimpleViewParameters(AdministrateProducer.VIEW_ID));
-        UIMessage.make(tofill, "page-title", "controlhierarchy.breadcrumb.title");
+   public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker checker) {
+      String currentUserId = external.getCurrentUserId();
+      boolean userAdmin = external.isUserAdmin(currentUserId);
 
-        HierarchyNode root = hierarchyProvider.getRootLevelNode();
-        renderHierarchyNode(tofill, root);
-        /*
-         * Done Link at bottom of page.
-         */
-        UIInternalLink.make(tofill, "done-link", UIMessage.make("controlhierarchy.done"), new SimpleViewParameters(AdministrateProducer.VIEW_ID));
-        
-    }
+      if (!userAdmin) {
+         // Security check and denial
+         throw new SecurityException("Non-admin users may not access this page");
+      }
 
-    public void renderHierarchyNode(UIContainer tofill, HierarchyNode node) {
-        System.out.println("Node: " + node.level + ", " + node.id + ", " + node.name);
-        
-        UIBranchContainer tableRow = UIBranchContainer.make(tofill, "hierarchy-level-row:");
-        UIOutput name = UIOutput.make(tableRow, "node-name", node.name);
-        Map attr = new HashMap();
-        attr.put("style", "text-indent:"+node.level+"em");
-        name.decorate(new UIFreeAttributeDecorator(attr));
-        UIInternalLink.make(tableRow, "add-child-link", new SimpleViewParameters(VIEW_ID));
-        UIInternalLink.make(tableRow, "modify-node-link", new SimpleViewParameters(VIEW_ID));
-        
-        Set<HierarchyNode> children = hierarchyProvider.getChildNodes(node.id);
-        
-        if (children.size() > 0) {
-            UIOutput.make(tableRow, "number-children", children.size()+"");
-        }
-        else {
-            UIForm removeForm = UIForm.make(tableRow, "remove-node-form");
-            UICommand.make(removeForm, "remove-node-button", "");
-        }
-        
-        for (HierarchyNode child: children) {
-            renderHierarchyNode(tofill, child);
-        }
-    }
+      /*
+       * top menu links and bread crumbs here
+       */
+      UIInternalLink.make(tofill, "summary-toplink", UIMessage.make("summary.page.title"),
+            new SimpleViewParameters(SummaryProducer.VIEW_ID));
+      UIInternalLink.make(tofill, "administrate-toplink", UIMessage.make("administrate.page.title"),
+            new SimpleViewParameters(AdministrateProducer.VIEW_ID));
+      UIMessage.make(tofill, "page-title", "controlhierarchy.breadcrumb.title");
+
+      EvalHierarchyNode root = hierarchyLogic.getRootLevelNode();
+      renderHierarchyNode(tofill, root);
+      /*
+       * Done Link at bottom of page.
+       */
+      UIInternalLink.make(tofill, "done-link", UIMessage.make("controlhierarchy.done"),
+            new SimpleViewParameters(AdministrateProducer.VIEW_ID));
+
+   }
+
+   public void renderHierarchyNode(UIContainer tofill, EvalHierarchyNode node) {
+      System.out.println("Node: " + node);
+
+      UIBranchContainer tableRow = UIBranchContainer.make(tofill, "hierarchy-level-row:");
+      UIOutput name = UIOutput.make(tableRow, "node-name", node.title);
+      // TODO - put this back in somehow? -AZ
+//      Map attr = new HashMap();
+//      attr.put("style", "text-indent:" + node.level + "em");
+//      name.decorate(new UIFreeAttributeDecorator(attr));
+      UIInternalLink.make(tableRow, "add-child-link", new SimpleViewParameters(VIEW_ID));
+      UIInternalLink.make(tableRow, "modify-node-link", new SimpleViewParameters(VIEW_ID));
+
+      Set<EvalHierarchyNode> children = hierarchyLogic.getChildNodes(node.id, true);
+
+      if (children.size() > 0) {
+         UIOutput.make(tableRow, "number-children", children.size() + "");
+      } else {
+         UIForm removeForm = UIForm.make(tableRow, "remove-node-form");
+         UICommand.make(removeForm, "remove-node-button", "");
+      }
+
+      for (EvalHierarchyNode child : children) {
+         renderHierarchyNode(tofill, child);
+      }
+   }
 }
