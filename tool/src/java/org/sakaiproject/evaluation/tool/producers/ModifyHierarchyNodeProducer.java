5689beaf15c58a934443765b63c0ddf3e26f8923
Steven Githens <swgithen@mtu.edu>
2007-09-06 09:15:45 +0000

    EVALSYS-246
    -Added link for ControlHierarchy to Admin Breadcrumb
    -Stubbed out producers for ControlHierarchy
    -Dummy implementation of EvalHierarchyProvider
    
    
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@11753 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ tool/src/java/org/sakaiproject/evaluation/tool/producers/ModifyHierarchyNodeProducer.java
@@ -0,0 +1,20 @@
+package org.sakaiproject.evaluation.tool.producers;
+
+import uk.org.ponder.rsf.components.UIContainer;
+import uk.org.ponder.rsf.view.ComponentChecker;
+import uk.org.ponder.rsf.view.ViewComponentProducer;
+import uk.org.ponder.rsf.viewstate.ViewParameters;
+
+public class ModifyHierarchyNodeProducer implements ViewComponentProducer {
+    public static final String VIEW_ID = "modify_hierarchy_node";
+
+    public String getViewID() {
+        return VIEW_ID;
+    }
+
+    public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker checker) {
+        
+        
+    }
+
+}
