a92db06d323b2dcdf544f55f4cc85fb1663770e0
Steven Githens <swgithen@mtu.edu>
2007-09-07 06:24:33 +0000

    EVALSYS-246
    - Updated for new Hiearchy API's and Service
    - Adding in Add / Modify Screen
    
    
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@11815 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/producers/ModifyHierarchyNodeProducer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/producers/ModifyHierarchyNodeProducer.java
@@ -1,20 +1,57 @@
 package org.sakaiproject.evaluation.tool.producers;
 
+import org.sakaiproject.evaluation.logic.EvalExternalLogic;
+import org.sakaiproject.evaluation.logic.externals.ExternalHierarchyLogic;
+import org.sakaiproject.evaluation.logic.providers.EvalHierarchyProvider;
+import org.sakaiproject.evaluation.tool.viewparams.ModifyHierarchyNodeParameters;
 import uk.org.ponder.rsf.components.UIContainer;
+import uk.org.ponder.rsf.components.UIInternalLink;
+import uk.org.ponder.rsf.components.UIMessage;
 import uk.org.ponder.rsf.view.ComponentChecker;
 import uk.org.ponder.rsf.view.ViewComponentProducer;
+import uk.org.ponder.rsf.viewstate.SimpleViewParameters;
 import uk.org.ponder.rsf.viewstate.ViewParameters;
+import uk.org.ponder.rsf.viewstate.ViewParamsReporter;
 
-public class ModifyHierarchyNodeProducer implements ViewComponentProducer {
+public class ModifyHierarchyNodeProducer implements ViewComponentProducer, ViewParamsReporter {
     public static final String VIEW_ID = "modify_hierarchy_node";
 
     public String getViewID() {
         return VIEW_ID;
     }
+    
+    private EvalExternalLogic external;
+    public void setExternal(EvalExternalLogic external) {
+       this.external = external;
+    }
+
+    private ExternalHierarchyLogic hierarchyLogic;
+    public void setHierarchyLogic(ExternalHierarchyLogic hierarchyLogic) {
+       this.hierarchyLogic = hierarchyLogic;
+    }
 
     public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker checker) {
+        String currentUserId = external.getCurrentUserId();
+        boolean userAdmin = external.isUserAdmin(currentUserId);
+
+        if (!userAdmin) {
+            // Security check and denial
+            throw new SecurityException("Non-admin users may not access this page");
+        }
         
+        /*
+         * top menu links and bread crumbs here
+         */
+        UIInternalLink.make(tofill, "summary-toplink", UIMessage.make("summary.page.title"), new SimpleViewParameters(SummaryProducer.VIEW_ID));
+        UIInternalLink.make(tofill, "administrate-toplink", UIMessage.make("administrate.page.title"), new SimpleViewParameters(AdministrateProducer.VIEW_ID));
+        UIInternalLink.make(tofill, "hierarchy-toplink", UIMessage.make("controlhierarchy.breadcrumb.title"), new SimpleViewParameters(ControlHierarchyProducer.VIEW_ID));
+        UIMessage.make(tofill, "page-title", "modifyhierarchynode.breadcrumb.title");
         
+        //EvalHierarchyNode toEdit 
+    }
+
+    public ViewParameters getViewParameters() {
+        return new ModifyHierarchyNodeParameters();
     }
 
 }
---
+++ tool/src/java/org/sakaiproject/evaluation/tool/viewparams/ModifyHierarchyNodeParameters.java
@@ -0,0 +1,21 @@
+package org.sakaiproject.evaluation.tool.viewparams;
+
+import uk.org.ponder.rsf.viewstate.SimpleViewParameters;
+
+public class ModifyHierarchyNodeParameters extends SimpleViewParameters {
+    public String nodeId;
+    
+    /* If adding child is true, we are adding a subnode to this node,
+     * rather than modifying the node itself.
+     */
+    public Boolean addingChild;
+    
+    public ModifyHierarchyNodeParameters() {
+    }
+    
+    public ModifyHierarchyNodeParameters(String viewID, String nodeId, boolean addingchild) {
+        this.viewID = viewID;
+        this.nodeId = nodeId;
+        this.addingChild = addingchild;
+    }
+}
