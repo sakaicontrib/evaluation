a7ee7b9fdb226e6cf77d44c3ac4222865521da2e
Aaron Zeckoski <azeckoski@vt.edu>
2008-01-25 16:03:35 +0000

    http://jira.sakaiproject.org/jira/browse/EVALSYS-358 Fixed problem with saving strings into settings when the current value is null, should be able to remove the fix when RSF 0.7.3 is released
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@45009 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/locators/SettingsWBL.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/locators/SettingsWBL.java
@@ -20,7 +20,6 @@ import org.sakaiproject.evaluation.tool.EvaluationConstant;
 
 import uk.org.ponder.beanutil.WriteableBeanLocator;
 import uk.org.ponder.conversion.StaticLeafParser;
-import uk.org.ponder.util.UniversalRuntimeException;
 
 /**
  * Obviates the need for a backing bean for administrative functionality 
@@ -32,127 +31,114 @@ import uk.org.ponder.util.UniversalRuntimeException;
 public class SettingsWBL implements WriteableBeanLocator {
    public static final String NEW_PREFIX = "new";
    public static String NEW_1 = NEW_PREFIX +"1";
-	
-	// Spring injection 
-	private EvalSettings evalSettings;
-	public void setEvalSettings(EvalSettings evalSettings) {
-		this.evalSettings = evalSettings;
-	}
-	
-	// Spring injection 
-	private StaticLeafParser leafParser;
-	public void setLeafParser(StaticLeafParser leafParser) {
-		this.leafParser = leafParser;
-	}
-	
-	/**
-	 * Simply tells the user that remove functionality is not 
-	 * supported (this is done by throwing an exception).
-	 * 
-	 * @param beanname -  Name of the property that has to be removed    
-	 * @return throws a new UnsupportedOperationException exception
-	 */
-	public boolean remove(String beanname) {
-		throw new UnsupportedOperationException("Removal not supported from SettingsWBL");
-	}
-	  
-	/**
-	 * Sets the data from producer to EvalSettings (database). 
-	 * 
-	 * @param beanname -  Name of the property to be set    
-	 * @param toset -  Value of the property to be set    
-	 */
-	public void set(String beanname, Object toset) {
-	  
-		/*
-		 * Fields inside isFieldToBeParsed are not directly mapped to those in database.
-		 * Thus parsing them.  
-		 *       
-		 * Note: If the value of field is null it means that this particular value should be
-		 * deleted from the table. 
-		 */ 
-		if ( isFieldToBeParsed(beanname) ) {
-			if ( ((String)toset).equals(EvaluationConstant.ADMIN_BOOLEAN_CONFIGURABLE) ) 
-				toset = null;
-			else if ( ((String)toset).equals(EvaluationConstant.ADMIN_BOOLEAN_YES) ) 
-				toset = Boolean.TRUE;
-			else if ( ((String)toset).equals(EvaluationConstant.ADMIN_BOOLEAN_NO) ) 
-				toset = Boolean.FALSE;
-			else {
-				//do nothing
-			}
-		}
-		else {
-			/*
-			 *  The UI has already converted Booleans.
-			 *  This is primarily to catch Integers and Strings. 
-			 */
-			if (toset instanceof String) {
-				Class<?> proptype = getPropertyType(beanname);
-				toset = leafParser.parse(proptype, (String) toset);
-			}
-		}
-		evalSettings.set(beanname, toset);
-	}
 
-	/**
-	 * Gets the data from EvalSettings (database) and returns to producer. 
-	 * 
-	 * @param path -  Name of the property whose value has to be fetched 
-	 * 				  from database
-	 * @return Value of the property obtained from database
-	 */
-	public Object locateBean(String path) {
-		  
-		Object toget = evalSettings.get(path);
-		
-		/*
-		 * Fields inside isFieldToBeParsed are not directly mapped to those in database.
-		 * Thus parsing them.  
-		 */
-		if ( isFieldToBeParsed(path) ) {
-			if (toget == null)
-				toget = EvaluationConstant.ADMIN_BOOLEAN_CONFIGURABLE;
-			else if (toget instanceof Boolean) {
-				if ( ((Boolean)toget).booleanValue() )
-					toget = EvaluationConstant.ADMIN_BOOLEAN_YES;
-				else 
-					toget = EvaluationConstant.ADMIN_BOOLEAN_NO;
-			}
-			else {
-				//do nothing
-			}
-		}
-		return toget;
-	}
-	
-	/**
-	 * Uses {@link Class} to find the class for a given property. 
-	 */
-	private static Class<?> getPropertyType(String propname) {
-		String typename = SettingsLogicUtils.getType(propname);
-		try {
-			return Class.forName(typename);
-		}
-		catch (Exception e) {
-			throw UniversalRuntimeException.accumulate(e, "Could not look up " + typename + " to a class");
-		}
-	}
-	
-	/**
-	 * Removing duplication of code in "get" and "set" methods 
-	 * by moving "if" block in this method. 
-	 */
-	private boolean isFieldToBeParsed (String path) {
+   // Spring injection 
+   private EvalSettings evalSettings;
+   public void setEvalSettings(EvalSettings evalSettings) {
+      this.evalSettings = evalSettings;
+   }
 
-		if ( path.equals(EvalSettings.INSTRUCTOR_ALLOWED_VIEW_RESULTS) ||
-				path.equals(EvalSettings.INSTRUCTOR_MUST_USE_EVALS_FROM_ABOVE) ||
-				path.equals(EvalSettings.STUDENT_ALLOWED_LEAVE_UNANSWERED) ||
-				path.equals(EvalSettings.STUDENT_MODIFY_RESPONSES) ||
-				path.equals(EvalSettings.STUDENT_VIEW_RESULTS) ||
-				path.equals(EvalSettings.ITEM_USE_COURSE_CATEGORY_ONLY) ) 
-			return true;
-		else
-			return false;
-	}
+   // Spring injection 
+   private StaticLeafParser leafParser;
+   public void setLeafParser(StaticLeafParser leafParser) {
+      this.leafParser = leafParser;
+   }
+
+   /**
+    * Simply tells the user that remove functionality is not 
+    * supported (this is done by throwing an exception).
+    * 
+    * @param beanname -  Name of the property that has to be removed    
+    * @return throws a new UnsupportedOperationException exception
+    */
+   public boolean remove(String beanname) {
+      throw new UnsupportedOperationException("Removal not supported from SettingsWBL");
+   }
+
+   /**
+    * Sets the data from producer to EvalSettings (database). 
+    * 
+    * @param beanname -  Name of the property to be set    
+    * @param toset -  Value of the property to be set    
+    */
+   public void set(String beanname, Object toset) {
+      /*
+       * Note: If the value of field is null it means that this particular value should be
+       * deleted from the table. 
+       */ 
+      if ( isTernaryBoolean(beanname) ) {
+         if ( ((String)toset).equals(EvaluationConstant.ADMIN_BOOLEAN_CONFIGURABLE) ) 
+            toset = null;
+         else if ( ((String)toset).equals(EvaluationConstant.ADMIN_BOOLEAN_YES) ) 
+            toset = Boolean.TRUE;
+         else if ( ((String)toset).equals(EvaluationConstant.ADMIN_BOOLEAN_NO) ) 
+            toset = Boolean.FALSE;
+         else {
+            throw new IllegalStateException("Invalid value for this ternary boolean: " + toset);
+         }
+      } else {
+         /*
+          *  The UI has already converted Booleans.
+          *  This is primarily to catch Integers and Strings. 
+          */
+         if (toset instanceof String) {
+            Class<?> proptype = SettingsLogicUtils.getTypeClass(beanname);
+            toset = leafParser.parse(proptype, (String) toset);
+         }
+      }
+      evalSettings.set(beanname, toset);
+   }
+
+   /**
+    * Gets the data from EvalSettings (database) and returns to producer. 
+    * 
+    * @param path -  Name of the property whose value has to be fetched 
+    * 				  from database
+    * @return Value of the property obtained from database
+    */
+   public Object locateBean(String path) {
+      Object toget = evalSettings.get(path);
+      /*
+       * Fields inside isFieldToBeParsed are not directly mapped to those in database.
+       * Thus parsing them.  
+       */
+      if ( isTernaryBoolean(path) ) {
+         if (toget == null)
+            toget = EvaluationConstant.ADMIN_BOOLEAN_CONFIGURABLE;
+         else if (toget instanceof Boolean) {
+            if ( ((Boolean)toget).booleanValue() ) {
+               toget = EvaluationConstant.ADMIN_BOOLEAN_YES;
+            } else { 
+               toget = EvaluationConstant.ADMIN_BOOLEAN_NO;
+            }
+         } else {
+            throw new IllegalStateException("Invalid value for this ternary boolean: " + toget);
+         }
+      }
+
+      if (toget == null) {
+         // this workaround is here for the DARApplier issue of handling nulls, should be fixed in 0.7.3
+         toget = "";
+      }
+      return toget;
+   }
+
+   /**
+    * Checks to see if a field is a ternary Boolean, that is to say
+    * it can store true, false, and "configurable"
+    * @param path
+    * @return true is this is ternary
+    */
+   private boolean isTernaryBoolean(String path) {
+      if ( path.equals(EvalSettings.INSTRUCTOR_ALLOWED_VIEW_RESULTS) ||
+            path.equals(EvalSettings.INSTRUCTOR_MUST_USE_EVALS_FROM_ABOVE) ||
+            path.equals(EvalSettings.STUDENT_ALLOWED_LEAVE_UNANSWERED) ||
+            path.equals(EvalSettings.STUDENT_MODIFY_RESPONSES) ||
+            path.equals(EvalSettings.STUDENT_VIEW_RESULTS) ||
+            path.equals(EvalSettings.ITEM_USE_COURSE_CATEGORY_ONLY) ) {
+         return true;
+      }
+      return false;
+   }
+
 }
