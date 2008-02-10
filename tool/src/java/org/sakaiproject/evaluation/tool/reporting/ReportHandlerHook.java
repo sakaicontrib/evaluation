b91c81ccdbba5afffeacfa6e0ca4599ef9e8d6c7
Steven Githens <swgithen@mtu.edu>
2008-02-10 10:29:49 +0000

    Sort of EVALSYS-365 Moved the complicated code to create a grid of Evaluation Responses to it's own utility class so it wouldn't muck up the handler hook anymore.  Changed all the response formatters to use this object so it will be easier and less expansive to generate all the report formats when we move to a process where they are automatically generated once when an eval closes.
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@45420 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/reporting/ReportHandlerHook.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/reporting/ReportHandlerHook.java
@@ -35,10 +35,14 @@ import org.sakaiproject.evaluation.model.EvalItem;
 import org.sakaiproject.evaluation.model.EvalTemplate;
 import org.sakaiproject.evaluation.model.EvalTemplateItem;
 import org.sakaiproject.evaluation.model.constant.EvalConstants;
+import org.sakaiproject.evaluation.tool.utils.EvalAggregatedResponses;
+import org.sakaiproject.evaluation.tool.utils.EvalResponseAggregatorUtil;
 import org.sakaiproject.evaluation.tool.viewparams.CSVReportViewParams;
 import org.sakaiproject.evaluation.tool.viewparams.DownloadReportViewParams;
 import org.sakaiproject.evaluation.tool.viewparams.ExcelReportViewParams;
 import org.sakaiproject.evaluation.tool.viewparams.PDFReportViewParams;
+
+import com.sun.org.apache.xerces.internal.impl.dv.DVFactoryException;
 
 import uk.org.ponder.rsf.processor.HandlerHook;
 import uk.org.ponder.rsf.viewstate.ViewParameters;
@@ -66,11 +70,6 @@ public class ReportHandlerHook implements HandlerHook {
       this.evaluationService = evaluationService;
    }
 
-   private EvalDeliveryService deliveryService;
-   public void setDeliveryService(EvalDeliveryService deliveryService) {
-      this.deliveryService = deliveryService;
-   }
-
    private CSVReportExporter csvReportExporter;
    public void setCsvReportExporter(CSVReportExporter csvReportExporter) {
       this.csvReportExporter = csvReportExporter;
@@ -94,6 +93,11 @@ public class ReportHandlerHook implements HandlerHook {
    private HttpServletResponse response;
    public void setResponse(HttpServletResponse response) {
       this.response = response;
+   }
+   
+   private EvalResponseAggregatorUtil responseAggregatorUtil; 
+   public void setEvalResponseAggregatorUtil(EvalResponseAggregatorUtil bean) {
+      this.responseAggregatorUtil = bean;
    }
 
    /* (non-Javadoc)
@@ -121,99 +125,7 @@ public class ReportHandlerHook implements HandlerHook {
          throw new SecurityException("Invalid user attempting to access report downloads: " + currentUserId);
       }
 
-      List<String> topRow = new ArrayList<String>(); //holds top row (item text)
-      List<EvalItem> allEvalItems = new ArrayList<EvalItem>(); //holds all expanded eval items (blocks are expanded here)
-      List<EvalTemplateItem> allEvalTemplateItems = new ArrayList<EvalTemplateItem>(); 
-      List<List<String>> responseRows = new ArrayList<List<String>>();//holds response rows
-
-      /*
-       * Getting list of response ids serves 2 purposes:
-       * 
-       * a) Main purpose: We need to check in the for loop at line 171
-       * that which student (i.e. which response) has not submitted
-       * the answer for a particular question. This is so that we can 
-       * add empty space instead
-       * 
-       * b) Side purpose: countResponses method in EvalDeliveryService
-       * does not take array of groups ids
-       */
-      List<Long> responseIds = evaluationService.getResponseIds(drvp.evalId, drvp.groupIds, true);
-      int numOfResponses = responseIds.size();
-
-      //add a row for each response
-      for (int i = 0; i < numOfResponses; i++) {
-         List<String> currResponseRow = new ArrayList<String>();
-         responseRows.add(currResponseRow);
-      }
-
-      //get all items
-      List<EvalTemplateItem> allItems = new ArrayList<EvalTemplateItem>(template.getTemplateItems());
-
-      if (!allItems.isEmpty()) {
-         //filter out the block child items, to get a list non-child items
-         List<EvalTemplateItem> ncItemsList = TemplateItemUtils.getNonChildItems(allItems);
-         Collections.sort(ncItemsList, new ComparatorsUtils.TemplateItemComparatorByOrder());
-         //for each item
-         for (int i = 0; i < ncItemsList.size(); i++) {
-            //fetch the item
-            EvalTemplateItem tempItem1 = (EvalTemplateItem) ncItemsList.get(i);
-            allEvalTemplateItems.add(tempItem1);
-            EvalItem item1 = tempItem1.getItem();
-
-            //if the item is normal scaled or text (essay)
-            if (TemplateItemUtils.getTemplateItemType(tempItem1).equals(EvalConstants.ITEM_TYPE_SCALED)
-                  || TemplateItemUtils.getTemplateItemType(tempItem1).equals(EvalConstants.ITEM_TYPE_TEXT)
-                  || TemplateItemUtils.getTemplateItemType(tempItem1).equals(EvalConstants.ITEM_TYPE_MULTIPLEANSWER)
-                  || TemplateItemUtils.getTemplateItemType(tempItem1).equals(EvalConstants.ITEM_TYPE_MULTIPLECHOICE)) {
-
-               //add the item description to the top row
-               // This is rich text, each particular output format can decide if it needs to be flattened.
-               topRow.add(item1.getItemText());
-               allEvalItems.add(item1);
-
-               //get all answers to this item within this evaluation
-               List<EvalAnswer> itemAnswers = deliveryService.getEvalAnswers(item1.getId(), drvp.evalId, drvp.groupIds);
-               updateResponseList(numOfResponses, responseIds, responseRows, itemAnswers, tempItem1, item1);
-
-            }
-            // block parent type (block child handled inside this)
-            else if (TemplateItemUtils.getTemplateItemType(tempItem1).equals(EvalConstants.ITEM_TYPE_BLOCK_PARENT)) {
-               //add the block description to the top row
-               topRow.add(item1.getItemText());
-               allEvalItems.add(item1);
-               for (int j = 0; j < numOfResponses; j++) {
-                  List<String> currRow = responseRows.get(j);
-                  //add blank response to block parent row
-                  currRow.add("");
-               }
-
-               //get child block items
-               List<EvalTemplateItem> childList = TemplateItemUtils.getChildItems(allItems, tempItem1.getId());
-               for (int j = 0; j < childList.size(); j++) {
-                  EvalTemplateItem tempItemChild = (EvalTemplateItem) childList.get(j);
-                  allEvalTemplateItems.add(tempItemChild);
-                  EvalItem child = tempItemChild.getItem();
-                  //add child's text to top row
-                  topRow.add(child.getItemText());
-                  allEvalItems.add(child);
-                  //get all answers to the child item within this eval
-                  List<EvalAnswer> itemAnswers = deliveryService.getEvalAnswers(child.getId(), drvp.evalId, drvp.groupIds);
-                  updateResponseList(numOfResponses, responseIds, responseRows, itemAnswers, tempItemChild, child);
-               }
-            }
-            // for block child 
-            else if (TemplateItemUtils.getTemplateItemType(tempItem1).equals(EvalConstants.ITEM_TYPE_BLOCK_CHILD)) {
-               // do nothing as they are already handled inside block parent
-            }
-            // for header type items
-            else if (TemplateItemUtils.getTemplateItemType(tempItem1).equals(EvalConstants.ITEM_TYPE_HEADER)) {
-               // DO nothing for header type
-            } else {
-               // SHOULD NOT GET HERE UNLESS WE HAVE an unhandled type which is an error
-               throw new UniversalRuntimeException("Unknown type, cannot do csv export: " + tempItem1.getItem().getClassification());
-            }
-         }
-      }
+      
 
       OutputStream resultsOutputStream = null;
       try {
@@ -226,96 +138,24 @@ public class ReportHandlerHook implements HandlerHook {
       // Response Headers that are the same for all Output types
       response.setHeader("Content-disposition", "inline");
       
+      EvalAggregatedResponses responses = responseAggregatorUtil.getAggregatedResponses(evaluation, drvp.groupIds);
+      
       if (drvp instanceof CSVReportViewParams) {
          response.setContentType("text/x-csv");
          response.setHeader("filename", "report.csv");
-         csvReportExporter.respondWithCSV(topRow, responseRows, numOfResponses, 
-               resultsOutputStream);
+         csvReportExporter.formatResponses(responses, resultsOutputStream);
       }
       else if (drvp instanceof ExcelReportViewParams) {
          response.setContentType("application/vnd.ms-excel");
          response.setHeader("filename", "report.xls");
-         xlsReportExporter.respondWithExcel(evaluation, template, allEvalItems, 
-               allEvalTemplateItems, topRow, responseRows, numOfResponses, drvp.groupIds,
-               resultsOutputStream);
+         xlsReportExporter.formatResponses(responses, resultsOutputStream);
       }
       else if (drvp instanceof PDFReportViewParams) {
          response.setContentType("application/pdf");
          response.setHeader("filename", "report.pdf");
-         pdfReportExporter.respondWithPDF(evaluation, template, allEvalItems, 
-               allEvalTemplateItems, topRow, responseRows, numOfResponses, drvp.groupIds,
-               resultsOutputStream);
+         pdfReportExporter.formatResponses(responses, resultsOutputStream);
       }
       return true;
-   }
-
-   /**
-    * This method iterates through list of answers for the concerened question 
-    * and updates the list of responses.
-    * 
-    * @param numOfResponses number of responses for the concerned evaluation
-    * @param responseIds list of response ids
-    * @param responseRows list containing all responses (i.e. list of answers for each question)
-    * @param itemAnswers list of answers for the concerened question
-    * @param tempItem1 EvalTemplateItem object for which the answers are fetched
-    * @param item1 EvalItem object for which the answers are fetched
-    */
-   private void updateResponseList(int numOfResponses, List<Long> responseIds, List<List<String>> responseRows, List<EvalAnswer> itemAnswers,
-         EvalTemplateItem tempItem1, EvalItem item1) {
-
-      /* 
-       * Fix for EVALSYS-123 i.e. export CSV functionality 
-       * fails when answer for a question left unanswered by 
-       * student.
-       * 
-       * Basically we need to check if the particular student 
-       * (identified by a response id) has answered a particular
-       * question. If yes, then add the answer to the list, else
-       * add empty string - kahuja 23rd Apr 2007. 
-       */
-      int actualIndexOfResponse = 0;
-      int idealIndexOfResponse = 0;
-      List<String> currRow = null;
-      int lengthOfAnswers = itemAnswers.size();
-      for (int j = 0; j < lengthOfAnswers; j++) {
-
-         EvalAnswer currAnswer = (EvalAnswer) itemAnswers.get(j);
-         actualIndexOfResponse = responseIds.indexOf(currAnswer.getResponse().getId());
-
-         // Fill empty answers if the answer corresponding to a response is not in itemAnswers list. 
-         if (actualIndexOfResponse > idealIndexOfResponse) {
-            for (int count = idealIndexOfResponse; count < actualIndexOfResponse; count++) {
-               currRow = responseRows.get(idealIndexOfResponse);
-               currRow.add(" ");
-            }
-         }
-
-         /*
-          * Add the answer to item within the current response to the output row.
-          * If text/essay type item just add the text 
-          * else (scaled type or block child, which is also scaled) item then look up the label
-          */
-         currRow = responseRows.get(actualIndexOfResponse);
-         if (TemplateItemUtils.getTemplateItemType(tempItem1).equals(EvalConstants.ITEM_TYPE_TEXT)) {
-            currRow.add(currAnswer.getText());
-         } else {
-            String labels[] = item1.getScale().getOptions();
-            currRow.add(labels[currAnswer.getNumeric().intValue()]);
-         }
-
-         /*
-          * Update the ideal index to "actual index + 1" 
-          * because now actual answer has been added to list.
-          */
-         idealIndexOfResponse = actualIndexOfResponse + 1;
-      }
-
-      // If empty answers occurs at end such that all responses have not been filled.
-      for (int count = idealIndexOfResponse; count < numOfResponses; count++) {
-         currRow = responseRows.get(idealIndexOfResponse);
-         currRow.add(" ");
-      }
-
    }
 
 }
