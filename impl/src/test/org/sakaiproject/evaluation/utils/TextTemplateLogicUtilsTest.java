4c69c95b466c8ebd13cbcc731e9e8a32dc5da520
Aaron Zeckoski <azeckoski@vt.edu>
2008-02-25 10:28:40 +0000

    http://jira.sakaiproject.org/jira/browse/EVALSYS-424 Added check to ensure the type is set correctly Moved all tests to the same package instead of splitting them up among the API and IMPLs Setup the evaluationBean to actually pay attention to the system settings
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@45930 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- impl/src/test/org/sakaiproject/evaluation/utils/TextTemplateLogicUtilsTest.java
+++ impl/src/test/org/sakaiproject/evaluation/utils/TextTemplateLogicUtilsTest.java
@@ -12,7 +12,7 @@
  * Aaron Zeckoski (azeckoski@gmail.com) (aaronz@vt.edu) (aaron@caret.cam.ac.uk)
  */
 
-package org.sakaiproject.evaluation.logic;
+package org.sakaiproject.evaluation.utils;
 
 import java.util.HashMap;
 import java.util.Map;
