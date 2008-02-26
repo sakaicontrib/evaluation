370376a2a00783c75be61eda05825e9d97a7e164
Steven Githens <swgithen@mtu.edu>
2008-02-26 17:54:49 +0000

    NOJIRA Cleaned up ViewParamsInterceptor definition.
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@45981 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/reporting/ReportViewParamsInterceptor.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/reporting/ReportViewParamsInterceptor.java
@@ -1,8 +1,5 @@
 package org.sakaiproject.evaluation.tool.reporting;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.sakaiproject.evaluation.tool.producers.ReportChooseGroupsProducer;
 import org.sakaiproject.evaluation.tool.producers.ReportsViewingProducer;
 import org.sakaiproject.evaluation.tool.viewparams.ReportParameters;
@@ -11,33 +8,24 @@ import uk.org.ponder.rsf.viewstate.AnyViewParameters;
 import uk.org.ponder.rsf.viewstate.RedirectViewParameters;
 import uk.org.ponder.rsf.viewstate.ViewParameters;
 import uk.org.ponder.rsf.viewstate.ViewParamsInterceptor;
-import uk.org.ponder.rsf.viewstate.support.ViewParamsInterceptorManager;
 
+/**
+ * This interceptor checks for incoming requests to the Choose Groups for Report
+ * viewing page. If there is less than 2 groups to choose from, it redirects you
+ * straight to the report view.
+ * 
+ * @author sgithens
+ */
 public class ReportViewParamsInterceptor implements ViewParamsInterceptor {
 
    private CurrentViewableReports curViewableReports;
    public void setCurrentViewableReports(CurrentViewableReports cur) {
       this.curViewableReports = cur;
    }
-   
-   private ViewParamsInterceptorManager viewParamsInterceptorManager;
-   public void setViewParamsInterceptorManager(ViewParamsInterceptorManager manager) {
-      this.viewParamsInterceptorManager = manager;
-   }
-   
-   /* FIXME TODO sgithens 2008-02-24 5:06PM Central Time
-    * This injecting of the VPIM is a temporary thing because I'm in a coffee 
-    * shop and don't know what the appropriate parent definition to use is.
-    */
-   public void init() {
-      List togo = new ArrayList();
-      togo.add(this);
-      viewParamsInterceptorManager.setInterceptors(togo);
-   }
 
    public AnyViewParameters adjustViewParameters(ViewParameters incoming) {
       AnyViewParameters togo = incoming;
-      
+
       if (ReportChooseGroupsProducer.VIEW_ID.equals(incoming.viewID)) {
          ReportParameters params = (ReportParameters) incoming;
          curViewableReports.populate(params.evaluationId);
