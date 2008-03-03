9972e2b80297b8bf2d7246971ee1b008cdcbc7f1
Steven Githens <swgithen@mtu.edu>
2008-03-03 02:36:11 +0000

    NOJIRA Refactoring Report View. Removed the seperate view for viewing all essays and combined it with the regular view. Minus ~200 lines of code.  Still some work to do, since we're still gathering the requirements for different options for viewing.
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@46174 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/viewparams/ReportParameters.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/viewparams/ReportParameters.java
@@ -21,28 +21,26 @@ import uk.org.ponder.rsf.viewstate.SimpleViewParameters;
  * For going staightforward from choose report groups to view reports page, the report bean takes care of it. 
  * 
  * @author Aaron Zeckoski (aaronz@vt.edu)
+ * @author Steven Githens
  */
 public class ReportParameters extends SimpleViewParameters {
 
-	// Removed extends TemplateViewParameters
-	// this should never have extended the TemplateViewParameters since it is checked on by an interceptor,
-	// furthermore, this used templateId when it was actually passing an evaluation id which is just wrong,
-	// removed former authors for doing this so incorrectly -AZ
+   public Long evaluationId;
+   public String[] groupIds;
+   public String viewmode;
+   public Long[] items;
 
-	public Long evaluationId;
-	public String[] groupIds;
+   public ReportParameters() {}
 
-	public ReportParameters() {}
+   public ReportParameters(String viewID, Long evaluationId){
+      this.viewID = viewID;
+      this.evaluationId = evaluationId;
+   }
 
-	public ReportParameters(String viewID, Long evaluationId){
-		this.viewID = viewID;
-		this.evaluationId = evaluationId;
-	}
-
-	public ReportParameters(String viewID, Long evaluationId, String[] groupIds) {
-		this.viewID = viewID;
-		this.evaluationId = evaluationId;
-		this.groupIds = groupIds;
-	}
+   public ReportParameters(String viewID, Long evaluationId, String[] groupIds) {
+      this.viewID = viewID;
+      this.evaluationId = evaluationId;
+      this.groupIds = groupIds;
+   }
 
 }
