8712aace7c9ad41a8a032e760d8d136c0f93a2ae
Steven Githens <swgithen@mtu.edu>
2008-03-19 17:06:10 +0000

    NOJIRA A central place to keep the list of tables and sql for eval table destruction.
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@46920 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ removeEvalTablesMySQL.sql
@@ -0,0 +1,28 @@
+-- This is the SQL for use on MySQL to clear the Sakai Evaluation tables from
+-- the database.
+--
+-- WARNING: This will delete all your evaluation data permanently! It should only
+--          be used for development purposes.
+--
+set FOREIGN_KEY_CHECKS=0;
+drop table EVAL_ANSWER;
+drop table EVAL_ASSIGN_GROUP;
+drop table EVAL_ASSIGN_HIERARCHY;
+drop table EVAL_CONFIG;
+drop table EVAL_EMAIL_TEMPLATE;
+drop table EVAL_EVALUATION;
+drop table EVAL_GROUPNODES;
+drop table EVAL_GROUPNODES_GROUPS;
+drop table EVAL_ITEM;
+drop table EVAL_ITEMGROUP;
+drop table EVAL_RESPONSE;
+drop table EVAL_SCALE;
+drop table EVAL_SCALE_OPTIONS;
+drop table EVAL_TEMPLATE;
+drop table EVAL_TEMPLATEITEM;
+drop table EVAL_ADHOC_EVALUATEES;
+drop table EVAL_ADHOC_GROUP;
+drop table EVAL_ADHOC_PARTICIPANTS;
+drop table EVAL_ADHOC_USER;
+drop table EVAL_LOCK;
+set FOREIGN_KEY_CHECKS=1;
