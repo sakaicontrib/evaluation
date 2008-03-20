5c26ad5469c5bf7d2147e961bc764e3584e1698d
Aaron Zeckoski <azeckoski@vt.edu>
2008-03-20 20:59:34 +0000

    Now for a real fix this time.... stupid long casting
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@47025 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- api/src/java/org/sakaiproject/evaluation/utils/EvalUtils.java
+++ api/src/java/org/sakaiproject/evaluation/utils/EvalUtils.java
@@ -313,7 +313,8 @@ public class EvalUtils {
     */
    public static int getHoursDifference(Date date1, Date date2) {
       long millisecondsDifference = date2.getTime() - date1.getTime();
-      return (int) (millisecondsDifference / (60*60*1000));
+      long difference = millisecondsDifference / (60l * 60l * 1000l);
+      return (int) difference;
    }
 
 
