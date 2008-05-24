6240913f5717d24ba2d76e7e8c0cfd2ab042f393
Aaron Zeckoski <azeckoski@vt.edu>
2008-05-24 17:59:04 +0000

    Committed aspect example for profiling
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@49417 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ impl/src/java/org/sakaiproject/evaluation/aspectj/ProfileSummary.java
@@ -0,0 +1,34 @@
+/**
+ * $Id$
+ * $URL$
+ * ProfileSummary.java - evaluation - May 24, 2008 6:32:06 PM - azeckoski
+ **************************************************************************
+ * Copyright (c) 2008 Aaron Zeckoski
+ * Licensed under the Apache License, Version 2.0
+ * 
+ * A copy of the Apache License has been included in this 
+ * distribution and is available at: http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ * Aaron Zeckoski (azeckoski @ gmail.com) (aaronz @ vt.edu) (aaron @ caret.cam.ac.uk)
+ */
+
+package org.sakaiproject.evaluation.aspectj;
+
+/**
+ * This is just a nice easy way to force a profile summary output
+ * 
+ * @author Aaron Zeckoski (azeckoski @ gmail.com)
+ */
+public class ProfileSummary {
+
+   /**
+    * Get the profiling summary and log it
+    * @return the summary from the profiling aspect
+    */
+   public static String generateSummary() {
+      // does nothing at all
+      String summary = "Aspect should have populated this";
+      return summary;
+   }
+
+}
