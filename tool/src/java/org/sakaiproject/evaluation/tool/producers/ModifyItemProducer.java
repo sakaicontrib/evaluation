1396f797010648a905deef6868cb24ecfbdf85dc
Andrew Thornton <art27@cantab.net>
2008-08-26 10:18:30 +0000

    CTL-1241: Change the cancel buttons into links which direct back up the breadcrumbs, thus removing the crazy messages stuff
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@52408 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/producers/ModifyItemProducer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/producers/ModifyItemProducer.java
@@ -276,10 +276,12 @@ public class ModifyItemProducer implements ViewComponentProducer, ViewParamsRepo
           UIInternalLink.make(tofill, "modify-template-items",
                UIMessage.make("modifytemplate.page.title"), 
                new TemplateViewParameters(ModifyTemplateItemsProducer.VIEW_ID, templateId));
+          UIInternalLink.make(tofill, "cancel-button", UIMessage.make("general.cancel.button"), new TemplateViewParameters(ModifyTemplateItemsProducer.VIEW_ID, templateId));
       } else {
           UIInternalLink.make(tofill, "control-items-breadcrumb",
                   UIMessage.make("controlitems.page.title"), 
                   new SimpleViewParameters(ControlItemsProducer.VIEW_ID));
+          UIInternalLink.make(tofill, "cancel-button", UIMessage.make("general.cancel.button"), new SimpleViewParameters(ControlItemsProducer.VIEW_ID));
       }
       UIMessage.make(tofill, "item-header", "modifyitem.item.header");
 
@@ -507,7 +509,7 @@ public class ModifyItemProducer implements ViewComponentProducer, ViewParamsRepo
          }
       }
 
-      UIMessage.make(form, "cancel-button", "general.cancel.button");
+      
 
       String saveBinding = null;
       if (templateId == null) {
