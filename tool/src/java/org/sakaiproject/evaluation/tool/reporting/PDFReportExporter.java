23351aa7b3469cf84b9c75e429eca05745da161a
Aaron Zeckoski <azeckoski@vt.edu>
2008-11-07 18:01:53 +0000

    http://www.caret.cam.ac.uk/jira/browse/CTL-1504 Initial commit, calcs are wrong still
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@54716 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/reporting/PDFReportExporter.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/reporting/PDFReportExporter.java
@@ -20,6 +20,8 @@ import org.sakaiproject.evaluation.model.EvalEvaluation;
 import org.sakaiproject.evaluation.model.EvalItem;
 import org.sakaiproject.evaluation.model.EvalTemplateItem;
 import org.sakaiproject.evaluation.tool.utils.EvalResponseAggregatorUtil;
+import org.sakaiproject.evaluation.tool.utils.RenderingUtils;
+import org.sakaiproject.evaluation.tool.utils.RenderingUtils.AnswersMean;
 import org.sakaiproject.evaluation.utils.EvalUtils;
 import org.sakaiproject.evaluation.utils.TemplateItemDataList;
 import org.sakaiproject.evaluation.utils.TemplateItemUtils;
@@ -30,203 +32,204 @@ import org.sakaiproject.evaluation.utils.TemplateItemDataList.TemplateItemGroup;
 import uk.org.ponder.messageutil.MessageLocator;
 
 public class PDFReportExporter implements ReportExporter {
-   private static Log log = LogFactory.getLog(PDFReportExporter.class);
+    private static Log log = LogFactory.getLog(PDFReportExporter.class);
 
-   int displayNumber;
+    int displayNumber;
 
-   private EvalCommonLogic commonLogic;
-   public void setCommonLogic(EvalCommonLogic commonLogic) {
-      this.commonLogic = commonLogic;
-   }
+    private EvalCommonLogic commonLogic;
+    public void setCommonLogic(EvalCommonLogic commonLogic) {
+        this.commonLogic = commonLogic;
+    }
 
-   private EvalEvaluationService evaluationService;
-   public void setEvaluationService(EvalEvaluationService evaluationService) {
-      this.evaluationService = evaluationService;
-   }
+    private EvalEvaluationService evaluationService;
+    public void setEvaluationService(EvalEvaluationService evaluationService) {
+        this.evaluationService = evaluationService;
+    }
 
-   private EvalSettings evalSettings;
-   public void setEvalSettings(EvalSettings evalSettings) {
-      this.evalSettings = evalSettings;
-   }
+    private EvalSettings evalSettings;
+    public void setEvalSettings(EvalSettings evalSettings) {
+        this.evalSettings = evalSettings;
+    }
 
-   private EvalDeliveryService deliveryService;
-   public void setDeliveryService(EvalDeliveryService deliveryService) {
-      this.deliveryService = deliveryService;
-   }
+    private EvalDeliveryService deliveryService;
+    public void setDeliveryService(EvalDeliveryService deliveryService) {
+        this.deliveryService = deliveryService;
+    }
 
-   private EvalResponseAggregatorUtil responseAggregator;
-   public void setEvalResponseAggregatorUtil(EvalResponseAggregatorUtil bean) {
-      this.responseAggregator = bean;
-   }
+    private EvalResponseAggregatorUtil responseAggregator;
+    public void setEvalResponseAggregatorUtil(EvalResponseAggregatorUtil bean) {
+        this.responseAggregator = bean;
+    }
 
-   private MessageLocator messageLocator;
-   public void setMessageLocator(MessageLocator locator) {
-      this.messageLocator = locator;
-   }
+    private MessageLocator messageLocator;
+    public void setMessageLocator(MessageLocator locator) {
+        this.messageLocator = locator;
+    }
 
-   /* (non-Javadoc)
-    * @see org.sakaiproject.evaluation.tool.reporting.ReportExporter#buildReport(org.sakaiproject.evaluation.model.EvalEvaluation, java.lang.String[], java.io.OutputStream)
-    */
-   public void buildReport(EvalEvaluation evaluation, String[] groupIds, OutputStream outputStream) {
+    /* (non-Javadoc)
+     * @see org.sakaiproject.evaluation.tool.reporting.ReportExporter#buildReport(org.sakaiproject.evaluation.model.EvalEvaluation, java.lang.String[], java.io.OutputStream)
+     */
+    public void buildReport(EvalEvaluation evaluation, String[] groupIds, OutputStream outputStream) {
 
-      EvalPDFReportBuilder evalPDFReportBuilder = new EvalPDFReportBuilder(outputStream);
+        EvalPDFReportBuilder evalPDFReportBuilder = new EvalPDFReportBuilder(outputStream);
 
-      Boolean useBannerImage = (Boolean) evalSettings.get(EvalSettings.ENABLE_PDF_REPORT_BANNER);
-      byte[] bannerImageBytes = null;
-      if (useBannerImage != null && useBannerImage == true) {
-         String bannerImageLocation = (String) evalSettings.get(EvalSettings.PDF_BANNER_IMAGE_LOCATION);
-         if (bannerImageLocation != null) {
-            bannerImageBytes = commonLogic.getFileContent(bannerImageLocation);
-         }
-      }
-
-      EvalUser user = commonLogic.getEvalUserById( commonLogic.getCurrentUserId() );
-
-      DateFormat df = DateFormat.getDateInstance(DateFormat.LONG);
-
-      // calculate the response rate
-      int responsesCount = deliveryService.countResponses(evaluation.getId(), null, true);
-      int enrollmentsCount = evaluationService.countParticipantsForEval(evaluation.getId());
-
-      String groupNames = responseAggregator.getCommaSeparatedGroupNames(groupIds);
-      
-      evalPDFReportBuilder.addTitlePage(evaluation.getTitle(), 
-            groupNames, 
-            messageLocator.getMessage("reporting.pdf.startdatetime",df.format(evaluation.getStartDate())),
-            messageLocator.getMessage("reporting.pdf.enddatetime",df.format(evaluation.getDueDate())),
-            messageLocator.getMessage("reporting.pdf.replyrate", 
-                  new String[] { EvalUtils.makeResponseRateStringFromCounts(responsesCount, enrollmentsCount) }),
-                  bannerImageBytes, messageLocator.getMessage("reporting.pdf.defaultsystemname"));
-
-      // set title and instructions
-      evalPDFReportBuilder.addIntroduction( evaluation.getTitle(), 
-            commonLogic.makePlainTextFromHTML(evaluation.getInstructions()) );
-
-      // Reset question numbering
-      displayNumber = 0;
-      
-      // 1 Make TIDL
-      TemplateItemDataList tidl = responseAggregator.prepareTemplateItemDataStructure(evaluation, groupIds);
-
-      // 1.5 get instructor info
-      Set<String> instructorIds = tidl.getAssociateIds(EvalConstants.ITEM_CATEGORY_INSTRUCTOR);
-      Map<String, EvalUser> instructorIdtoEvalUser = responseAggregator.getInstructorsInformation(instructorIds);
-
-      // Loop through the major group types: Course Questions, Instructor Questions, etc.
-      int renderedItemCount = 0;
-      for (TemplateItemGroup tig: tidl.getTemplateItemGroups()) {
-         // Print the type of the next group we're doing
-         if (EvalConstants.ITEM_CATEGORY_COURSE.equals(tig.associateType)) {
-            evalPDFReportBuilder.addSectionHeader(
-                  messageLocator.getMessage("viewreport.itemlist.course") );
-         }
-         else if (EvalConstants.ITEM_CATEGORY_INSTRUCTOR.equals(tig.associateType)) {
-            evalPDFReportBuilder.addSectionHeader(
-                  messageLocator.getMessage("viewreport.itemlist.instructor", 
-                        new String[] {instructorIdtoEvalUser.get(tig.associateId).displayName}));
-         } else {
-            evalPDFReportBuilder.addSectionHeader( messageLocator.getMessage("unknown.caps") );
-         }
-
-         for (HierarchyNodeGroup hng: tig.hierarchyNodeGroups) {
-            // Render the Node title if it's enabled in the admin settings.
-            if (hng.node != null) {
-               // Showing the section title is system configurable via the administrate view
-               Boolean showHierSectionTitle = (Boolean) evalSettings.get(EvalSettings.DISPLAY_HIERARCHY_HEADERS);
-               if (showHierSectionTitle) {
-                  evalPDFReportBuilder.addSectionHeader(hng.node.title);
-               }
+        Boolean useBannerImage = (Boolean) evalSettings.get(EvalSettings.ENABLE_PDF_REPORT_BANNER);
+        byte[] bannerImageBytes = null;
+        if (useBannerImage != null && useBannerImage == true) {
+            String bannerImageLocation = (String) evalSettings.get(EvalSettings.PDF_BANNER_IMAGE_LOCATION);
+            if (bannerImageLocation != null) {
+                bannerImageBytes = commonLogic.getFileContent(bannerImageLocation);
             }
+        }
 
-            List<DataTemplateItem> dtis = hng.getDataTemplateItems(true); // include block children
-            for (int i = 0; i < dtis.size(); i++) {
-               DataTemplateItem dti = dtis.get(i);
-               renderDataTemplateItem(evalPDFReportBuilder, dti);
-               renderedItemCount++;
+        EvalUser user = commonLogic.getEvalUserById( commonLogic.getCurrentUserId() );
+
+        DateFormat df = DateFormat.getDateInstance(DateFormat.LONG);
+
+        // calculate the response rate
+        int responsesCount = deliveryService.countResponses(evaluation.getId(), null, true);
+        int enrollmentsCount = evaluationService.countParticipantsForEval(evaluation.getId());
+
+        String groupNames = responseAggregator.getCommaSeparatedGroupNames(groupIds);
+
+        evalPDFReportBuilder.addTitlePage(evaluation.getTitle(), 
+                groupNames, 
+                messageLocator.getMessage("reporting.pdf.startdatetime",df.format(evaluation.getStartDate())),
+                messageLocator.getMessage("reporting.pdf.enddatetime",df.format(evaluation.getDueDate())),
+                messageLocator.getMessage("reporting.pdf.replyrate", 
+                        new String[] { EvalUtils.makeResponseRateStringFromCounts(responsesCount, enrollmentsCount) }),
+                        bannerImageBytes, messageLocator.getMessage("reporting.pdf.defaultsystemname"));
+
+        // set title and instructions
+        evalPDFReportBuilder.addIntroduction( evaluation.getTitle(), 
+                commonLogic.makePlainTextFromHTML(evaluation.getInstructions()) );
+
+        // Reset question numbering
+        displayNumber = 0;
+
+        // 1 Make TIDL
+        TemplateItemDataList tidl = responseAggregator.prepareTemplateItemDataStructure(evaluation, groupIds);
+
+        // 1.5 get instructor info
+        Set<String> instructorIds = tidl.getAssociateIds(EvalConstants.ITEM_CATEGORY_INSTRUCTOR);
+        Map<String, EvalUser> instructorIdtoEvalUser = responseAggregator.getInstructorsInformation(instructorIds);
+
+        // Loop through the major group types: Course Questions, Instructor Questions, etc.
+        int renderedItemCount = 0;
+        for (TemplateItemGroup tig: tidl.getTemplateItemGroups()) {
+            // Print the type of the next group we're doing
+            if (EvalConstants.ITEM_CATEGORY_COURSE.equals(tig.associateType)) {
+                evalPDFReportBuilder.addSectionHeader(
+                        messageLocator.getMessage("viewreport.itemlist.course") );
             }
-         }
-      }
-
-      evalPDFReportBuilder.close();
-   }
-
-   /**
-    * Renders a single question giving the DataTemplateItem.
-    * 
-    * @param evalPDFReportBuilder
-    * @param dti
-    */
-   private void renderDataTemplateItem(EvalPDFReportBuilder evalPDFReportBuilder, DataTemplateItem dti) {
-      EvalTemplateItem templateItem = dti.templateItem;
-      EvalItem item = templateItem.getItem();
-      String questionText = commonLogic.makePlainTextFromHTML(item.getItemText());
-
-      List<EvalAnswer> itemAnswers = dti.getAnswers();
-
-      String templateItemType = TemplateItemUtils.getTemplateItemType(templateItem);
-
-      if (EvalConstants.ITEM_TYPE_HEADER.equals(templateItemType)) {
-         evalPDFReportBuilder.addSectionHeader(questionText);
-      }
-      else if (EvalConstants.ITEM_TYPE_BLOCK_PARENT.equals(templateItemType)) {
-         evalPDFReportBuilder.addSectionHeader(questionText);
-      }
-      else if (EvalConstants.ITEM_TYPE_TEXT.equals(templateItemType)) {
-    	 displayNumber++;
-         List<String> essays = new ArrayList<String>();
-         for (EvalAnswer answer: itemAnswers) {
-            essays.add(answer.getText());
-         }
-         evalPDFReportBuilder.addTextItemsList(displayNumber + ". " + questionText, essays);
-      }
-      else if (EvalConstants.ITEM_TYPE_MULTIPLEANSWER.equals(templateItemType) ||
-            EvalConstants.ITEM_TYPE_MULTIPLECHOICE.equals(templateItemType) ||
-            EvalConstants.ITEM_TYPE_SCALED.equals(templateItemType) ||
-            EvalConstants.ITEM_TYPE_BLOCK_CHILD.equals(templateItemType)) {
-         // always showing percentages for now
-         boolean showPercentages = true;
-//       boolean showPercentages = false;
-//       if (EvalConstants.ITEM_TYPE_MULTIPLEANSWER.equals(templateItemType)) {
-//       showPercentages = true;
-//       }
-
-         displayNumber++;
-         
-         int[] responseArray = TemplateItemDataList.getAnswerChoicesCounts(templateItemType, item.getScale().getOptions().length, itemAnswers);
-
-         String[] optionLabels;
-         if (templateItem.getUsesNA()) {
-            optionLabels = new String[item.getScale().getOptions().length+1];
-            for (int m = 0; m < item.getScale().getOptions().length; m++) {
-               optionLabels[m] = item.getScale().getOptions()[m];
-            }
-            optionLabels[optionLabels.length-1] = messageLocator.getMessage("reporting.notapplicable.longlabel");
-         }
-         else {
-            optionLabels = item.getScale().getOptions();
-         }
-
-         evalPDFReportBuilder.addLikertResponse(displayNumber + ". " + questionText, 
-               optionLabels, responseArray, showPercentages);
-
-         // handle comments
-         if (dti.usesComments()) {
-            List<String> comments = dti.getComments();
-            if (comments.size() <= 0) {
-               evalPDFReportBuilder.addSectionHeader( messageLocator.getMessage("viewreport.comments.header") );
-               evalPDFReportBuilder.addRegularText( messageLocator.getMessage("viewreport.no.comments") );
+            else if (EvalConstants.ITEM_CATEGORY_INSTRUCTOR.equals(tig.associateType)) {
+                evalPDFReportBuilder.addSectionHeader(
+                        messageLocator.getMessage("viewreport.itemlist.instructor", 
+                                new String[] {instructorIdtoEvalUser.get(tig.associateId).displayName}));
             } else {
-               evalPDFReportBuilder.addTextItemsList(messageLocator.getMessage("viewreport.comments.header"), comments);
+                evalPDFReportBuilder.addSectionHeader( messageLocator.getMessage("unknown.caps") );
             }
-         }
 
-      }
-      else {
-         log.warn("Trying to add unknown type to PDF: " + templateItemType);
-      }
-   }
-   
-   public String getContentType() {
-       return "application/pdf";
-   }
+            for (HierarchyNodeGroup hng: tig.hierarchyNodeGroups) {
+                // Render the Node title if it's enabled in the admin settings.
+                if (hng.node != null) {
+                    // Showing the section title is system configurable via the administrate view
+                    Boolean showHierSectionTitle = (Boolean) evalSettings.get(EvalSettings.DISPLAY_HIERARCHY_HEADERS);
+                    if (showHierSectionTitle) {
+                        evalPDFReportBuilder.addSectionHeader(hng.node.title);
+                    }
+                }
+
+                List<DataTemplateItem> dtis = hng.getDataTemplateItems(true); // include block children
+                for (int i = 0; i < dtis.size(); i++) {
+                    DataTemplateItem dti = dtis.get(i);
+                    renderDataTemplateItem(evalPDFReportBuilder, dti);
+                    renderedItemCount++;
+                }
+            }
+        }
+
+        evalPDFReportBuilder.close();
+    }
+
+    /**
+     * Renders a single question given the DataTemplateItem.
+     * 
+     * @param evalPDFReportBuilder
+     * @param dti the data template item
+     */
+    private void renderDataTemplateItem(EvalPDFReportBuilder evalPDFReportBuilder, DataTemplateItem dti) {
+        EvalTemplateItem templateItem = dti.templateItem;
+        EvalItem item = templateItem.getItem();
+        String questionText = commonLogic.makePlainTextFromHTML(item.getItemText());
+
+        List<EvalAnswer> itemAnswers = dti.getAnswers();
+
+        String templateItemType = TemplateItemUtils.getTemplateItemType(templateItem);
+
+        if (EvalConstants.ITEM_TYPE_HEADER.equals(templateItemType)) {
+            evalPDFReportBuilder.addSectionHeader(questionText);
+        }
+        else if (EvalConstants.ITEM_TYPE_BLOCK_PARENT.equals(templateItemType)) {
+            evalPDFReportBuilder.addSectionHeader(questionText);
+        }
+        else if (EvalConstants.ITEM_TYPE_TEXT.equals(templateItemType)) {
+            displayNumber++;
+            List<String> essays = new ArrayList<String>();
+            for (EvalAnswer answer: itemAnswers) {
+                essays.add(answer.getText());
+            }
+            evalPDFReportBuilder.addTextItemsList(displayNumber + ". " + questionText, essays);
+        }
+        else if (EvalConstants.ITEM_TYPE_MULTIPLEANSWER.equals(templateItemType) ||
+                EvalConstants.ITEM_TYPE_MULTIPLECHOICE.equals(templateItemType) ||
+                EvalConstants.ITEM_TYPE_SCALED.equals(templateItemType) ||
+                EvalConstants.ITEM_TYPE_BLOCK_CHILD.equals(templateItemType)) {
+            // always showing percentages for now
+            boolean showPercentages = true;
+            //       boolean showPercentages = false;
+            //       if (EvalConstants.ITEM_TYPE_MULTIPLEANSWER.equals(templateItemType)) {
+            //       showPercentages = true;
+            //       }
+
+            displayNumber++;
+            String[] itemScaleOptions = item.getScale().getOptions();
+            int[] responseArray = TemplateItemDataList.getAnswerChoicesCounts(templateItemType, itemScaleOptions.length, itemAnswers);
+
+            String[] optionLabels;
+            if (templateItem.getUsesNA()) {
+                optionLabels = new String[itemScaleOptions.length+1];
+                for (int m = 0; m < itemScaleOptions.length; m++) {
+                    optionLabels[m] = itemScaleOptions[m];
+                }
+                optionLabels[optionLabels.length-1] = messageLocator.getMessage("reporting.notapplicable.longlabel");
+            }
+            else {
+                optionLabels = itemScaleOptions;
+            }
+
+            // http://www.caret.cam.ac.uk/jira/browse/CTL-1504
+            AnswersMean answersMean = RenderingUtils.calculateMean(responseArray, templateItem.getUsesNA());
+            Object[] params = new Object[] {answersMean.getAnswersCount()+"", answersMean.getMeanText()};
+            String answersAndMean = messageLocator.getMessage("viewreport.answers.mean", params);
+
+            evalPDFReportBuilder.addLikertResponse(displayNumber + ". " + questionText, 
+                    optionLabels, responseArray, showPercentages, answersAndMean);
+
+            // handle comments
+            if (dti.usesComments()) {
+                List<String> comments = dti.getComments();
+                evalPDFReportBuilder.addCommentList(messageLocator.getMessage("viewreport.comments.header"), 
+                        comments, messageLocator.getMessage("viewreport.no.comments"));
+            }
+
+        }
+        else {
+            log.warn("Trying to add unknown type to PDF: " + templateItemType);
+        }
+    }
+
+    public String getContentType() {
+        return "application/pdf";
+    }
 }
