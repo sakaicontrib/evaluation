e67e5cd40e2c377fefbf4dc29d2c67a0dc8a705a
Aaron Zeckoski <azeckoski@vt.edu>
2009-03-18 20:16:37 +0000

    http://jira.sakaiproject.org/jira/browse/EVALSYS-666 Cleanup of viewparams and wrappers to ensure no user perms checks are being done without using services
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@59067 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/viewparams/ModifyHierarchyNodeParameters.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/viewparams/ModifyHierarchyNodeParameters.java
@@ -1,7 +1,5 @@
 package org.sakaiproject.evaluation.tool.viewparams;
 
-import uk.org.ponder.rsf.viewstate.SimpleViewParameters;
-
 public class ModifyHierarchyNodeParameters extends HierarchyNodeParameters {
     /* If adding child is true, we are adding a subnode to this node,
      * rather than modifying the node itself.
