3e0b8f81839930a695826d75c4bd85959390b562
Lovemore Nalube <lovemore.nalube@uct.ac.za>
2009-03-19 19:06:10 +0000

    EVALSYS-661. Finished JS and RSF customizations for Instructor/Ta selection controls.
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@59108 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/evalTakeEval.js
+++ tool/src/webapp/content/js/evalTakeEval.js
@@ -19,7 +19,8 @@ $(document).ready(function() {
             activeCheckbox: {background:'#eee'}
         },
         type: 1, //Type is for type of category we are handling. ie: 0 = instructor, 1 = ta
-        debug: true
+        debug: false,
+        fields: ['input', 'select', 'textarea'] //Array of fields in the form
     }
     /**
      * Private methods and variables
@@ -35,17 +36,25 @@ $(document).ready(function() {
         questionsToShow: new Array(),
         questionsToHide: new Array(),
         get:{
-            shownQuestions: function() {
-                log("Getting questions for type: " + variables.get.typeOfBranch());
-                var str = '"div.' + variables.get.typeOfBranch() + 'Branch:visible"';
-                return $(str).get();
-            },
             typeOfBranch:function() {
+                log("Active code type is: " + variables.options.type);
                 switch (variables.options.type) {
                     case 0: return "instructor";break;
                     case 1: return "ta";break;
                 }
             } ,
+            shownQuestions: function() {
+                log("Getting questions for type: " + variables.get.typeOfBranch());
+                var str = 'div.' + variables.get.typeOfBranch() + 'Branch:visible';
+                var temp = new Array();
+                $.each($(str).get(), function(i, item) {
+                    temp.push($(item).attr("name"));
+                    log("Search found: " + $(item).attr("name"));
+                });
+                log("Search found total: " + temp.length);
+                return temp;
+            },
+
             selectedBoxesArray:function(that) {
                 var checked = new Array();
                 that.find('input[@type=checkbox]').each(function() {
@@ -58,23 +67,35 @@ $(document).ready(function() {
                 return variables.get.selectedBoxesArray(that).length > 0
             }
         },
-        options:'',
+        set:{
+            typeOfBranch:function(that) {
+                var temp;
+                if (that.attr('name').search(/instructor/i) != -1)
+                    temp = 0;
+                else if (that.attr('name').search(/tas/i) != -1)
+                    temp = 1;
+                variables.options.type = temp;
+                log("Active type is: " + variables.get.typeOfBranch());
+
+            }
+        },
+        options:null,
         foundArray: function() {
-            var f = new Array();
-            $.each(variables.questionsToShow, function(s, show) {
-                log("Checking if "+show+" is already showing in dom.");
-                $.each(variables.questionsToHide, function(h, hide) {
-                    if (show == hide) {
-                        f.push(show);
-                        log("Found visible field : " + show);
+            var temp = new Array();
+            $.each(variables.questionsToShow, function(s, shown) {
+                log("Checking if " + shown + " is already showing in dom. Dom has:" + variables.get.shownQuestions().length + " visible.");
+                $.each(variables.get.shownQuestions(), function(h, hidden) {
+                    if (shown == hidden) {
+                        temp.push(shown);
+                        log("Found visible field: " + shown);
                     }
-                });            
+                });
             });
-            return f;
+            return temp;
         },
         foundBool: function() {
             var temp = variables.foundArray();
-            log("Found "+temp.length+" with IDs: "+ temp.toString());
+            log("Found " + temp.length + " with IDs: " + temp.toString());
             return (temp.length > 0)
         },
         foundSplice:function(item) {
@@ -88,8 +109,20 @@ $(document).ready(function() {
                 });
             }
             return false;
+        },
+        isVisible:function(item) {
+            var visible = false;
+            if (variables.foundBool) {
+                $.each(variables.get.shownQuestions(), function(i, found) {
+                    if (item == found) {
+                        visible = true;
+                    }
+                });
+            }
+            return visible;
         }
     }
+
     function init(that, options) {
         //copy options to this class
         variables.options = options;
@@ -102,78 +135,81 @@ $(document).ready(function() {
                 log("Found a checkbox list for: " + variables.get.typeOfBranch());
             }
             initControls($(this));
+            initClassVars();
         });
     }
 
+    function initClassVars() {
+        variables.questionsToHide = new Array();
+        variables.questionsToShow = new Array();
+    }
+
     function initControls(that) {
-        var elemId;
-        if (variables.typeOfSelector.one) {
-            //Working with dropdown
-           that.find('select').change(function(){
-               var elem = $(this)
-               elemId =  elem.val();
-               log("Selected guy with id: "+elemId);
-               variables.questionsToShow = new Array();
-               variables.questionsToShow.push(elemId)
-               log("Added item "+elemId+" to array. Now Array has this number of elements: "+variables.questionsToShow.length);
-           });
-        }
         if (variables.typeOfSelector.multiple) {
             //Working with checkboxes
-            that.find('input[@type=checkbox]').each(function() {
+            that.find('input[type=checkbox]').each(function() {
                 var elem = $(this);
-                elemId = elem.attr('id');
-                log("Attaching event listener for: " + elem.parent().find('label').text() + " with id: " + elemId);
+                var elemId = elem.attr('id');
+                var elemLabel = elem.parent().find('label').text();
+                log("Attaching event listener for: " + elemLabel + " with id: " + elemId);
                 elem.bind('click', function() {
+                    variables.set.typeOfBranch($(this));
                     if (this.checked) {
                         //Fn for Check
-                        variables.questionsToShow.push(elemId);
                         elem.parent().css('background', variables.options.css.activeCheckbox.background);
-                        log("SELECTED: " + elem.parent().find('label').text() + " with id: " + elemId);
+                        log("SELECTED: " + elemLabel + " with id: " + elemId);
+                        if (!variables.isVisible(elemId)) {
+                            variables.questionsToShow.push(elemId);
+                            log("ADDED: " + elemLabel + " with id: " + elemId);
+                            showQuestions();
+                            initClassVars();
+                        } else {
+                            log("Person: " + elemLabel + " with id: " + elemId + " is already visible.");
+                        }
                     }
-                    else
-                    {   //Fn for Uncheck
-                        variables.questionsToHide.push(elemId);
-                        variables.foundSplice(elemId);
-                        elem.parent().css('background', '');
-                        log("DESELECTED: " + elem.parent().find('label').text() + " with id: " + elemId);
-
+                    else {
+                        this.checked = true;
+                        if (variables.isVisible(elemId) && confirmSelection(elemLabel)) {
+                            elem.parent().css('background', '');
+                            log("DESELECTED: " + elemLabel + " with id: " + elemId);
+                            variables.questionsToHide.push(elemId);
+                            variables.foundSplice(elemId);
+                            log("DELETED: " + elemLabel + " with id: " + elemId);
+                            showQuestions();
+                            initClassVars();
+                            this.checked = false;
+                        }
                     }
                 });
             });
         }
-        
-        that.find('input[@type=button]:eq(0)').bind('click', function() {
-            if (variables.typeOfSelector.one) {
-                if (variables.foundBool) {
-                        render = confirmSelection();
-                    } else {
-                        render = true;
-                    }
-                    if (render) {
-                        log("Showing " + variables.questionsToShow.length + " item in dom.");
-                            showQuestions();
 
-                        
-                    }
-            }
-            if (variables.typeOfSelector.multiple) {
-                if (variables.get.selectedBoxesBool(that)) {
-                    var render = false;
-                    if (variables.foundBool) {
-                        render = confirmSelection();
-                    } else {
-                        render = true;
-                    }
-                    if (render) {
-                        log("Showing " + variables.questionsToShow.length + " items in dom.");
-                        $.each(variables.questionsToShow, function(i, item) {
-                            showQuestions();
-                        });
-                    }
-                    else {
-                        log('No selection made');
-                    }
+        that.find('input[@type=button]').bind('click', function() {
+            var elem = that.find('select:eq(0)');
+            variables.set.typeOfBranch(elem);
+            if (variables.typeOfSelector.one) {
+                //Working with dropdown
+                var elemId = elem.val();
+                var render = false;
+                log("Selected guy with id: " + elemId);
+                if (variables.get.shownQuestions().length == 0) {
+                    render = true;
+                }
+                else if (variables.isVisible(elemId)) {
+                    log("Person with id: " + elemId + " is already visible.");
+                    return false;
+                } else
+                {
+                    var temp = 'div[name=' + variables.get.shownQuestions()[0] + ']';
+                    render = confirmSelection($(temp).find('legend').attr('title'));
+                }
+                if (render) {
+                    variables.questionsToHide = variables.get.shownQuestions();
+                    variables.questionsToShow = new Array();
+                    variables.questionsToShow.push(elemId);
+                    log("Added item " + elemId + " to array. Now Array has this number of elements: " + variables.questionsToShow.length);
+                    showQuestions();
+                    initClassVars();
                 }
             }
         });
@@ -181,47 +217,62 @@ $(document).ready(function() {
 
     function showQuestions() {
         hideQuestions();
+        log("Showing " + variables.questionsToShow.length + " items in dom.");
         $.each(variables.questionsToShow, function(i, item) {
-            var str = 'div[name=' + item + ']';
-            $(str).slideDown('normal', function() {
-                log("Revealing: "+item)
+            var tempFound = 0;
+            $.each(variables.get.shownQuestions(), function(s, toSkip) {
+                if (item == toSkip) {
+                    tempFound++;
+                }
             });
+            if (tempFound == 0) {
+                var str = 'div[name=' + item + ']';
+                $(str).slideDown('normal', function() {
+                    log("Revealing: " + item)
+                });
+            }
         });
     }
-    function hideQuestions() {
-        $.each(variables.questionsToHide, function(i, item) {
-            var str = '"div[@name=' + item + ']"';
+
+    function hideQuestions(all) {
+        var temp = new Array();
+        temp = (all && all == 1) ? variables.get.shownQuestions() : variables.questionsToHide;
+        $.each(temp, function(i, item) {
+            var str = 'div[@name=' + item + ']';
             $(str).slideUp('normal', function() {
-                log(item + " HIDDEN")
+                log("WARN: Hiding " + item + " in dom.");
+                clearFieldsFor($(this));
             });
         });
     }
 
-    function confirmSelection() {
-        log("Found is: "+variables.foundBool());
-        if (variables.foundBool()) {
-            log("Found "+variables.foundArray().length+" with IDs: "+ variables.foundArray().toString());
-            if (confirm("Are you sure :)")) {
-                return true;
-            }else{
-                return false;
-            }
-        }
-        return true;
+    function confirmSelection(name) {
+        return confirm($('div#evalSelectWarn').text().replace(/XXX/, name));
+    }
+
+    function clearFieldsFor(item) {
+        $.each(variables.options.fields, function(f, field) {
+            log("Clearing all " + field.toLowerCase() + " fields.");
+            item.find(field.toLowerCase()).each(function() {
+                var t = this.type, tag = this.tagName.toLowerCase();
+                log("Found type: " + t);
+                log("Found tag: " + tag);
+                if (t == 'text' || t == 'password' || tag == 'textarea')
+                    this.value = '';
+                else if (t == 'checkbox' || t == 'radio')
+                    this.checked = false;
+                else if (tag == 'select')
+                        this.selectedIndex = -1;
+            });
+        });
     }
 
     // Debugging
-    //
     function log($obj) {
         if (variables.options.debug) {
             if (window.console && window.console.log) {
                 window.console.log('INFO: ' + $obj);
             }
-            else {
-                alert($obj);
-            }
         }
     }
-
-
-})(jQuery)
+})($)
