0c96160d7f8cce20444a585c616b5c35fe2c1cf5
Aaron Zeckoski <azeckoski@vt.edu>
2009-03-29 14:13:43 +0000

    http://jira.sakaiproject.org/jira/browse/EVALSYS-640 Should no longer get the option to click this when there are no templates available, also cleaned up a number of the views and made sure various links were available correctly
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@59400 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/producers/SummaryProducer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/producers/SummaryProducer.java
@@ -134,22 +134,25 @@ public class SummaryProducer implements ViewComponentProducer, DefaultView, Navi
 
         if (userAdmin) {
             UIInternalLink.make(tofill, "administrate-link", UIMessage.make("administrate.page.title"), new SimpleViewParameters(AdministrateProducer.VIEW_ID));
-            UIInternalLink.make(tofill, "control-scales-link", UIMessage.make("controlscales.page.title"), new SimpleViewParameters(
-                    ControlScalesProducer.VIEW_ID));
         }
 
         if (createTemplate) {
             UIInternalLink.make(tofill, "control-templates-link", UIMessage.make("controltemplates.page.title"), new SimpleViewParameters(
                     ControlTemplatesProducer.VIEW_ID));
-            if (!((Boolean)settings.get(EvalSettings.DISABLE_ITEM_BANK))) {
-                UIInternalLink
-                .make(tofill, "control-items-link", UIMessage.make("controlitems.page.title"), new SimpleViewParameters(ControlItemsProducer.VIEW_ID));
+            if (! ((Boolean)settings.get(EvalSettings.DISABLE_ITEM_BANK))) {
+                UIInternalLink.make(tofill, "control-items-link", UIMessage.make("controlitems.page.title"), 
+                        new SimpleViewParameters(ControlItemsProducer.VIEW_ID));
+                UIInternalLink.make(tofill, "control-scales-link", UIMessage.make("controlscales.page.title"), 
+                        new SimpleViewParameters(ControlScalesProducer.VIEW_ID));
             }
         }
 
         if (beginEvaluation) {
-            UIInternalLink.make(tofill, "control-evaluations-link", UIMessage.make("controlevaluations.page.title"), new SimpleViewParameters(
-                    ControlEvaluationsProducer.VIEW_ID));
+            UIInternalLink.make(tofill, "control-evaluations-link", UIMessage.make("controlevaluations.page.title"), 
+                    new SimpleViewParameters(ControlEvaluationsProducer.VIEW_ID));
+            UIInternalLink.make(tofill, "control-emailtemplates-link", UIMessage.make("controlemailtemplates.page.title"),
+                    new SimpleViewParameters(ControlEmailTemplatesProducer.VIEW_ID));
+            // show instructor instructions
             UIMessage.make(tofill, "instructor-instructions", "summary.instructor.instruction");
         }
 
