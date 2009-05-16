923de6567f1724df2ee01bb1645a570039037ce6
Stephen Marquard <stephen.marquard@uct.ac.za>
2009-05-16 19:44:05 +0000

    EVALSYS-618. VULA-575. Handle case where selections are set but site has no users in those roles (merge from UCT branch)
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@60506 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/producers/TakeEvalProducer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/producers/TakeEvalProducer.java
@@ -498,6 +498,9 @@ public class TakeEvalProducer implements ViewComponentProducer, ViewParamsReport
                                 throw new IllegalStateException("Invalid selection option ("
                                         + selectValue + "): do not know how to handle this.");
                             }
+                        } else {
+                            // handle case where there are selections set but no users in the roles.
+                            form.parameters.add(new UIELBinding(selectionOTP, "none"));
                         }
                     }
                 }
