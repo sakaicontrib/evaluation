40346a38cb8aa70c22917706186919c794bb8fb9
Lovemore Nalube <lovemore.nalube@uct.ac.za>
2009-08-07 09:04:30 +0000

    EVALSYS-783 Save reordering evoked by the row dropdowns
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@62388 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/eval.template.sort.js
+++ tool/src/webapp/content/js/eval.template.sort.js
@@ -1,4 +1,29 @@
 // Eval Template Modify JS for Sorting
 // @author lovemore.nalube@uct.ac.za
 
+var evalTemplateSort = (function(){
+    //Logger
+    var log = evalTemplateUtils.debug;
 
+    //Update the itemRow labels after a sort event
+    var updateLabellingAfterSort = function(enableSaveButton){
+        var list = $("#itemList > div.itemRow").not('.ui-sortable-helper').get(),
+        enableBtn = ( typeof enableSaveButton === "undefined" ) ? true : enableSaveButton;
+        log.group("Updating Labelling After Sort");
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].id) {
+                setIndex(list[i].id, i);
+                log.debug("Setting index for id %s from object %o", list[i].id, list[i]);
+            }
+        }
+        if( enableBtn){
+            log.info("enabling save buttons");
+            enableOrderButtons();
+        }
+        log.groupEnd();
+    };
+
+    return {
+        updateLabelling: updateLabellingAfterSort
+    };
+})($);
