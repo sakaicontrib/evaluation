501e40b571bef61f00cd4bd65a2d92fcb9f67c05
Lovemore Nalube <lovemore.nalube@uct.ac.za>
2009-08-18 09:12:43 +0000

    EVALSYS-802 Final fix using a different approach that's lighter and faster.
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@62654 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/eval.template.sort.js
+++ tool/src/webapp/content/js/eval.template.sort.js
@@ -8,8 +8,8 @@ var evalTemplateSort = (function(){
     //Update the itemRow labels after a sort event
     var updateLabellingAfterSort = function(enableSaveButton){
         var list = $("#itemList > div.itemRow").not('.ui-sortable-helper').get(),
-        maxDropDownOptions = list.length,
-        count = 0,
+        currentMaxDropDownOptions = list.length,
+        previousMaxDropDownOptions = $("select[id*=item-select-selection]:eq(0) > option").length,
         enableBtn = ( typeof enableSaveButton === "undefined" ) ? true : enableSaveButton;
         log.group("Updating Labelling After Sort");
         for (var i = 0; i < list.length; i++) {
@@ -18,20 +18,24 @@ var evalTemplateSort = (function(){
                 log.debug("Setting index for id %s from object %o", list[i].id, list[i]);
             }
         }
+
         if( enableBtn){
             log.info("enabling save buttons");
             enableOrderButtons();
         }
         //update all dropdowns to show new max EVALSYS-802
-        $("select[id*=item-select-selection]").each(function(){
-            count = 0;
-            $(this).find("option").each(function(){
-                if (count >= maxDropDownOptions){
-                    $(this).remove();
-                }
-                count++;
-            });
+        $.each($("select[id*=item-select-selection]"), function(m, dropdown) {
+            log.group("Trimming dropdown vals for %o", dropdown);
+            //Only consider option items in the range to remove so as to avoid unneccessary looping
+            for (var toRemove = previousMaxDropDownOptions; toRemove >= currentMaxDropDownOptions; toRemove--){
+                $(dropdown).find("option:eq(" + toRemove + ")").each(function() {
+                       log.info("Removing %o with num: %i", this, toRemove);
+                        $(this).remove();
+                });
+            }
+            log.groupEnd();
         });
+
         refreshSort();
         log.groupEnd();
     };
