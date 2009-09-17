bd33aa243aa7cbc03d6138e82c2c8776d5ec839e
Lovemore Nalube <lovemore.nalube@uct.ac.za>
2009-09-17 14:35:18 +0000

    EVALSYS-795 Inject jQuery.i18n.properties plugin by Nuno Fernandes (http://codingwithcoffee.com/?p=272), use maven to copy resourse bundles to webapp/content/bundle
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@63366 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/eval.template.loader.js
+++ tool/src/webapp/content/js/eval.template.loader.js
@@ -8,6 +8,19 @@ $(document).ready(function () {
     buildSortableIds();
     //init Global Ajax Overide Options
     evalTemplateData.ajaxSetUp();
+    
+    // This will initialize the plugin
+    // and show two dialog boxes: one with the text "Ol‡ World"
+    // and other with the text "Good morning John!"
+    /*jQuery.i18n({
+        name:'messages',
+        path:'/sakai-evaluation-tool/content/bundle/',
+        callback: function() {
+            alert(general.direct.link.title);
+            alert(general.category.link.tip('John'));
+        }
+    });
+    */
 });
 
 //trigger FB overrides
