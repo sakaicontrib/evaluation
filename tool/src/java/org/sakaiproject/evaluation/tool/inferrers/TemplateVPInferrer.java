4e87f86bb7a6686d140178c2d36c48cb201fe7ff
Aaron Zeckoski <azeckoski@vt.edu>
2010-06-17 18:59:31 +0000

    http://jira.sakaiproject.org/browse/EVALSYS-888 applied patch from Oxford to change behavior so that templates are only visible at the URL when the user is either logged in OR the template is set as public
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@68241 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/inferrers/TemplateVPInferrer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/inferrers/TemplateVPInferrer.java
@@ -15,7 +15,11 @@
 package org.sakaiproject.evaluation.tool.inferrers;
 
 import org.sakaiproject.entitybroker.IdEntityReference;
+import org.sakaiproject.evaluation.constant.EvalConstants;
+import org.sakaiproject.evaluation.logic.EvalAuthoringService;
+import org.sakaiproject.evaluation.logic.EvalCommonLogic;
 import org.sakaiproject.evaluation.logic.entity.TemplateEntityProvider;
+import org.sakaiproject.evaluation.model.EvalTemplate;
 import org.sakaiproject.evaluation.tool.producers.PreviewEvalProducer;
 import org.sakaiproject.evaluation.tool.viewparams.EvalViewParameters;
 
@@ -28,6 +32,18 @@ import uk.org.ponder.rsf.viewstate.ViewParameters;
  * @author Aaron Zeckoski (aaronz@vt.edu)
  */
 public class TemplateVPInferrer implements EntityViewParamsInferrer {
+
+	private EvalAuthoringService authoringService;
+	
+	private EvalCommonLogic commonLogic;
+
+	public void setAuthoringService(EvalAuthoringService authoringService) {
+		this.authoringService = authoringService;
+	}
+
+	public void setCommonLogic(EvalCommonLogic commonLogic) {
+		this.commonLogic = commonLogic;
+	}
 
 	/* (non-Javadoc)
 	 * @see uk.ac.cam.caret.sakai.rsf.entitybroker.EntityViewParamsInferrer#getHandledPrefixes()
@@ -42,13 +58,15 @@ public class TemplateVPInferrer implements EntityViewParamsInferrer {
 	public ViewParameters inferDefaultViewParameters(String reference) {
 		IdEntityReference ep = new IdEntityReference(reference);
 		Long templateId = new Long(ep.id);
-		// MAYBE add in restriction for access to template preview later? -AZ
-//		EvalTemplate template = authoringService.getTemplateById(templateId); 
-//		if (EvalConstants.SHARING_PUBLIC.equals(template.getSharing()) ||
-//				Boolean.TRUE.equals(template.getExpert())) {
-//		} else {
-//			authoringService.canControlTemplate(userId, templateId);
-//		}
+		EvalTemplate template = authoringService.getTemplateById(templateId); 
+		if (EvalConstants.SHARING_PUBLIC.equals(template.getSharing()) ||
+				Boolean.TRUE.equals(template.getExpert())) {
+		} else {
+			String userId = commonLogic.getCurrentUserId();
+			if (!authoringService.canModifyTemplate(userId, templateId)) {
+				throw new SecurityException("You are not a maintainer on this template.");
+			}
+		}
 		EvalViewParameters vp = new EvalViewParameters(PreviewEvalProducer.VIEW_ID, null, templateId);
 		vp.external = true;
 		return vp;
