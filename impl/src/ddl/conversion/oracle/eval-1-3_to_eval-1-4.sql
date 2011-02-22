295b76b4603be38aa0aea8ada8452b862255e9d6
Jim Eng <jimeng@umich.edu>
2011-02-22 16:41:31 +0000

    EVALSYS_1055 Correcting typos in names of 1.3 to 1.4 conversion scripts. Adding ddl to conversion scripts for adding columns that were left out of hibernate mapping files.
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/branches/EVALSYS-1055-2@72848 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ impl/src/ddl/conversion/oracle/eval-1-3_to_eval-1-4.sql
@@ -0,0 +1,8 @@
+-- Oracle conversion script - 1.3 to 1.4 
+alter table EVAL_EVALUATION add (LOCAL_SELECTOR varchar2(255 char));
+
+-- AVAILABLE_EMAIL_SENT was included in ddl files but not in hibernate mapping files 
+-- alter table EVAL_EVALUATION add (AVAILABLE_EMAIL_SENT number(1,0));
+-- If AVAILABLE_EMAIL_SENT is being added, give it a reasonable default -- assume emails have been sent for Active evals and not for others?
+-- update EVAL_EVALUATION set AVAILABLE_EMAIL_SENT='1' where STATE='Active' or STATE='Closed';
+-- update EVAL_EVALUATION set AVAILABLE_EMAIL_SENT='0' where AVAILABLE_EMAIL_SENT is null;
