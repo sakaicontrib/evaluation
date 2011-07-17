3466d37a7b6e79c1660d52cff66aadd99843e32a
Aaron Zeckoski <azeckoski@vt.edu>
2011-07-17 14:02:53 +0000

    EVALSYS-1084 Undoing commit 74705
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@75558 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- impl/src/java/org/sakaiproject/evaluation/logic/entity/EvalReportsEntityProviderImpl.java
+++ impl/src/java/org/sakaiproject/evaluation/logic/entity/EvalReportsEntityProviderImpl.java
@@ -38,7 +38,7 @@ public class EvalReportsEntityProviderImpl implements EvalReportsEntityProvider,
         Long evaluationId;
         try {
             evaluationId = new Long(id);
-            if (evaluationService.canViewEvaluation(null, evaluationId)) {
+            if (evaluationService.checkEvaluationExists(evaluationId)) {
                 return true;
             }
         } catch (NumberFormatException e) {
