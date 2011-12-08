aeb49ca04359765073d763f4dd9ce28b15902e0e
Jim Eng <jimeng@umich.edu>
2011-12-08 16:14:58 +0000

    EVALSYS-1154 Added LockManager interface and impl to handle locks for higher-level classes like ConsolidatedNotificationsJob. ConsolidatedNotificationsJob uses LockManager to get a lock for one server so other servers will not attempt to run the same job.
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@77795 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ impl/src/java/org/sakaiproject/evaluation/logic/EvalLockManagerImpl.java
@@ -0,0 +1,51 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2011 The Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.evaluation.logic;
+
+import org.sakaiproject.evaluation.dao.EvaluationDao;
+
+/**
+ * 
+ *
+ */
+public class EvalLockManagerImpl implements EvalLockManager {
+	
+    private EvaluationDao dao;
+    public void setDao(EvaluationDao dao) {
+        this.dao = dao;
+    }
+	
+	/* (non-Javadoc)
+	 * @see org.sakaiproject.evaluation.logic.EvalLockManager#obtainLock(java.lang.String, java.lang.String, long)
+	 */
+	public Boolean obtainLock(String lockId, String executerId, long timePeriod) {
+		return dao.obtainLock(lockId, executerId, timePeriod);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.sakaiproject.evaluation.logic.EvalLockManager#releaseLock(java.lang.String, java.lang.String)
+	 */
+	public Boolean releaseLock(String lockId, String executerId) {
+		return dao.releaseLock(lockId, executerId);
+	}
+
+}
--- impl/src/java/org/sakaiproject/evaluation/logic/scheduling/ConsolidatedNotificationsJobImpl.java
+++ impl/src/java/org/sakaiproject/evaluation/logic/scheduling/ConsolidatedNotificationsJobImpl.java
@@ -1,6 +1,24 @@
-/**
- * 
- */
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2011 The Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
 package org.sakaiproject.evaluation.logic.scheduling;
 
 import java.text.DateFormat;
@@ -19,6 +37,7 @@ import org.quartz.JobExecutionException;
 import org.sakaiproject.evaluation.jobmonitor.JobStatusReporter;
 import org.sakaiproject.evaluation.jobmonitor.LoggingJobStatusReporter;
 import org.sakaiproject.evaluation.logic.EvalEmailsLogic;
+import org.sakaiproject.evaluation.logic.EvalLockManager;
 import org.sakaiproject.evaluation.logic.EvalSettings;
 import org.sakaiproject.evaluation.logic.externals.EvalExternalLogic;
 
@@ -29,6 +48,9 @@ import org.sakaiproject.evaluation.logic.externals.EvalExternalLogic;
  */
 public class ConsolidatedNotificationsJobImpl implements ConsolidatedNotificationsJob {
 	
+	public static final String LOCK_CONSOLIDATED_EMAIL_JOB = "LOCK_CONSOLIDATED_EMAIL_JOB";
+	private static final long TWO_HOURS = 2L * 60L * 60L * 1000L;
+
 	Log log = LogFactory.getLog(ConsolidatedNotificationsJobImpl.class);
 	
 	protected EvalExternalLogic externalLogic;
@@ -44,6 +66,11 @@ public class ConsolidatedNotificationsJobImpl implements ConsolidatedNotificatio
 	protected EvalSettings evalSettings;
 	public void setEvalSettings(EvalSettings evalSettings) {
 		this.evalSettings = evalSettings;
+	}
+	
+	protected EvalLockManager lockManager;
+	public void setEvalLockManager(EvalLockManager lockManager) {
+		this.lockManager = lockManager;
 	}
 	
     protected JobStatusReporter jobStatusReporter;
@@ -80,101 +107,113 @@ public class ConsolidatedNotificationsJobImpl implements ConsolidatedNotificatio
 			throws JobExecutionException {
 		log.info("execute()");
 		
-		Date beginTime = new Date();
-	
-		String jobId = this.jobStatusReporter.reportStarted("Email");
+		// this server must get lock to do this job
 		String serverId = this.externalLogic.getServerId();
-		this.jobStatusReporter.reportProgress(jobId, "server-id", serverId);
+		Boolean gotLock = lockManager.obtainLock(LOCK_CONSOLIDATED_EMAIL_JOB, serverId, TWO_HOURS);
+		if(gotLock != null && gotLock.booleanValue()) {
+			try {
+				
+				Date beginTime = new Date();
+			
+				String jobId = this.jobStatusReporter.reportStarted("Email");
+				this.jobStatusReporter.reportProgress(jobId, "server-id", serverId);
+				
+				Boolean sendAvailableEmails = (Boolean) this.evalSettings.get(EvalSettings.CONSOLIDATED_EMAIL_NOTIFY_AVAILABLE);
+				if(sendAvailableEmails == null) {
+					sendAvailableEmails = new Boolean(true);
+				}
+				
+				if(sendAvailableEmails.booleanValue()) {
+					String[] recipients = this.emailLogic.sendConsolidatedAvailableNotifications(jobStatusReporter, jobId);
+					if(recipients == null) {
+						log.debug("announcements sent: 0");
+					} else {
+						log.debug("announcements sent: " + recipients.length);
+					}
+				}
 		
-		Boolean sendAvailableEmails = (Boolean) this.evalSettings.get(EvalSettings.CONSOLIDATED_EMAIL_NOTIFY_AVAILABLE);
-		if(sendAvailableEmails == null) {
-			sendAvailableEmails = new Boolean(true);
-		}
+				int reminderInterval = ((Integer) evalSettings.get(EvalSettings.SINGLE_EMAIL_REMINDER_DAYS)).intValue();
+				// check if reminders are to be sent
+				if(reminderInterval > 0) {
+					Date nextReminder = null;
+					String nextReminderStr = (String) evalSettings.get(EvalSettings.NEXT_REMINDER_DATE);
+					if(nextReminderStr == null || nextReminderStr.trim().equals("")) {
+						nextReminder = new Date();
+					} else {
+				        DateFormat df = new SimpleDateFormat("EEE MMM dd kk:mm:ss zzz yyyy"); //DateFormat.getDateTimeInstance(DateFormat.FULL,DateFormat.FULL);
+						try {
+							nextReminder = df.parse( nextReminderStr );
+						} catch (ParseException e) {
+							// Use current date
+							nextReminder = new Date();
+						}
+					}
+					if (log.isInfoEnabled()) {
+						log.info("Next reminder date is " + nextReminder + ".");
+					}
+					//reminder interval unit is a day
+					long one_hour = 1000L * 60L * 60L;
+					long one_day = one_hour * 24L;
+					// see if time is equal to or after reminder date (+/- six hours)
+					long rdate = nextReminder.getTime();
+					long tdate = System.currentTimeMillis();
+					if (tdate >= (rdate - 6L * one_hour)) {
+						
 		
-		if(sendAvailableEmails.booleanValue()) {
-			String[] recipients = this.emailLogic.sendConsolidatedAvailableNotifications(jobStatusReporter, jobId);
-			if(recipients == null) {
-				log.debug("announcements sent: 0");
-			} else {
-				log.debug("announcements sent: " + recipients.length);
-			}
-		}
-
-		int reminderInterval = ((Integer) evalSettings.get(EvalSettings.SINGLE_EMAIL_REMINDER_DAYS)).intValue();
-		// check if reminders are to be sent
-		if(reminderInterval > 0) {
-			Date nextReminder = null;
-			String nextReminderStr = (String) evalSettings.get(EvalSettings.NEXT_REMINDER_DATE);
-			if(nextReminderStr == null || nextReminderStr.trim().equals("")) {
-				nextReminder = new Date();
-			} else {
-		        DateFormat df = new SimpleDateFormat("EEE MMM dd kk:mm:ss zzz yyyy"); //DateFormat.getDateTimeInstance(DateFormat.FULL,DateFormat.FULL);
-				try {
-					nextReminder = df.parse( nextReminderStr );
-				} catch (ParseException e) {
-					// Use current date
-					nextReminder = new Date();
-				}
-			}
-			if (log.isInfoEnabled()) {
-				log.info("Next reminder date is " + nextReminder + ".");
-			}
-			//reminder interval unit is a day
-			long one_hour = 1000L * 60L * 60L;
-			long one_day = one_hour * 24L;
-			// see if time is equal to or after reminder date (+/- six hours)
-			long rdate = nextReminder.getTime();
-			long tdate = System.currentTimeMillis();
-			if (tdate >= (rdate - 6L * one_hour)) {
-				
-
-				String[] recipients = this.emailLogic.sendConsolidatedReminderNotifications(jobStatusReporter, jobId);
-				if(recipients == null) {
-					log.debug("reminders sent: 0");
-				} else {
-					log.debug("reminders sent: " + recipients.length);
-				}
-				Calendar cal = Calendar.getInstance();
-				cal.setTimeInMillis(tdate + reminderInterval * one_day);
-				Integer startTime = (Integer) this.evalSettings.get(EvalSettings.CONSOLIDATED_EMAIL_DAILY_START_TIME);
-				Integer startMinute = (Integer) this.evalSettings.get(EvalSettings.CONSOLIDATED_EMAIL_DAILY_START_MINUTES);
-
-				if(startTime != null) {
-					cal.set(Calendar.HOUR_OF_DAY, startTime.intValue());
-					cal.set(Calendar.MINUTE, startMinute.intValue());
-					cal.set(Calendar.SECOND, 0);
-				}
-				this.evalSettings.set(EvalSettings.NEXT_REMINDER_DATE, cal.getTime());
-				
-				Date endTime = new Date();
-				
-				//"FINISHED" "summary" The email job took <elapsed-time> seconds to run. It kicked off at <begin-time> and ended at <begin-time>.
-				
-				StringBuilder buf = new StringBuilder();
-				DateFormat df = DateFormat.getTimeInstance();
-				long seconds = endTime.getTime() - beginTime.getTime();
-				long milliseconds = seconds % 1000;
-				seconds = seconds / 1000;
-				
-				buf.append("The email job took ");
-				buf.append(seconds);
-				buf.append(".");
-				if(milliseconds < 10) {
-					buf.append("00");
-				} else if (milliseconds < 100) {
-					buf.append("0");
-				}
-				buf.append(milliseconds);
-				buf.append(" seconds to run. It kicked off at ");
-				buf.append(df.format(beginTime));
-				buf.append(" and ended at ");
-				buf.append(df.format(endTime));
-				buf.append(".");
-				
-				jobStatusReporter.reportFinished(jobId, false, "finished", buf.toString());
-			}
-		}
+						String[] recipients = this.emailLogic.sendConsolidatedReminderNotifications(jobStatusReporter, jobId);
+						if(recipients == null) {
+							log.debug("reminders sent: 0");
+						} else {
+							log.debug("reminders sent: " + recipients.length);
+						}
+						Calendar cal = Calendar.getInstance();
+						cal.setTimeInMillis(tdate + reminderInterval * one_day);
+						Integer startTime = (Integer) this.evalSettings.get(EvalSettings.CONSOLIDATED_EMAIL_DAILY_START_TIME);
+						Integer startMinute = (Integer) this.evalSettings.get(EvalSettings.CONSOLIDATED_EMAIL_DAILY_START_MINUTES);
 		
+						if(startTime != null) {
+							cal.set(Calendar.HOUR_OF_DAY, startTime.intValue());
+							cal.set(Calendar.MINUTE, startMinute.intValue());
+							cal.set(Calendar.SECOND, 0);
+						}
+						this.evalSettings.set(EvalSettings.NEXT_REMINDER_DATE, cal.getTime());
+						
+						Date endTime = new Date();
+						
+						//"FINISHED" "summary" The email job took <elapsed-time> seconds to run. It kicked off at <begin-time> and ended at <begin-time>.
+						
+						StringBuilder buf = new StringBuilder();
+						DateFormat df = DateFormat.getTimeInstance();
+						long seconds = endTime.getTime() - beginTime.getTime();
+						long milliseconds = seconds % 1000;
+						seconds = seconds / 1000;
+						
+						buf.append("The email job took ");
+						buf.append(seconds);
+						buf.append(".");
+						if(milliseconds < 10) {
+							buf.append("00");
+						} else if (milliseconds < 100) {
+							buf.append("0");
+						}
+						buf.append(milliseconds);
+						buf.append(" seconds to run. It kicked off at ");
+						buf.append(df.format(beginTime));
+						buf.append(" and ended at ");
+						buf.append(df.format(endTime));
+						buf.append(".");
+						
+						jobStatusReporter.reportFinished(jobId, false, "finished", buf.toString());
+					}
+				}
+			} catch(Exception e) {
+				log.warn("Error processing email job",e);
+			} finally {
+				// this server must release lock
+				lockManager.releaseLock(LOCK_CONSOLIDATED_EMAIL_JOB, serverId);
+			}
+			
+		}
 	}
 
 }
