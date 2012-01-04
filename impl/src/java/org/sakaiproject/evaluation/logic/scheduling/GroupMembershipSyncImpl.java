5581accaf555a8e95a0921cf990cde615fe7e4fb
Jim Eng <jimeng@umich.edu>
2012-01-04 22:11:11 +0000

    EVALSYS-1178 Added explicit checks in GroupMembershipSyncImpl for the sync-server-id, to ensure that the sync runs only on that server.
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@78084 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- impl/src/java/org/sakaiproject/evaluation/logic/scheduling/GroupMembershipSyncImpl.java
+++ impl/src/java/org/sakaiproject/evaluation/logic/scheduling/GroupMembershipSyncImpl.java
@@ -14,6 +14,8 @@ import org.quartz.JobExecutionException;
 import org.sakaiproject.evaluation.logic.EvalEvaluationService;
 import org.sakaiproject.evaluation.logic.EvalEvaluationSetupService;
 import org.sakaiproject.evaluation.logic.EvalEvaluationSetupServiceImpl;
+import org.sakaiproject.evaluation.logic.EvalSettings;
+import org.sakaiproject.evaluation.logic.externals.EvalExternalLogic;
 import org.sakaiproject.evaluation.model.EvalEvaluation;
 
 /**
@@ -33,6 +35,16 @@ public class GroupMembershipSyncImpl implements GroupMembershipSync {
     public void setEvaluationSetupService(EvalEvaluationSetupService evaluationSetupService) {
         this.evaluationSetupService = evaluationSetupService;
     }
+    
+    private EvalExternalLogic externalLogic;
+    public void setExternalLogic(EvalExternalLogic externalLogic) {
+        this.externalLogic = externalLogic;
+    }
+
+    private EvalSettings evalSettings;
+    public void setEvalSettings(EvalSettings settings) {
+        this.evalSettings = settings;
+    }
 
     /*
      * (non-Javadoc)
@@ -40,62 +52,73 @@ public class GroupMembershipSyncImpl implements GroupMembershipSync {
      */
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 		logger.debug("GroupMembershipSync.execute()");
-		JobDetail jobDetail = context.getJobDetail();
-		JobDataMap data = jobDetail.getJobDataMap();
-		String statusStr = (String) data.get(GroupMembershipSync.GROUP_MEMBERSHIP_SYNC_PROPNAME_STATE_LIST);
-		if(statusStr == null || statusStr.trim().equals("")) {
-			// better throw something?
-		} else {
-			String[] stateList = statusStr.trim().split(" ");
-
-			for(String state : stateList) {
-				List<EvalEvaluation> evals = evaluationService.getEvaluationsByState(state);
-				if(logger.isDebugEnabled()) {
-					StringBuilder buf = new StringBuilder();
-					buf.append("====> ");
-					buf.append(state);
-					buf.append(" evals  ==> ");
-					buf.append(evals.size());
-					logger.debug(buf.toString());
-				}
-				 
-				for(EvalEvaluation eval : evals) {
-					if(this.evaluationSetupService instanceof EvalEvaluationSetupServiceImpl) {
-						if(logger.isDebugEnabled()) {
-							StringBuilder buf = new StringBuilder();
-							buf.append("====> ");
-							buf.append(state);
-							buf.append("          ==> ");
-							buf.append(eval.getEid());
-							buf.append(" using impl");
-							logger.debug(buf.toString());
+		String syncServerId = (String) this.evalSettings.get(EvalSettings.SYNC_SERVER);
+		String thisServerId = this.externalLogic.getServerId();
+		if(thisServerId != null && thisServerId.equals(syncServerId)) {
+			JobDetail jobDetail = context.getJobDetail();
+			JobDataMap data = jobDetail.getJobDataMap();
+			String statusStr = (String) data.get(GroupMembershipSync.GROUP_MEMBERSHIP_SYNC_PROPNAME_STATE_LIST);
+			logger.info("GroupMembershipSync.execute() starting sync of evals by state: " + statusStr);
+			if(statusStr == null || statusStr.trim().equals("")) {
+				// better throw something?
+			} else {
+				String[] stateList = statusStr.trim().split(" ");
+				
+				logger.info("GroupMembershipSync.execute() syncing " + statusStr);
+	
+				for(String state : stateList) {
+					List<EvalEvaluation> evals = evaluationService.getEvaluationsByState(state);
+					int count = 0;
+					if(evals != null) {
+						count = evals.size();
+					}
+					if(logger.isInfoEnabled()) {
+						StringBuilder buf1 = new StringBuilder();
+						buf1.append("GroupMembershipSync.execute() syncing ");
+						buf1.append(count);
+						buf1.append("groups for evals in state: ");
+						buf1.append(state);
+						logger.info(buf1.toString());
+					}
+					for(EvalEvaluation eval : evals) {
+						if(this.evaluationSetupService instanceof EvalEvaluationSetupServiceImpl) {
+							if(logger.isDebugEnabled()) {
+								StringBuilder buf = new StringBuilder();
+								buf.append("====> ");
+								buf.append(state);
+								buf.append("          ==> ");
+								buf.append(eval.getEid());
+								buf.append(" using impl");
+								logger.debug(buf.toString());
+							}
+							try {
+								((EvalEvaluationSetupServiceImpl) this.evaluationSetupService).synchronizeUserAssignmentsForced(eval, null, true);
+							} catch(IllegalStateException e) {
+								StringBuilder buf = new StringBuilder();
+								buf.append("Unable to user assignments for eval (");
+								buf.append(eval.getId());
+								buf.append(") due to IllegalStateException: ");
+								buf.append(e.getMessage());
+								logger.warn(buf.toString());
+								
+								// TODO: should update the state so it is not selected next time ??
+							}
+						} else {
+							if(logger.isDebugEnabled()) {
+								StringBuilder buf = new StringBuilder();
+								buf.append("====> ");
+								buf.append(state);
+								buf.append("          ==> ");
+								buf.append(eval.getEid());
+								buf.append(" using api");
+								logger.debug(buf.toString());
+							}
+							this.evaluationSetupService.synchronizeUserAssignments(eval.getId(), null);
 						}
-						try {
-							((EvalEvaluationSetupServiceImpl) this.evaluationSetupService).synchronizeUserAssignmentsForced(eval, null, true);
-						} catch(IllegalStateException e) {
-							StringBuilder buf = new StringBuilder();
-							buf.append("Unable to user assignments for eval (");
-							buf.append(eval.getId());
-							buf.append(") due to IllegalStateException: ");
-							buf.append(e.getMessage());
-							logger.warn(buf.toString());
-							
-							// TODO: should update the state so it is not selected next time ??
-						}
-					} else {
-						if(logger.isDebugEnabled()) {
-							StringBuilder buf = new StringBuilder();
-							buf.append("====> ");
-							buf.append(state);
-							buf.append("          ==> ");
-							buf.append(eval.getEid());
-							buf.append(" using api");
-							logger.debug(buf.toString());
-						}
-						this.evaluationSetupService.synchronizeUserAssignments(eval.getId(), null);
 					}
 				}
 			}
+			logger.info("GroupMembershipSync.execute() done with sync of evals by state: " + statusStr);
 		}
 	}
 	
