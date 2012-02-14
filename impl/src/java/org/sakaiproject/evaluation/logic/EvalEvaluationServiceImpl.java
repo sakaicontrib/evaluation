c79cd72011377c4e69e95ee11b00b3e049b60212
Michael Gillian <mgillian@unicon.net>
2012-02-14 21:31:05 +0000

    EVALSYS-1181 fix for incompaibility between save without submit and modify up to due date
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/branches/unicon-ucb@78433 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- impl/src/java/org/sakaiproject/evaluation/logic/EvalEvaluationServiceImpl.java
+++ impl/src/java/org/sakaiproject/evaluation/logic/EvalEvaluationServiceImpl.java
@@ -545,7 +545,7 @@ public class EvalEvaluationServiceImpl implements EvalEvaluationService {
                                     // cannot modify responses
                                     // check if the user already took this evaluation for this group
                                     EvalResponse response = getResponseForUserAndGroup(evaluationId, userId, evalGroupId);
-                                    if (response != null) {
+                                    if (response != null && response.getEndTime() != null) {
                                         // user already has a response saved for this evaluation and evalGroupId
                                         log.info("User (" + userId + ") cannot take evaluation (" + evaluationId 
                                                 + ") again in this group (" + evalGroupId 
