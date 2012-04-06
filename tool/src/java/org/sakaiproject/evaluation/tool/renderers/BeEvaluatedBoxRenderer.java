e98884f3ffa360b38a585f2484ae1039231c8dc7
Aaron Zeckoski <azeckoski@vt.edu>
2012-04-06 15:32:43 +0000

    EVALSYS-1241 fixed the data which appears in the be evaluated window
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/branches/unicon-ucb@79165 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/renderers/BeEvaluatedBoxRenderer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/renderers/BeEvaluatedBoxRenderer.java
@@ -25,6 +25,7 @@ package org.sakaiproject.evaluation.tool.renderers;
 
 import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
@@ -34,6 +35,7 @@ import org.sakaiproject.evaluation.logic.EvalCommonLogic;
 import org.sakaiproject.evaluation.logic.EvalDeliveryService;
 import org.sakaiproject.evaluation.logic.EvalEvaluationService;
 import org.sakaiproject.evaluation.logic.EvalEvaluationSetupService;
+import org.sakaiproject.evaluation.logic.EvalSettings;
 import org.sakaiproject.evaluation.logic.model.EvalGroup;
 import org.sakaiproject.evaluation.model.EvalEvaluation;
 import org.sakaiproject.evaluation.tool.producers.PreviewEvalProducer;
@@ -62,43 +64,41 @@ public class BeEvaluatedBoxRenderer {
     private DateFormat df;
 
     private Locale locale;
-
     public void setLocale(Locale locale) {
         this.locale = locale;
     }
 
-    private EvalCommonLogic commonLogic;
+    private EvalSettings settings;
+    public void setSettings(EvalSettings settings) {
+        this.settings = settings;
+    }
 
+    private EvalCommonLogic commonLogic;
     public void setCommonLogic(EvalCommonLogic commonLogic) {
         this.commonLogic = commonLogic;
     }
 
     private EvalEvaluationSetupService evaluationSetupService;
-
     public void setEvaluationSetupService(EvalEvaluationSetupService evaluationSetupService) {
         this.evaluationSetupService = evaluationSetupService;
     }
 
     private EvalEvaluationService evaluationService;
-
     public void setEvaluationService(EvalEvaluationService evaluationService) {
         this.evaluationService = evaluationService;
     }
 
     private HumanDateRenderer humanDateRenderer;
-
     public void setHumanDateRenderer(HumanDateRenderer humanDateRenderer) {
         this.humanDateRenderer = humanDateRenderer;
     }
 
     private EvalDeliveryService deliveryService;
-
     public void setDeliveryService(EvalDeliveryService deliveryService) {
         this.deliveryService = deliveryService;
     }
 
     private EvalBeanUtils evalBeanUtils;
-
     public void setEvalBeanUtils(EvalBeanUtils evalBeanUtils) {
         this.evalBeanUtils = evalBeanUtils;
     }
@@ -110,83 +110,69 @@ public class BeEvaluatedBoxRenderer {
     public void renderBox(UIContainer tofill, String currentUserId) {
         boolean evalsToShow = false;
         // need to determine if there are any evals in which the user can be evaluated
+        // this will not return any deleted or partial evals but it will return closed ones so you need to check which ones to show depending on the use cases
         List<EvalEvaluation> evalsForInstructor = this.evaluationSetupService.getEvaluationsForEvaluatee(currentUserId);
         if (evalsForInstructor == null || evalsForInstructor.isEmpty()) {
-            // no evals found
-            // show a message saying no evals?
+            // no evals found so show nothing
         } else {
             // evals found; show the widget
             UIBranchContainer evalResponsesBC = UIBranchContainer.make(tofill, "evalResponsesBox:");
             UIForm evalResponsesForm = UIForm.make(evalResponsesBC, "evalResponsesForm");
 
             UIBranchContainer evalResponseTable = null;
-            // build an array of evaluation ids
-            Long[] evalIds = new Long[evalsForInstructor.size()];
-            int i = 0;
-            for (EvalEvaluation eval : evalsForInstructor) {
-                evalIds[i++] = eval.getId();
-            }
+
+            // re-sort the evals so closed ones are at the end
+            evalsForInstructor = EvalUtils.sortClosedEvalsToEnd(evalsForInstructor);
 
             // show a list of evals with four columns:
             for (EvalEvaluation eval : evalsForInstructor) {
-                // is this eval partial, in-queue, active or grace period?
-                if (EvalConstants.EVALUATION_STATE_INQUEUE.equals(eval.getState())
-                        || EvalConstants.EVALUATION_STATE_PARTIAL.equals(eval.getState())
-                        || EvalConstants.EVALUATION_STATE_ACTIVE.equals(eval.getState())
-                        || EvalConstants.EVALUATION_STATE_GRACEPERIOD.equals(eval.getState())) {
-                    // there is an eval in-queue, active or grace period
-                    evalsToShow = true;
-                    if (evalResponseTable == null) {
-                        evalResponseTable = UIBranchContainer.make(evalResponsesForm, "evalResponseTable:");
-                        // show four column headings
+                evalsToShow = true;
+                if (evalResponseTable == null) {
+                    evalResponseTable = UIBranchContainer.make(evalResponsesForm, "evalResponseTable:");
+                    // show four column headings
 
-                    }
-                    // set display values for this eval
-                    String evalState = evaluationService.updateEvaluationState(eval.getId());
-                    evalState = commonLogic.calculateViewability(evalState);
-                    // show one link per group assigned to in-queue, active or grace period eval
-                    List<EvalGroup> groups = eval.getEvalGroups();
-                    if (groups == null) {
-                        groups = new ArrayList<EvalGroup>();
-                    }
-                    for (EvalGroup group : groups) {
-                        UIBranchContainer evalrow = UIBranchContainer.make(evalResponseTable, "evalResponsesList:");
-                        UIOutput.make(evalrow, "evalResponsesStartDate", df.format(eval.getStartDate()));
-                        humanDateRenderer.renderDate(evalrow, "evalResponsesDueDate", eval.getDueDate());
+                }
+                // set display values for this eval
+                String evalState = commonLogic.calculateViewability(eval.getState());
+                // show one link per group assigned to in-queue, active or grace period eval
+                List<EvalGroup> groups = eval.getEvalGroups();
+                if (groups == null) {
+                    groups = new ArrayList<EvalGroup>();
+                }
+                for (EvalGroup group : groups) {
+                    UIBranchContainer evalrow = UIBranchContainer.make(evalResponseTable, "evalResponsesList:");
+                    UIOutput.make(evalrow, "evalResponsesStartDate", df.format(eval.getStartDate()));
+                    humanDateRenderer.renderDate(evalrow, "evalResponsesDueDate", eval.getDueDate());
 
-                        String title = EvalUtils.makeMaxLengthString(group.title + " " + eval.getTitle() + " ", 50);
-                        UIInternalLink.make(evalrow, "evalResponsesTitleLink_preview", title, new EvalViewParameters(
-                                PreviewEvalProducer.VIEW_ID, eval.getId(), group.evalGroupId));// view
-                                                                                               // params
+                    String title = EvalUtils.makeMaxLengthString(group.title + " " + eval.getTitle() + " ", 50);
+                    UIInternalLink.make(evalrow, "evalResponsesTitleLink_preview", title, 
+                            new EvalViewParameters(PreviewEvalProducer.VIEW_ID, eval.getId(), group.evalGroupId));
 
-                        makeDateComponent(evalrow, eval, evalState, "evalResponsesDateLabel", "evalResponsesDate",
-                                "evalResponsesStatus");
+                    int responsesCount = deliveryService.countResponses(eval.getId(), group.evalGroupId, true);
+                    int enrollmentsCount = evaluationService.countParticipantsForEval(eval.getId(), new String[] { group.evalGroupId });
+                    int responsesNeeded = evalBeanUtils.getResponsesNeededToViewForResponseRate(responsesCount, enrollmentsCount);
+                    String responseString = EvalUtils.makeResponseRateStringFromCounts(responsesCount, enrollmentsCount);
 
-                        int responsesCount = deliveryService.countResponses(eval.getId(), group.evalGroupId, true);
-                        int enrollmentsCount = evaluationService.countParticipantsForEval(eval.getId(),
-                                new String[] { group.evalGroupId });
-                        int responsesNeeded = evalBeanUtils.getResponsesNeededToViewForResponseRate(responsesCount,
-                                enrollmentsCount);
-                        String responseString = EvalUtils.makeResponseRateStringFromCounts(responsesCount,
-                                enrollmentsCount);
-                        if (EvalUtils.checkStateAfter(evalState, EvalConstants.EVALUATION_STATE_CLOSED, true)) {
-                            if (responsesNeeded == 0) {
-                                UIInternalLink.make(evalrow, "evalResponsesDisplayLink", UIMessage.make(
-                                        "controlevaluations.eval.responses.inline", new Object[] { responseString }),
-                                        new ReportParameters(ReportsViewingProducer.VIEW_ID, eval.getId(),
-                                                new String[] { group.evalGroupId }));
-                            } else {
-                                UIOutput responseOutput = UIOutput
-                                        .make(evalrow, "evalResponsesDisplay", responseString);
-                                responseOutput.decorate(new UITooltipDecorator(UIMessage.make(
-                                        "controlevaluations.eval.report.awaiting.responses",
-                                        new Object[] { responsesNeeded })));
-                            }
+                    // now handle the results viewing flags (i.e. filter out evals the instructor should not see)
+                    boolean instViewResultsEval = evalBeanUtils.checkInstructorViewResultsForEval(eval);
+
+                    makeDateComponent(evalrow, eval, evalState, "evalResponsesDateLabel", "evalResponsesDate", "evalResponsesStatus", 
+                            instViewResultsEval, responsesCount, enrollmentsCount, responsesNeeded);
+
+                    if (EvalUtils.checkStateAfter(evalState, EvalConstants.EVALUATION_STATE_CLOSED, true)) {
+                        if (responsesNeeded == 0) {
+                            UIInternalLink.make(evalrow, "evalResponsesDisplayLink", UIMessage.make("controlevaluations.eval.responses.inline",
+                                    new Object[] { responseString }), new ReportParameters(ReportsViewingProducer.VIEW_ID, eval.getId(),
+                                    new String[] { group.evalGroupId }));
                         } else {
-                            UIOutput.make(evalrow, "evalResponsesDisplay", responseString);
+                            UIOutput responseOutput = UIOutput.make(evalrow, "evalResponsesDisplay", responseString);
+                            responseOutput.decorate(new UITooltipDecorator(UIMessage.make("controlevaluations.eval.report.awaiting.responses",
+                                    new Object[] { responsesNeeded })));
                         }
-                    }// link per group
-                }// partial, in-queue, active or grace period eval
+                    } else {
+                        UIOutput.make(evalrow, "evalResponsesDisplay", responseString);
+                    }
+                }// link per group
             }// for evals iterator
             if (!evalsToShow) {
                 UIMessage.make(evalResponsesForm, "summary-be-evaluated-none", "summary.be.evaluated.none");
@@ -194,8 +180,20 @@ public class BeEvaluatedBoxRenderer {
         }// there are evals for instructor
     }
 
-    protected void makeDateComponent(UIContainer evalrow, EvalEvaluation eval, String evalState, String evalDateLabel,
-            String evalDateItem, String evalStatusItem) {
+    /**
+     * @param evalrow
+     * @param eval
+     * @param evalState
+     * @param evalDateLabel
+     * @param evalDateItem
+     * @param evalStatusItem
+     * @param instViewResultsEval if true, instructor can view results for this eval, else instructor cannot view them
+     * @param responsesCount
+     * @param enrollmentsCount
+     * @param responsesNeeded
+     */
+    protected void makeDateComponent(UIContainer evalrow, EvalEvaluation eval, String evalState, String evalDateLabel, String evalDateItem,
+            String evalStatusItem, boolean instViewResultsEval, int responsesCount, int enrollmentsCount, int responsesNeeded) {
         // use a date which is related to the current users locale
         DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM, locale);
         if (EvalConstants.EVALUATION_STATE_INQUEUE.equals(evalState)) {
@@ -218,37 +216,29 @@ public class BeEvaluatedBoxRenderer {
 
             UIMessage.make(evalrow, evalStatusItem, "summary.status." + evalState);
         } else if (EvalConstants.EVALUATION_STATE_GRACEPERIOD.equals(evalState)) {
-            // Evaluations can have a grace period, if so that must
-            // close at some point;
+            // Evaluations can have a grace period, if so that must close at some point;
             // Grace periods never remain open forever
             UIMessage.make(evalrow, evalDateLabel, "summary.label.gracetill");
             UIOutput.make(evalrow, evalDateItem, df.format(eval.getSafeStopDate()));
 
             UIMessage.make(evalrow, evalStatusItem, "summary.status." + evalState);
         } else if (EvalConstants.EVALUATION_STATE_CLOSED.equals(evalState)) {
-            // if an evaluation is closed then it is not yet viewable
-            // and ViewDate must have been set
+            // if an evaluation is closed then it is not yet viewable and ViewDate must have been set
             UIMessage.make(evalrow, evalDateLabel, "summary.label.resultsviewableon");
             UIOutput.make(evalrow, evalDateItem, df.format(eval.getSafeViewDate()));
 
             UIMessage.make(evalrow, evalStatusItem, "summary.status." + evalState);
         } else if (EvalConstants.EVALUATION_STATE_VIEWABLE.equals(evalState)) {
-            // TODO if an evaluation is viewable we may want to notify
-            // if there are instructor/student dates
+            // if an evaluation is viewable we have to check the instructor view date
             UIMessage.make(evalrow, evalDateLabel, "summary.label.resultsviewablesince");
             UIOutput.make(evalrow, evalDateItem, df.format(eval.getSafeViewDate()));
 
-            int responsesCount = deliveryService.countResponses(eval.getId(), null, true);
-            int enrollmentsCount = evaluationService.countParticipantsForEval(eval.getId(), null);
-            int responsesNeeded = evalBeanUtils.getResponsesNeededToViewForResponseRate(responsesCount,
-                    enrollmentsCount);
             if (responsesNeeded == 0) {
-                UIInternalLink.make(evalrow, "viewReportLink", UIMessage.make("viewreport.page.title"),
-                        new ReportParameters(ReportChooseGroupsProducer.VIEW_ID, eval.getId()));
+                UIInternalLink.make(evalrow, "viewReportLink", UIMessage.make("viewreport.page.title"), new ReportParameters(
+                        ReportChooseGroupsProducer.VIEW_ID, eval.getId()));
             } else {
                 UIMessage.make(evalrow, evalStatusItem, "summary.status." + evalState).decorate(
-                        new UITooltipDecorator(UIMessage.make("controlevaluations.eval.report.awaiting.responses",
-                                new Object[] { responsesNeeded })));
+                        new UITooltipDecorator(UIMessage.make("controlevaluations.eval.report.awaiting.responses", new Object[] { responsesNeeded })));
             }
         } else {
             UIMessage.make(evalrow, evalDateLabel, "summary.label.fallback");
