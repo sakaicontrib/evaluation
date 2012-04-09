ccc80dc06796a095eadd5a3b532874926b9e759b
Aaron Zeckoski <azeckoski@vt.edu>
2012-04-09 13:45:17 +0000

    NOJIRA fixing up the svn props
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/branches/unicon-ucb@79176 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/admin.provider.sync.js
+++ tool/src/webapp/content/js/admin.provider.sync.js
@@ -1,3 +1,16 @@
+/**
+ * evaluation system project
+ * $URL$
+ * $Id$
+ **************************************************************************
+ * Copyright (c) 2008 Centre for Applied Research in Educational Technologies, University of Cambridge
+ * Licensed under the Educational Community License version 1.0
+ * 
+ * A copy of the Educational Community License has been included in this 
+ * distribution and is available at: http://www.opensource.org/licenses/ecl1.php
+ *
+ * Aaron Zeckoski (azeckoski@gmail.com) (aaronz@vt.edu) (aaron@caret.cam.ac.uk)
+ */
 var evalsysAdminSync = evalsysAdminSync || {};
 
 evalsysAdminSync.resizeFrame = function(updown) {
