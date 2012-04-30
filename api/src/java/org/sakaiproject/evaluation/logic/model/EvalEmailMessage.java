a70147910479f058a40e441346f3b59428736144
Aaron Zeckoski <azeckoski@vt.edu>
2012-04-30 17:20:14 +0000

    NOJIRA fixed up all license headers in the entire project to be consistent
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@79443 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- api/src/java/org/sakaiproject/evaluation/logic/model/EvalEmailMessage.java
+++ api/src/java/org/sakaiproject/evaluation/logic/model/EvalEmailMessage.java
@@ -1,17 +1,17 @@
 /**
- * $Id: EvalEmailMessage.java 1000 Jun 10, 2010 12:26:47 PM azeckoski $
- * $URL: https://source.sakaiproject.org/contrib $
- * EvalEmailMessage.java - evaluation - Jun 10, 2010 12:26:47 PM - azeckoski
- **************************************************************************
- * Copyright (c) 2008 Aaron Zeckoski
- * Licensed under the Apache License, Version 2.0
- * 
- * A copy of the Apache License has been included in this 
- * distribution and is available at: http://www.apache.org/licenses/LICENSE-2.0.txt
+ * Copyright 2006 Sakai Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
  *
- * Aaron Zeckoski (azeckoski @ gmail.com) (aaronz @ vt.edu) (aaron @ caret.cam.ac.uk)
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  */
-
 package org.sakaiproject.evaluation.logic.model;
 
 
--- tool/src/java/org/sakaiproject/evaluation/tool/renderers/ScaledRenderer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/renderers/ScaledRenderer.java
@@ -1,17 +1,17 @@
 /**
- * ScaledRenderer.java - evaluation - Oct 29, 2007 2:59:12 PM - azeckoski
- * $URL$
- * $Id$
- **************************************************************************
- * Copyright (c) 2008 Centre for Applied Research in Educational Technologies, University of Cambridge
- * Licensed under the Educational Community License version 1.0
+ * Copyright 2005 Sakai Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
  *
- * A copy of the Educational Community License has been included in this
- * distribution and is available at: http://www.opensource.org/licenses/ecl1.php
+ * http://www.osedu.org/licenses/ECL-2.0
  *
- * Aaron Zeckoski (azeckoski@gmail.com) (aaronz@vt.edu) (aaron@caret.cam.ac.uk)
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  */
-
 package org.sakaiproject.evaluation.tool.renderers;
 
 import java.util.Map;
