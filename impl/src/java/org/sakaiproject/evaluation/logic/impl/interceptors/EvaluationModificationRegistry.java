efc2449853efd006d2dab8738e1446dfbf2dd8f0
Aaron Zeckoski <azeckoski@vt.edu>
2012-04-30 17:20:14 +0000

    NOJIRA fixed up all license headers in the entire project to be consistent
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/trunk@79443 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- impl/src/java/org/sakaiproject/evaluation/logic/impl/interceptors/EvaluationModificationRegistry.java
+++ impl/src/java/org/sakaiproject/evaluation/logic/impl/interceptors/EvaluationModificationRegistry.java
@@ -1,18 +1,17 @@
-/******************************************************************************
- * EvaluationModificationRegistry.java - created on Jan 03, 2006
- * 
- * Copyright (c) 2007 Virginia Polytechnic Institute and State University
- * Licensed under the Educational Community License version 1.0
- * 
- * A copy of the Educational Community License has been included in this 
- * distribution and is available at: http://www.opensource.org/licenses/ecl1.php
- * 
- * Contributors:
- * Antranig Basman
- * Aaron Zeckoski (aaronz@vt.edu)
- * 
- *****************************************************************************/
-
+/**
+ * Copyright 2006 Sakai Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 package org.sakaiproject.evaluation.logic.impl.interceptors;
 
 import java.util.HashMap;
--- tool/src/webapp/content/js/reorderUtils.js
+++ tool/src/webapp/content/js/reorderUtils.js
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2005 Sakai Foundation Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 /**
  * Reordering utilities for the items drag and drop ordering page -AZ
  * Does not put stuff into the global namespace and uses only methods that are compatible with most browsers
