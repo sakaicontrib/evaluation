9b294819c6014cdf77a689ab7a45485455ca214d
Aaron Zeckoski <azeckoski@vt.edu>
2012-06-20 14:08:22 +0000

    EVALSYS-1279 Add time display to evaluations to take on dashboard
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/branches/unicon-ucb@80439 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/evaluation/tool/renderers/HumanDateRenderer.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/renderers/HumanDateRenderer.java
@@ -29,26 +29,29 @@ import uk.org.ponder.rsf.components.decorators.UITooltipDecorator;
 public class HumanDateRenderer {
     private long MILLIS_PER_DAY = 24*60*60*1000;
     private DateFormat df;
-    
+    private DateFormat tf;
+    private boolean useDateTime = false;
+
     private Locale locale;
     public void setLocale(Locale locale) {
-       this.locale = locale;
+        this.locale = locale;
     }
-    
+
     private EvalSettings settings;
     public void setSettings(EvalSettings settings) {
         this.settings = settings;
     }
-    
+
     public void init() {
-        boolean useDateTime = (Boolean) settings.get(EvalSettings.EVAL_USE_DATE_TIME);
+        useDateTime = (Boolean) settings.get(EvalSettings.EVAL_USE_DATE_TIME);
+        tf = DateFormat.getTimeInstance(DateFormat.SHORT, locale); // e.g. 3:30pm
         if (useDateTime) {
-        	df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT);
+            df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT, locale);
         } else {
-        	df = DateFormat.getDateInstance(DateFormat.MEDIUM, locale);
+            df = DateFormat.getDateInstance(DateFormat.MEDIUM, locale);
         }
     }
-    
+
     /**
      * renderDate updates the passed field to display a human-readable date,
      * along with a tooltip of the actual date, based on a date passed
@@ -66,41 +69,48 @@ public class HumanDateRenderer {
     public void renderDate(UIContainer parent, String fieldName, Date renderDate) {
         // if date is less than WARNING_DAYS in the future, add emphasis
         final int WARNING_DAYS = 2;
-        
+
         // default render values
-        String message = "humandate.date";
-        String[] parameters = { df.format(renderDate) };
-        
-        long diffInDays = calculateDateDiff(renderDate);        
+        String messagePrefix = "humandate.";
+        if (useDateTime) {
+            messagePrefix = "humandatetime.";
+        }
+        String message = messagePrefix+"date";
+        String[] parameters = { df.format(renderDate), tf.format(renderDate) };
+
+        long diffInDays = calculateDateDiff(renderDate);
         if (diffInDays > 7) {
-            message = "humandate.date";
+            // use default
+            //message = messagePrefix+"date";
         } else if (diffInDays == 7) {
-            message = "humandate.inweek";
+            message = messagePrefix+"inweek";
         } else if (diffInDays > 1) {
-            message = "humandate.indays";
+            message = messagePrefix+"indays";
             parameters[0] = Long.toString(Math.abs(diffInDays));
         } else if (diffInDays == 1) {
-            message = "humandate.tomorrow";
+            message = messagePrefix+"tomorrow";
         } else if (diffInDays == 0) {
-            message = "humandate.today";
+            message = messagePrefix+"today";
         } else if (diffInDays == -1) {
-            message = "humandate.yesterday";
+            message = messagePrefix+"yesterday";
         } else if (diffInDays > -7) {
-            message = "humandate.daysago";
+            message = messagePrefix+"daysago";
             parameters[0] = Long.toString(Math.abs(diffInDays));
         } else if (diffInDays == -7) {
-            message = "humandate.weekago";
+            message = messagePrefix+"weekago";
         } else {
-            message = "humandate.date";
+            // diffInDays > 7, etc.
+            // use default
+            //message = messagePrefix+"date";
         }
-        
+
         UIMessage output = UIMessage.make(parent, fieldName, message, parameters);
         output.decorate(new UITooltipDecorator( UIOutput.make(df.format(renderDate)) ));
         if (diffInDays >= 0 && diffInDays <= WARNING_DAYS) {
             output.decorate(new UIStyleDecorator( "urgentStyle" ));
         }
     }
-    
+
     /**
      * calculateDateDiff returns the number of days between the passed date and 
      * the current date.  Dates are normalized to 23:59:59, so that only year/month/day
@@ -118,14 +128,14 @@ public class HumanDateRenderer {
         Calendar trimDate = Calendar.getInstance();
         trimDate.setTime(_date);
         setEndOfDay(trimDate);
-        
+
         Calendar curDate = Calendar.getInstance();
         setEndOfDay(curDate);
 
         long diffInMillis = trimDate.getTimeInMillis() - curDate.getTimeInMillis();
         return diffInMillis / MILLIS_PER_DAY;
     }
-    
+
     /**
      * setEndOfDay changes the hours, minutes, and seconds to be at the end of 
      * the day - 23:59:59
@@ -138,4 +148,5 @@ public class HumanDateRenderer {
         cal.set(Calendar.SECOND, 59);
         cal.set(Calendar.MILLISECOND, 0);        
     }
+
 }
