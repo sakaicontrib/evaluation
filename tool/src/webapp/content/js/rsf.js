5a4e27c5ba273a1019b804ffd064bb87045d4298
Aaron Zeckoski <azeckoski@vt.edu>
2012-09-26 14:29:17 +0000

    NOJIRA fixing an RSF JS bug which causes header warnings in webkit browsers
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/branches/unicon-ucb@81454 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/webapp/content/js/rsf.js
+++ tool/src/webapp/content/js/rsf.js
@@ -589,8 +589,9 @@ var RSF = RSF || {};
             contentType = options.headers["Content-type"];
             }
           http_request.setRequestHeader("Content-type", contentType);
-          http_request.setRequestHeader("Content-length", parameters.length);
-          http_request.setRequestHeader("Connection", "close");
+          // NOTE: setRequestHeader Content-length/Connection is not allowed in webkit browsers anymore -AZ 2012
+          //http_request.setRequestHeader("Content-length", parameters.length);
+          //http_request.setRequestHeader("Connection", "close");
           http_request.send(parameters);
        }
        //delete(http_request); // NOTE: clearing this causes problems for IE7 and lower
