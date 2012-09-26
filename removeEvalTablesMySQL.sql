d9469d729dc11043afc85ce29a61ff9b289ce982
Aaron Zeckoski <azeckoski@vt.edu>
2012-09-26 13:26:05 +0000

    NOJIRA Fixing the mysql clear tables script to include all the current tables (including the new ones added by UM and UMD)
    git-svn-id: https://source.sakaiproject.org/contrib/evaluation/branches/unicon-ucb@81452 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- removeEvalTablesMySQL.sql
+++ removeEvalTablesMySQL.sql
@@ -13,35 +13,35 @@
 -- permissions and limitations under the License.
 --
 
--- This is the SQL for use on MySQL to clear the Sakai Evaluation tables from
--- the database.
+-- This is the SQL for use on MySQL to clear the Sakai Evaluation tables from the database.
 --
--- WARNING: This will delete all your evaluation data permanently! It should only
---          be used for development purposes.
+-- WARNING: This will delete all your evaluation data permanently! It should only be used for development purposes.
 --
 set FOREIGN_KEY_CHECKS=0;
-drop table if exists  EVAL_ADHOC_EVALUATEES;
-drop table if exists  EVAL_ADHOC_PARTICIPANTS;
-drop table if exists  EVAL_ADHOC_GROUP;
-drop table if exists  EVAL_ADHOC_USER;
-drop table if exists  EVAL_CONFIG;
-drop table if exists  EVAL_LOCK;
-drop table if exists  EVAL_TAGS;
-drop table if exists  EVAL_TAGS_META;
-drop table if exists  EVAL_TRANSLATION;
-drop table if exists  EVAL_ASSIGN_GROUP;
-drop table if exists  EVAL_ASSIGN_USER;
-drop table if exists  EVAL_ANSWER;
-drop table if exists  EVAL_RESPONSE;
-drop table if exists  EVAL_TEMPLATEITEM;
-drop table if exists  EVAL_EVALUATION;
-drop table if exists  EVAL_EMAIL_TEMPLATE;
-drop table if exists  EVAL_TEMPLATE;
-drop table if exists  EVAL_ITEMGROUP;
-drop table if exists  EVAL_ITEM;
-drop table if exists  EVAL_SCALE;
-drop table if exists  EVAL_SCALE_OPTIONS;
-drop table if exists  EVAL_GROUPNODES;
-drop table if exists  EVAL_GROUPNODES_GROUPS;
-drop table if exists  EVAL_ASSIGN_HIERARCHY;
+drop table if exists  eval_adhoc_evaluatees;
+drop table if exists  eval_adhoc_group;
+drop table if exists  eval_adhoc_participants;
+drop table if exists  eval_adhoc_user;
+drop table if exists  eval_admin;
+drop table if exists  eval_answer;
+drop table if exists  eval_assign_group;
+drop table if exists  eval_assign_hierarchy;
+drop table if exists  eval_assign_user;
+drop table if exists  eval_config;
+drop table if exists  eval_email_processing_queue;
+drop table if exists  eval_email_template;
+drop table if exists  eval_evaluation;
+drop table if exists  eval_groupnodes;
+drop table if exists  eval_groupnodes_groups;
+drop table if exists  eval_item;
+drop table if exists  eval_itemgroup;
+drop table if exists  eval_lock;
+drop table if exists  eval_response;
+drop table if exists  eval_scale;
+drop table if exists  eval_scale_options;
+drop table if exists  eval_tags;
+drop table if exists  eval_tags_meta;
+drop table if exists  eval_template;
+drop table if exists  eval_templateitem;
+drop table if exists  eval_translation;
 set FOREIGN_KEY_CHECKS=1;
