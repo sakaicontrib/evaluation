950047e632af87ae02b9373cd45edcbbd9c42ba0
danielmerino <daniel.merino@unavarra.es>
2015-06-08 12:07:22 +0200

    EVALSYS-1122 - Manual reminders don't have template variables expanded.
    Patch adapted from Matthew Bucket's one to work in trunk.

--- tool/src/java/org/sakaiproject/evaluation/tool/SendEmailsBean.java
+++ tool/src/java/org/sakaiproject/evaluation/tool/SendEmailsBean.java
@@ -14,7 +14,13 @@
  */
 package org.sakaiproject.evaluation.tool;
 
+import java.util.HashMap;
+import org.sakaiproject.email.api.EmailMessage;
 import org.sakaiproject.evaluation.logic.EvalEmailsLogic;
+import org.sakaiproject.evaluation.logic.EvalEvaluationService;
+import org.sakaiproject.evaluation.logic.model.EvalEmailMessage;
+import org.sakaiproject.evaluation.logic.model.EvalGroup;
+import org.sakaiproject.evaluation.model.EvalEvaluation;
 import org.sakaiproject.evaluation.utils.EvalUtils;
 
 import uk.org.ponder.messageutil.TargettedMessage;
@@ -40,6 +46,11 @@ public class SendEmailsBean {
     private EvalEmailsLogic emailsLogic;
     public void setEmailsLogic(EvalEmailsLogic emailsLogic) {
         this.emailsLogic = emailsLogic;
+    }
+    
+    private EvalEvaluationService evalEvaluationService;
+    public void setEvalEvaluationService(EvalEvaluationService evalEvaluationService) {
+    this.evalEvaluationService = evalEvaluationService;
     }
 
     private TargettedMessageList messages;
@@ -67,7 +78,11 @@ public class SendEmailsBean {
         if (evalGroupId != null) {
             evalGroupIds = new String[] {evalGroupId};
         }
-        String[] sent = emailsLogic.sendEmailMessages(message, subject, evaluationId, evalGroupIds, sendTo);
+
+        EvalEvaluation evaluation = evalEvaluationService.getEvaluationById(evaluationId);
+        
+        EvalEmailMessage emailMessage = emailsLogic.makeEmailMessage(message, subject, evaluation, null, null);
+        String sent[] = emailsLogic.sendEmailMessages(emailMessage.message, emailMessage.subject, evaluationId, evalGroupIds, sendTo);
 
         messages.addMessage( new TargettedMessage("evalnotify.sent.mails",
                 new Object[] { sent.length }, TargettedMessage.SEVERITY_INFO));
