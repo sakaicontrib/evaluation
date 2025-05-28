31fbefd51531c97115cabbf79aa4b170ea188e5c
Sam Ottenhoff <ottenhoff@longsight.com>
2025-05-28 14:11:23 -0400

    Remove Apache Velocity as a backup template engine (#166) (#167)

--- sakai-evaluation-impl/src/java/org/sakaiproject/evaluation/utils/TextTemplateLogicUtils.java
+++ sakai-evaluation-impl/src/java/org/sakaiproject/evaluation/utils/TextTemplateLogicUtils.java
@@ -22,12 +22,6 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.VelocityEngine;
-import org.apache.velocity.exception.MethodInvocationException;
-import org.apache.velocity.exception.ParseErrorException;
-import org.apache.velocity.exception.ResourceNotFoundException;
-import org.apache.velocity.runtime.RuntimeConstants;
 
 import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapper;
@@ -46,16 +40,6 @@ public class TextTemplateLogicUtils {
 
     public static final Object LOCK = new Object();
 
-    /**
-     * Set this to true to use freemarker (http://freemarker.org/) for template processing,
-     * freemarker will be used by default
-     */
-    public static boolean useFreemarker = false;
-    /**
-     * Set this to true to use Velocity (http://velocity.apache.org/) for template processing,
-     * freemarker will be used by default
-     */
-    public static boolean useVelocity = false;
     public final static int TOTAL_RESET_COUNT = 500;
     /**
      * Used to track and reset the processor every totalResetCount runs
@@ -68,10 +52,8 @@ public class TextTemplateLogicUtils {
      * the text template as well<br/>
      * Uses and expects freemarker (http://freemarker.org/) style templates 
      * (that is using ${name} as the marker for a replacement)<br/>
-     * NOTE: These should be compatible with Velocity (http://velocity.apache.org/) templates
-     * if you use the formal notation (formal: ${variable}, shorthand: $variable)
      * 
-     * @param textTemplate a freemarker/velocity style text template,
+     * @param textTemplate a freemarker style text template,
      * cannot be null or empty string
      * @param replacementValues a set of replacement values which are in the map like so:<br/>
      * key => value (String => Object)<br/>
@@ -89,13 +71,7 @@ public class TextTemplateLogicUtils {
             "please pass in at least something in the template or do not call this method");
         }
 
-        if (useFreemarker == true) {
-            return processFreemarkerTextTemplate(textTemplate, replacementValues);
-        } else if (useVelocity == true) {
-            return processVelocityTextTemplate(textTemplate, replacementValues);
-        } else {
-            return processFreemarkerTextTemplate(textTemplate, replacementValues);
-        }
+        return processFreemarkerTextTemplate(textTemplate, replacementValues);
     }
 
     private static Configuration freemarkerConfig = null;
@@ -149,57 +125,5 @@ public class TextTemplateLogicUtils {
         return true;
     }
 
-    private static VelocityEngine velocityEngine = null;
-    private static String processVelocityTextTemplate(String textTemplate, Map<String, String> replacementValues) {
-
-        // setup velocity if not already done
-        synchronized (LOCK) {
-            if (velocityEngine == null || resetCounter.addAndGet(1) > TOTAL_RESET_COUNT) {
-                // setup the velocity configuration via properties
-                Properties p = new Properties();
-                p.setProperty(RuntimeConstants.INPUT_ENCODING, "UTF-8");
-                p.setProperty(RuntimeConstants.OUTPUT_ENCODING, "UTF-8");
-                p.setProperty(RuntimeConstants.RUNTIME_LOG_LOGSYSTEM_CLASS, "org.apache.velocity.runtime.log.NullLogSystem"); // no logging at all
-                //       p.setProperty(RuntimeConstants.RUNTIME_LOG_LOGSYSTEM_CLASS, "org.apache.velocity.runtime.log.Log4JLogChute"); // SimpleLog4JLogSystem");
-                p.setProperty("runtime.log.logsystem.log4j.category", "vm.none");
-                p.setProperty(RuntimeConstants.RUNTIME_LOG_REFERENCE_LOG_INVALID, "true");
-                p.setProperty(RuntimeConstants.RESOURCE_MANAGER_DEFAULTCACHE_SIZE, "0");
-                p.setProperty(RuntimeConstants.EVENTHANDLER_INVALIDREFERENCES, "org.apache.velocity.app.event.implement.ReportInvalidReferences");
-                p.setProperty("eventhandler.invalidreference.exception", "true");
-                try {
-                    // attempt to create a new instance of velocity -AZ
-                    velocityEngine = new VelocityEngine();
-                    velocityEngine.init(p); // initialize the engine with the set of properties
-                } catch (Exception e) {
-                    throw new RuntimeException("Could not initialize velocity", e);
-                }
-                resetCounter.getAndSet(0);
-                log.info("Constructed new velocity engine for template processing");
-            }
-        }
-
-        // load in the passed in replacement values
-        VelocityContext context = new VelocityContext(replacementValues);
-
-        Writer output = new StringWriter();
-        boolean result = false;
-        try {
-            result = velocityEngine.evaluate(context, output, "textProcess", textTemplate);
-        } catch (ParseErrorException e) {
-            throw new RuntimeException("Velocity parsing error: ", e);
-        } catch (MethodInvocationException e) {
-            throw new RuntimeException("Velocity method invocation error: ", e);
-        } catch (ResourceNotFoundException e) {
-            throw new RuntimeException("Velocity resource not found error: ", e);
-        } catch (IOException e) {
-            throw new RuntimeException("Velocity IO error: ", e);
-        }
-
-        if ( result ) {
-            return output.toString();
-        } else {
-            throw new RuntimeException("Failed to process velocity text template: " + textTemplate);
-        }
-    }
 
 }
